9937bbc209b4720777a933fbfdc4036b01c5f2da
BigRetroMike <bigretromike@users.noreply.github.com>
2015-12-28 12:48:47 +0100

    Episodes with few types have not '|' in the name so parsing is impossible

--- JMMServer/Kodi/KodiEpisodeType.cs
+++ JMMServer/Kodi/KodiEpisodeType.cs
@@ -19,54 +19,54 @@ namespace JMMServer.Kodi
             switch (epType)
             {
                 case enEpisodeType.Credits:
-                    tp.Name = plural ? "Credits" : "Credit";
+                    tp.Name = plural ? "Credits|" : "Credit|";
                     tp.Image = "plex_credits.png";
                     return;
                 case enEpisodeType.Episode:
                     switch (an)
                     {
                         case AnimeTypes.Movie:
-                            tp.Name = plural ? "Movies" : "Movie";
+                            tp.Name = plural ? "Movies|" : "Movie|";
                             tp.Image = "plex_movies.png";
                             return;
                         case AnimeTypes.OVA:
-                            tp.Name = plural ? "Ovas" : "Ova";
+                            tp.Name = plural ? "Ovas|" : "Ova|";
                             tp.Image = "plex_ovas.png";
                             return;
                         case AnimeTypes.Other:
-                            tp.Name = plural ? "Others" : "Other";
+                            tp.Name = plural ? "Others|" : "Other|";
                             tp.Image = "plex_others.png";
                             return;
                         case AnimeTypes.TV_Series:
-                            tp.Name = plural ? "Episodes" : "Episode";
+                            tp.Name = plural ? "Episodes|" : "Episode|";
                             tp.Image = "plex_episodes.png";
                             return;
                         case AnimeTypes.TV_Special:
-                            tp.Name = plural ? "TV Episodes" : "TV Episode";
+                            tp.Name = plural ? "TV Episodes|" : "TV Episode|";
                             tp.Image = "plex_tvepisodes.png";
                             return;
                         case AnimeTypes.Web:
-                            tp.Name = plural ? "Web Clips" : "Web Clip";
+                            tp.Name = plural ? "Web Clips|" : "Web Clip|";
                             tp.Image = "plex_webclips.png";
                             return;
                     }
-                    tp.Name = plural ? "Episodes" : "Episode";
+                    tp.Name = plural ? "Episodes|" : "Episode|";
                     tp.Image = "plex_episodes.png";
                     return;
                 case enEpisodeType.Parody:
-                    tp.Name = plural ? "Parodies" : "Parody";
+                    tp.Name = plural ? "Parodies|" : "Parody|";
                     tp.Image = "plex_parodies.png";
                     return;
                 case enEpisodeType.Special:
-                    tp.Name = plural ? "Specials" : "Special";
+                    tp.Name = plural ? "Specials|" : "Special|";
                     tp.Image = "plex_specials.png";
                     return;
                 case enEpisodeType.Trailer:
-                    tp.Name = plural ? "Trailers" : "Trailer";
+                    tp.Name = plural ? "Trailers|" : "Trailer|";
                     tp.Image = "plex_trailers.png";
                     return;
                 default:
-                    tp.Name = "Misc";
+                    tp.Name = "Misc|";
                     tp.Image = "plex_misc.png";
                     return;
             }
