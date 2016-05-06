f48eba1ca57df2da0f049fef7f10b0661d7ad53a
da3dsoul <da3dsoul@gmail.com>
2016-05-06 05:13:18 -0400

    more whitespace

--- JMMServer/Mappings/AnimeGroupMap.cs
+++ JMMServer/Mappings/AnimeGroupMap.cs
@@ -7,26 +7,26 @@ using JMMServer.Entities;
 
 namespace JMMServer.Mappings
 {
-	public class AnimeGroupMap : ClassMap<AnimeGroup>
-	{
-		public AnimeGroupMap()
+    public class AnimeGroupMap : ClassMap<AnimeGroup>
+    {
+        public AnimeGroupMap()
         {
-			Not.LazyLoad();
+            Not.LazyLoad();
             Id(x => x.AnimeGroupID);
 
-			Map(x => x.AnimeGroupParentID);
-			Map(x => x.DefaultAnimeSeriesID);
-			Map(x => x.DateTimeCreated).Not.Nullable();
-			Map(x => x.DateTimeUpdated).Not.Nullable();
-			Map(x => x.Description);
-			Map(x => x.GroupName);
-			Map(x => x.IsManuallyNamed).Not.Nullable();
-			Map(x => x.OverrideDescription).Not.Nullable();
-			Map(x => x.SortName);
-			Map(x => x.EpisodeAddedDate);
+            Map(x => x.AnimeGroupParentID);
+            Map(x => x.DefaultAnimeSeriesID);
+            Map(x => x.DateTimeCreated).Not.Nullable();
+            Map(x => x.DateTimeUpdated).Not.Nullable();
+            Map(x => x.Description);
+            Map(x => x.GroupName);
+            Map(x => x.IsManuallyNamed).Not.Nullable();
+            Map(x => x.OverrideDescription).Not.Nullable();
+            Map(x => x.SortName);
+            Map(x => x.EpisodeAddedDate);
             Map(x => x.LatestEpisodeAirDate);
             Map(x => x.MissingEpisodeCount).Not.Nullable();
-			Map(x => x.MissingEpisodeCountGroups).Not.Nullable();
+            Map(x => x.MissingEpisodeCountGroups).Not.Nullable();
         }
-	}
+    }
 }
