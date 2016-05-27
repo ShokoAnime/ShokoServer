f5d8edba3f09f8e52422b5c1d0087f715a1fe280
Maximo Piva <maximo.piva@gmail.com>
2016-05-27 14:12:52 -0300

    Revert "Added UpdateAt in Groups/Series"
    This reverts commit 5531bc46da1e497f0a077b261f2fa22694a8f4a3.

--- JMMServer/PlexAndKodi/Helper.cs
+++ JMMServer/PlexAndKodi/Helper.cs
@@ -541,7 +541,6 @@ namespace JMMServer.PlexAndKodi
                     {
                         Video v = GenerateFromSeries(cserie, ser, ser.GetAnime(session), userid);
                         v.AirDate = ser.AirDate.HasValue ? ser.AirDate.Value : DateTime.MinValue;
-                        v.UpdatedAt = ser.LatestEpisodeAirDate.HasValue ? ser.LatestEpisodeAirDate.Value.ToUnixTime() : null;
                         v.Group = cgrp;
                         return v;
                     }
@@ -554,7 +553,6 @@ namespace JMMServer.PlexAndKodi
                 Video v = FromGroup(cgrp, cserie, userid,subgrpcnt);
                 v.Group = cgrp;
                 v.AirDate = cgrp.Stat_AirDate_Min.HasValue ? cgrp.Stat_AirDate_Min.Value : DateTime.MinValue;
-                v.UpdatedAt = cgrp.LatestEpisodeAirDate.HasValue ? cgrp.LatestEpisodeAirDate.Value.ToUnixTime() : null;
                 return v;
             }
             return null;
