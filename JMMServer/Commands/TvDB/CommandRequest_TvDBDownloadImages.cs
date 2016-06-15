c8154452529b244a2d58630cf1fced870f89b89d
bigretromike <bigretromike@monogatari.pl>
2016-06-15 08:54:25 +0200

    Attempt #2 https://github.com/japanesemediamanager/jmmclient/issues/360

--- JMMServer/Commands/TvDB/CommandRequest_TvDBDownloadImages.cs
+++ JMMServer/Commands/TvDB/CommandRequest_TvDBDownloadImages.cs
@@ -27,13 +27,13 @@ namespace JMMServer.Commands
             }
         }
 
-        /*
+        
         public CommandRequest_TvDBDownloadImages()
         {
         }
-        */
+        
 
-        /*
+        
 		public CommandRequest_TvDBDownloadImages(int tvDBSeriesID, bool forced)
 		{
 			this.TvDBSeriesID = tvDBSeriesID;
@@ -43,7 +43,7 @@ namespace JMMServer.Commands
 
 			GenerateCommandID();
         }
-        */
+        
 
         public override void ProcessCommand()
         {
