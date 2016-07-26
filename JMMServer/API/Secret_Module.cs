c3d57130f7527ae5f6a181eb335938ec85b77b2b
bigretromike <bigretromike@monogatari.pl>
2016-07-26 10:22:09 +0200

    OAuth2 for API Currently tokens are per session (stored in memory)

--- JMMServer/API/Secret_Module.cs
+++ JMMServer/API/Secret_Module.cs
@@ -1,6 +1,5 @@
 ﻿using Nancy;
 using Nancy.Security;
-using System.Dynamic;
 
 namespace JMMServer.API
 {
@@ -9,12 +8,20 @@ namespace JMMServer.API
         public Secret_Module() : base("/secure")
         {
             this.RequiresAuthentication();
+            Get["/"] = x =>
+                {
+                    //Context.CurrentUser was set by StatelessAuthentication earlier in the pipeline
+                    var identity = this.Context.CurrentUser;
+                    //return the secure information in a json response
+                    var userModel = new JMMServer.Entities.JMMUser(identity.UserName);
+                    return this.Response.AsJson(new
+                    {
+                        SecureContent = "here's some secure content that you can only see if you provide a correct apiKey",
+                        User = userModel
 
-            Get["/"] = x => {
-                dynamic Model = new ExpandoObject();
-                Model = new JMMServer.Entities.JMMUser(this.Context.CurrentUser.UserName);
-                return View["API/Views/secure", Model];
-            };
+                    });
+
+                };
         }
     }
 }
