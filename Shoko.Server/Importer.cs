89a7352bc92b289d24b0d6a914f9dbeaeea456f3
da3dsoul <da3dsoul@gmail.com>
2017-01-12 09:27:28 -0500

    Get places to ensure update in remove missing files

--- Shoko.Server/Importer.cs
+++ Shoko.Server/Importer.cs
@@ -875,7 +875,7 @@ namespace Shoko.Server
 					        }
 				        }
 			        }
-			        if (places?.Count > 0) continue;
+			        if (v.Places?.Count > 0) continue;
 			        // delete video local record
 			        logger.Info("RemoveOrphanedVideoLocal : {0}", v.FileName);
 			        episodesToUpdate.UnionWith(v.GetAnimeEpisodes());
