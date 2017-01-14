7e59ca3e2764de14abf7286a03a70a7a92472884
Maximo Piva <maximo.piva@gmail.com>
2017-01-14 16:46:10 -0300

    WIP

---
+++ Interfaces/IJMMServerImage.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Nancy.Rest.Annotations.Atributes;
+using Nancy.Rest.Annotations.Enums;
+
+namespace Shoko.Models.Interfaces
+{
+    [RestBasePath("/Image")]
+    public interface IJMMServerImage
+    {
+        [Rest("{entityID}/{entityType}/{thumnbnailOnly}",Verbs.Get)]
+        Stream GetImage(string entityID, int entityType, bool thumnbnailOnly);
+
+        [Rest("{serverImagePath}", Verbs.Get)]
+        Stream GetImageUsingPath(string serverImagePath);
+    }
+}
---
+++ Interfaces/IJMMServerStreaming.cs
@@ -0,0 +1,12 @@
+﻿using Nancy.Rest.Annotations.Atributes;
+using Nancy.Rest.Annotations.Enums;
+
+namespace Shoko.Models.Interfaces
+{
+    [RestBasePath("/MetroDownload")]
+    public interface IJMMServerStreaming
+    {
+        [Rest("Download/{filename}",Verbs.Get)]
+        System.IO.Stream Download(string fileName);
+    }
+}
