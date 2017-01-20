b35143a1e0bf56cdbd13c79d5a914278548b7b94
Maximo Piva <maximo.piva@gmail.com>
2017-01-20 11:01:23 -0300

    WIP

--- Server/CrossRef_AniDB_TvDBV2.cs
+++ Server/CrossRef_AniDB_TvDBV2.cs
@@ -8,7 +8,7 @@ namespace Shoko.Models.Server
 {
     public class CrossRef_AniDB_TvDBV2
     {
-        public int CrossRef_AniDB_TvDBV2ID { get; private set; }
+        public int CrossRef_AniDB_TvDBV2ID { get; set; }
         public int AnimeID { get; set; }
         public int AniDBStartEpisodeType { get; set; }
         public int AniDBStartEpisodeNumber { get; set; }
