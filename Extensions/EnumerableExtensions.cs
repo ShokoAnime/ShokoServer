f1410ea22de5ecc1638e2fe235ddbbfee48823aa
da3dsoul <da3dsoul@gmail.com>
2017-04-20 02:59:47 -0400

    Add DistinctBy to EnumerableExtensions

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -8,6 +8,19 @@ namespace Shoko.Commons.Extensions
 {
     public static class EnumerableExtensions
     {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
         public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> items, Func<T, string> selector, StringComparer stringComparer = null)
         {
             var regex = new Regex(@"\d+", RegexOptions.Compiled);
