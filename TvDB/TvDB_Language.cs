3eb5e5c5b18b81a404b922d58c3c9895e9129f2b
hidden4003 <hidden@4you.lt>
2017-05-07 22:45:45 +0300

    TvDB API v2

--- TvDB/TvDB_Language.cs
+++ TvDB/TvDB_Language.cs
@@ -2,6 +2,8 @@
 {
     public class TvDB_Language
     {
+        public int? Id { get; set; }
+        public string EnglishName { get; set; }
         public string Name { get; set; }
         public string Abbreviation { get; set; }
 
