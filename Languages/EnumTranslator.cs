73c854d7f7aadc9646ca658cd4f4a0c2b4923cf1
da3dsoul <da3dsoul@gmail.com>
2017-05-19 07:46:58 -0400

    Remove Downloads

--- Languages/EnumTranslator.cs
+++ Languages/EnumTranslator.cs
@@ -5,7 +5,6 @@ namespace Shoko.Commons.Languages
 {
     public class EnumTranslator
     {
-        
         public static string EpisodeTypeTranslated(enEpisodeType epType)
         {
 
@@ -43,46 +42,6 @@ namespace Shoko.Commons.Languages
             if (epType == Properties.Resources.EpisodeType_Specials) return enEpisodeType.Special;
 
             return enEpisodeType.Episode;
-        }
-
-        public static string TorrentSourceTranslated(TorrentSourceType tsType)
-        {
-            switch (tsType)
-            {
-                case TorrentSourceType.TokyoToshokanAnime: return "Tokyo Toshokan (Anime)";
-                case TorrentSourceType.TokyoToshokanAll: return "Tokyo Toshokan (All)";
-                case TorrentSourceType.BakaBT: return "BakaBT";
-                case TorrentSourceType.Nyaa: return "Nyaa";
-                case TorrentSourceType.Sukebei: return "Sukebei Nyaa";
-                case TorrentSourceType.AnimeBytes: return "AnimeBytes";
-                default: return "Tokyo Toshokan (Anime)";
-            }
-        }
-
-        public static string TorrentSourceTranslatedShort(TorrentSourceType tsType)
-        {
-            switch (tsType)
-            {
-                case TorrentSourceType.TokyoToshokanAnime: return "TT";
-                case TorrentSourceType.TokyoToshokanAll: return "TT";
-                case TorrentSourceType.BakaBT: return "BakaBT";
-                case TorrentSourceType.Nyaa: return "Nyaa";
-                case TorrentSourceType.Sukebei: return "SukeNyaa";
-                case TorrentSourceType.AnimeBytes: return "AByt.es";
-                default: return "TT";
-            }
-        }
-
-        public static TorrentSourceType TorrentSourceTranslatedReverse(string tsType)
-        {
-            if (tsType == "Tokyo Toshokan (Anime)") return TorrentSourceType.TokyoToshokanAnime;
-            if (tsType == "Tokyo Toshokan (All)") return TorrentSourceType.TokyoToshokanAll;
-            if (tsType == "BakaBT") return TorrentSourceType.BakaBT;
-            if (tsType == "Nyaa") return TorrentSourceType.Nyaa;
-            if (tsType == "Sukebei Nyaa") return TorrentSourceType.Sukebei;
-            if (tsType == "AnimeBytes") return TorrentSourceType.AnimeBytes;
-
-            return TorrentSourceType.TokyoToshokanAnime;
         }
     }
 }
