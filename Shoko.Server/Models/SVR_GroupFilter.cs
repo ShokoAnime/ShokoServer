55e668bc8377ed2ba306aa2deb1fd1046e1db5fd
da3dsoul <da3dsoul@gmail.com>
2017-06-08 16:03:04 -0400

    Improve Group Filter Saving and Loading

--- Shoko.Server/Models/SVR_GroupFilter.cs
+++ Shoko.Server/Models/SVR_GroupFilter.cs
@@ -90,7 +90,7 @@ namespace Shoko.Server.Models
         {
             get
             {
-                if (_conditions.Count == 0 && GroupConditionsVersion == GROUPCONDITIONS_VERSION)
+                if (_conditions.Count == 0 && !string.IsNullOrEmpty(GroupConditions))
                 {
                     _conditions =
                         Newtonsoft.Json.JsonConvert.DeserializeObject<List<GroupFilterCondition>>(GroupConditions);
