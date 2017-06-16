c041120bb2b8a26cd9f2fb97015ff42268aa034d
da3dsoul <da3dsoul@gmail.com>
2017-06-16 08:57:55 -0400

    Make shared versioning and Naming amongst Shoko.UI and Shoko.Server

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyVersion("3.7.0.6")]
+[assembly: AssemblyFileVersion("3.7.0.6")]
+[assembly: AssemblyTitle("Shoko Server")]
+[assembly: AssemblyProduct("Shoko Server")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
