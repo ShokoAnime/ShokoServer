9b64f3e91b9c09553faed4278bec09d08b97ec27
Maximo Piva <maximo.piva@gmail.com>
2017-07-04 13:19:25 -0300

    Fixed Nugets. Update .NET Framework to 4.7 Added missing Enum StatCountType Added autoincrement versioning and change it to 3.8

---
+++ Enums/StatCountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoko.Models.Enums
+{
+    public enum StatCountType
+    {
+        Watched = 1,
+        Played = 2,
+        Stopped = 3
+    }
+}
