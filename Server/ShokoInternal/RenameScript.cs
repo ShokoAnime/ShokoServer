51ace98eeb527d818136eabe29f871fabee9472e
Cayde Dixon <me@cazzar.net>
2017-07-15 01:41:55 +1000

    Add extra data to the rename script

--- Server/ShokoInternal/RenameScript.cs
+++ Server/ShokoInternal/RenameScript.cs
@@ -1,4 +1,6 @@
-﻿namespace Shoko.Models.Server
+﻿using Microsoft.SqlServer.Server;
+
+namespace Shoko.Models.Server
 {
     public class RenameScript
     {
@@ -9,7 +11,8 @@
         public string ScriptName { get; set; }
         public string Script { get; set; }
         public int IsEnabledOnImport { get; set; }
-
+        public string RenamerType { get; set; }
+        public string ExtraData { get; set; }
 
     }
 }
