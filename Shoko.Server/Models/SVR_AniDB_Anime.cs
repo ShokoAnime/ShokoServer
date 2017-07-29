2cf42a67cc0c37f1f322179fa88271c404acc908
da3dsoul <da3dsoul@gmail.com>
2017-07-29 01:30:53 -0400

    Fix a minor logic issue for downloading relations

--- Shoko.Server/Models/SVR_AniDB_Anime.cs
+++ Shoko.Server/Models/SVR_AniDB_Anime.cs
@@ -1311,7 +1311,7 @@ ORDER BY count(DISTINCT AnimeID) DESC, Anime_GroupName ASC";
                 if (!anime_rel.Populate(rawrel)) continue;
                 relsToSave.Add(anime_rel);
 
-                if (downloadRelations && ServerSettings.AutoGroupSeries)
+                if (downloadRelations || ServerSettings.AutoGroupSeries)
                 {
                     logger.Info("Adding command to download related anime for {0} ({1}), related anime ID = {2}",
                         this.MainTitle, this.AnimeID, anime_rel.RelatedAnimeID);
