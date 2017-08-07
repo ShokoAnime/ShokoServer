42fdcd491d75479a544fd01c2ebf0c9652a1bf66
da3dsoul <da3dsoul@gmail.com>
2017-08-07 15:11:08 -0400

    Fix Sync_MyList

--- Shoko.Server/Commands/AniDB/CommandRequest_SyncMyList.cs
+++ Shoko.Server/Commands/AniDB/CommandRequest_SyncMyList.cs
@@ -102,15 +102,15 @@ namespace Shoko.Server.Commands
                     if (onlineFiles.ContainsKey(fileID))
                     {
                         Raw_AniDB_MyListFile file = onlineFiles[fileID];
-                        
+
                         // Update file state if deleted
-                        if (file.State == (int) AniDBFile_State.Deleted)
-                        {
-                            CommandRequest_UpdateMyListFileStatus cmdUpdateFile = 
-                                new CommandRequest_UpdateMyListFileStatus(vid.Hash, file.WatchedDate.HasValue, false, 0);
-                            cmdUpdateFile.Save();
-                        }
-                    };
+                        if (file.State != (int) AniDBFile_State.Deleted) continue;
+
+                        CommandRequest_UpdateMyListFileStatus cmdUpdateFile =
+                            new CommandRequest_UpdateMyListFileStatus(vid.Hash, file.WatchedDate.HasValue, false, 0);
+                        cmdUpdateFile.Save();
+                        continue;
+                    }
 
                     // means we have found a file in our local collection, which is not recorded online
                     CommandRequest_AddFileToMyList cmdAddFile = new CommandRequest_AddFileToMyList(vid.Hash);
