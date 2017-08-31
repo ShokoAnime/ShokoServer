6cfe30481bfa267148abc287132d471f858c6ce1
da3dsoul <da3dsoul@gmail.com>
2017-08-30 21:02:11 -0400

    Add info for New Series Form

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Shoko.Commons.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -8670,6 +8670,15 @@ namespace Shoko.Commons.Properties {
         public static string NewSeries_CreateNew {
             get {
                 return ResourceManager.GetString("NewSeries_CreateNew", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The group name will be generated from the anime title preferences..
+        /// </summary>
+        public static string NewSeries_NameInfo {
+            get {
+                return ResourceManager.GetString("NewSeries_NameInfo", resourceCulture);
             }
         }
         
