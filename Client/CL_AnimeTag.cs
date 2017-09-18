8d02caa9a53c5170dc72c5eb651b99d31750efd2
da3dsoul <da3dsoul@gmail.com>
2017-09-18 13:34:11 -0400

    missed a spot

--- Client/CL_AnimeTag.cs
+++ Client/CL_AnimeTag.cs
@@ -6,9 +6,21 @@
         //public int Spoiler { get; set; }
         public int LocalSpoiler { get; set; }
         public int GlobalSpoiler { get; set; }
-        public string TagName { get; set; }
-        //public int TagCount { get; set; }
-        public string TagDescription { get; set; }
         public int Weight { get; set; }
+
+        // Not really pretty, but much prettier than replacing the entire CL_ tree with VMs
+        private string tagName;
+        public string TagName
+        {
+            get => tagName == null ? null : string.Intern(tagName);
+            set => tagName = value == null ? null : string.Intern(value);
+        }
+
+        private string tagDescription;
+        public string TagDescription
+        {
+            get => tagDescription == null ? null : string.Intern(tagDescription);
+            set => tagDescription = value == null ? null : string.Intern(value);
+        }
     }
 }
