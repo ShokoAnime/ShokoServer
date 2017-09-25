0eaef8ea51b8ce6068fc81d58c715815d9c4bc8d
da3dsoul <da3dsoul@gmail.com>
2017-09-25 06:34:42 -0400

    Add database endpoints. Add ability to test sql connection.

---
+++ Shoko.Server/API/v2/Models/core/DatabaseSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace Shoko.Server.API.v2.Models.core
+{
+    public class DatabaseSettings
+    {
+        // case invariant, no spaces representation of db type
+        public string db_type { get; set; }
+
+        public string sqlserver_databaseserver { get; set; }
+
+        public string sqlserver_databasename { get; set; }
+
+        public string sqlserver_username { get; set; }
+
+        public string sqlserver_password { get; set; }
+
+        public string sqlite_databasefile { get; set; }
+
+        public string mysql_hostname { get; set; }
+
+        public string mysql_schemaname { get; set; }
+
+        public string mysql_username { get; set; }
+
+        public string mysql_password { get; set; }
+    }
+}
