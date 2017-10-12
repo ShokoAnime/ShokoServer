445f4d62535b6d3ded4b6dc365088b135dbe15e9
da3dsoul <da3dsoul@gmail.com>
2017-10-12 16:51:04 -0400

    Cleanup. Work on a crash involving #684

--- Shoko.Server/UI/ServerInfo.cs
+++ Shoko.Server/UI/ServerInfo.cs
@@ -4,13 +4,13 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Threading;
-
 using NutzCode.CloudFileSystem;
 using Shoko.Commons.Extensions;
 using Shoko.Commons.Notification;
+using Shoko.Commons.Properties;
 using Shoko.Models.Azure;
+using Shoko.Server.Commands;
 using Shoko.Server.Models;
-using Shoko.Server.Extensions;
 using Shoko.Server.Providers.Azure;
 using Shoko.Server.Repositories;
 
@@ -55,26 +55,14 @@ namespace Shoko.Server
         {
             //RefreshImportFolders();
 
-            ShokoService.CmdProcessorGeneral.OnQueueCountChangedEvent +=
-                new Commands.CommandProcessorGeneral.QueueCountChangedHandler(
-                    CmdProcessorGeneral_OnQueueCountChangedEvent);
-            ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent +=
-                new Commands.CommandProcessorGeneral.QueueStateChangedHandler(
-                    CmdProcessorGeneral_OnQueueStateChangedEvent);
+            ShokoService.CmdProcessorGeneral.OnQueueCountChangedEvent += CmdProcessorGeneral_OnQueueCountChangedEvent;
+            ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent += CmdProcessorGeneral_OnQueueStateChangedEvent;
 
-            ShokoService.CmdProcessorHasher.OnQueueCountChangedEvent +=
-                new Commands.CommandProcessorHasher.QueueCountChangedHandler(
-                    CmdProcessorHasher_OnQueueCountChangedEvent);
-            ShokoService.CmdProcessorHasher.OnQueueStateChangedEvent +=
-                new Commands.CommandProcessorHasher.QueueStateChangedHandler(
-                    CmdProcessorHasher_OnQueueStateChangedEvent);
+            ShokoService.CmdProcessorHasher.OnQueueCountChangedEvent += CmdProcessorHasher_OnQueueCountChangedEvent;
+            ShokoService.CmdProcessorHasher.OnQueueStateChangedEvent += CmdProcessorHasher_OnQueueStateChangedEvent;
 
-            ShokoService.CmdProcessorImages.OnQueueCountChangedEvent +=
-                new Commands.CommandProcessorImages.QueueCountChangedHandler(
-                    CmdProcessorImages_OnQueueCountChangedEvent);
-            ShokoService.CmdProcessorImages.OnQueueStateChangedEvent +=
-                new Commands.CommandProcessorImages.QueueStateChangedHandler(
-                    CmdProcessorImages_OnQueueStateChangedEvent);
+            ShokoService.CmdProcessorImages.OnQueueCountChangedEvent += CmdProcessorImages_OnQueueCountChangedEvent;
+            ShokoService.CmdProcessorImages.OnQueueStateChangedEvent += CmdProcessorImages_OnQueueStateChangedEvent;
 
 
             //Populate Cloud Providers
@@ -93,32 +81,32 @@ namespace Shoko.Server
             }
         }
 
-        void CmdProcessorImages_OnQueueStateChangedEvent(Commands.QueueStateEventArgs ev)
+        void CmdProcessorImages_OnQueueStateChangedEvent(QueueStateEventArgs ev)
         {
             ImagesQueueState = ev.QueueState.formatMessage();
         }
 
-        void CmdProcessorImages_OnQueueCountChangedEvent(Commands.QueueCountEventArgs ev)
+        void CmdProcessorImages_OnQueueCountChangedEvent(QueueCountEventArgs ev)
         {
             ImagesQueueCount = ev.QueueCount;
         }
 
-        void CmdProcessorHasher_OnQueueStateChangedEvent(Commands.QueueStateEventArgs ev)
+        void CmdProcessorHasher_OnQueueStateChangedEvent(QueueStateEventArgs ev)
         {
             HasherQueueState = ev.QueueState.formatMessage();
         }
 
-        void CmdProcessorHasher_OnQueueCountChangedEvent(Commands.QueueCountEventArgs ev)
+        void CmdProcessorHasher_OnQueueCountChangedEvent(QueueCountEventArgs ev)
         {
             HasherQueueCount = ev.QueueCount;
         }
 
-        void CmdProcessorGeneral_OnQueueStateChangedEvent(Commands.QueueStateEventArgs ev)
+        void CmdProcessorGeneral_OnQueueStateChangedEvent(QueueStateEventArgs ev)
         {
             GeneralQueueState = ev.QueueState.formatMessage();
         }
 
-        void CmdProcessorGeneral_OnQueueCountChangedEvent(Commands.QueueCountEventArgs ev)
+        void CmdProcessorGeneral_OnQueueCountChangedEvent(QueueCountEventArgs ev)
         {
             GeneralQueueCount = ev.QueueCount;
         }
@@ -155,143 +143,143 @@ namespace Shoko.Server
 
         public bool AdminMessagesAvailable
         {
-            get { return adminMessagesAvailable; }
-            set { this.SetField(() => adminMessagesAvailable, value); }
+            get => adminMessagesAvailable;
+            set => this.SetField(() => adminMessagesAvailable, value);
         }
 
         private int hasherQueueCount = 0;
 
         public int HasherQueueCount
         {
-            get { return hasherQueueCount; }
-            set { this.SetField(() => hasherQueueCount, value); }
+            get => hasherQueueCount;
+            set => this.SetField(() => hasherQueueCount, value);
         }
 
         private string hasherQueueState = string.Empty;
 
         public string HasherQueueState
         {
-            get { return hasherQueueState; }
-            set { this.SetField(() => hasherQueueState, value); }
+            get => hasherQueueState;
+            set => this.SetField(() => hasherQueueState, value);
         }
 
         private int imagesQueueCount = 0;
 
         public int ImagesQueueCount
         {
-            get { return imagesQueueCount; }
-            set { this.SetField(() => imagesQueueCount, value); }
+            get => imagesQueueCount;
+            set => this.SetField(() => imagesQueueCount, value);
         }
 
         private string imagesQueueState = string.Empty;
 
         public string ImagesQueueState
         {
-            get { return imagesQueueState; }
-            set { this.SetField(() => imagesQueueState, value); }
+            get => imagesQueueState;
+            set => this.SetField(() => imagesQueueState, value);
         }
 
         private int generalQueueCount = 0;
 
         public int GeneralQueueCount
         {
-            get { return generalQueueCount; }
-            set { this.SetField(() => generalQueueCount, value); }
+            get => generalQueueCount;
+            set => this.SetField(() => generalQueueCount, value);
         }
 
         private string generalQueueState = string.Empty;
 
         public string GeneralQueueState
         {
-            get { return generalQueueState; }
-            set { this.SetField(() => generalQueueState, value); }
+            get => generalQueueState;
+            set => this.SetField(() => generalQueueState, value);
         }
 
         private bool hasherQueuePaused = false;
 
         public bool HasherQueuePaused
         {
-            get { return hasherQueuePaused; }
-            set { this.SetField(() => hasherQueuePaused, value); }
+            get => hasherQueuePaused;
+            set => this.SetField(() => hasherQueuePaused, value);
         }
 
         private bool hasherQueueRunning = true;
 
         public bool HasherQueueRunning
         {
-            get { return hasherQueueRunning; }
-            set { this.SetField(() => hasherQueueRunning, value); }
+            get => hasherQueueRunning;
+            set => this.SetField(() => hasherQueueRunning, value);
         }
 
         private bool generalQueuePaused = false;
 
         public bool GeneralQueuePaused
         {
-            get { return generalQueuePaused; }
-            set { this.SetField(() => generalQueuePaused, value); }
+            get => generalQueuePaused;
+            set => this.SetField(() => generalQueuePaused, value);
         }
 
         private bool generalQueueRunning = true;
 
         public bool GeneralQueueRunning
         {
-            get { return generalQueueRunning; }
-            set { this.SetField(() => generalQueueRunning, value); }
+            get => generalQueueRunning;
+            set => this.SetField(() => generalQueueRunning, value);
         }
 
         private bool imagesQueuePaused = false;
 
         public bool ImagesQueuePaused
         {
-            get { return imagesQueuePaused; }
-            set { this.SetField(() => imagesQueuePaused, value); }
+            get => imagesQueuePaused;
+            set => this.SetField(() => imagesQueuePaused, value);
         }
 
         private bool imagesQueueRunning = true;
 
         public bool ImagesQueueRunning
         {
-            get { return imagesQueueRunning; }
-            set { this.SetField(() => imagesQueueRunning, value); }
+            get => imagesQueueRunning;
+            set => this.SetField(() => imagesQueueRunning, value);
         }
 
         private string banReason = string.Empty;
 
         public string BanReason
         {
-            get { return banReason; }
-            set { this.SetField(() => banReason, value); }
+            get => banReason;
+            set => this.SetField(() => banReason, value);
         }
 
         private string banOrigin = string.Empty;
 
         public string BanOrigin
         {
-            get { return banOrigin; }
-            set { this.SetField(() => banOrigin, value); }
+            get => banOrigin;
+            set => this.SetField(() => banOrigin, value);
         }
 
         private bool isBanned = false;
 
         public bool IsBanned
         {
-            get { return isBanned; }
-            set { this.SetField(() => isBanned, value); }
+            get => isBanned;
+            set => this.SetField(() => isBanned, value);
         }
 
         private bool isInvalidSession = false;
 
         public bool IsInvalidSession
         {
-            get { return isInvalidSession; }
-            set { this.SetField(() => isInvalidSession, value); }
+            get => isInvalidSession;
+            set => this.SetField(() => isInvalidSession, value);
         }
 
         private bool waitingOnResponseAniDBUDP = false;
 
         public bool WaitingOnResponseAniDBUDP
         {
-            get { return waitingOnResponseAniDBUDP; }
+            get => waitingOnResponseAniDBUDP;
             set
             {
                 this.SetField(() => waitingOnResponseAniDBUDP, value);
@@ -303,11 +291,11 @@ namespace Shoko.Server
 
         public bool NotWaitingOnResponseAniDBUDP
         {
-            get { return notWaitingOnResponseAniDBUDP; }
-            set { this.SetField(() => notWaitingOnResponseAniDBUDP, value); }
+            get => notWaitingOnResponseAniDBUDP;
+            set => this.SetField(() => notWaitingOnResponseAniDBUDP, value);
         }
 
-        private string waitingOnResponseAniDBUDPString = Commons.Properties.Resources.Command_Idle;
+        private string waitingOnResponseAniDBUDPString = Resources.Command_Idle;
 
         public string WaitingOnResponseAniDBUDPString
         {
@@ -315,26 +303,26 @@ namespace Shoko.Server
             {
                 Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(ServerSettings.Culture);
 
-                waitingOnResponseAniDBUDPString = Commons.Properties.Resources.Command_Idle;
+                waitingOnResponseAniDBUDPString = Resources.Command_Idle;
                 return waitingOnResponseAniDBUDPString;
             }
-            set { this.SetField(() => waitingOnResponseAniDBUDPString, value); }
+            set => this.SetField(() => waitingOnResponseAniDBUDPString, value);
         }
 
         private string extendedPauseString = string.Empty;
 
         public string ExtendedPauseString
         {
-            get { return extendedPauseString; }
-            set { this.SetField(() => extendedPauseString, value); }
+            get => extendedPauseString;
+            set => this.SetField(() => extendedPauseString, value);
         }
 
         private bool hasExtendedPause = false;
 
         public bool HasExtendedPause
         {
-            get { return hasExtendedPause; }
-            set { this.SetField(() => hasExtendedPause, value); }
+            get => hasExtendedPause;
+            set => this.SetField(() => hasExtendedPause, value);
         }
 
         public ObservableCollection<SVR_ImportFolder> ImportFolders { get; set; }
@@ -355,9 +343,9 @@ namespace Shoko.Server
 
         public void RefreshImportFolders()
         {
-            ImportFolders.Clear();
             try
             {
+                ImportFolders.Clear();
                 RepoFactory.ImportFolder.GetAll().ForEach(a => ImportFolders.Add(a));
             }
             catch (Exception ex)
@@ -368,9 +356,9 @@ namespace Shoko.Server
 
         public void RefreshCloudAccounts()
         {
-            CloudAccounts.Clear();
             try
             {
+                CloudAccounts.Clear();
                 RepoFactory.CloudAccount.GetAll().ForEach(a => CloudAccounts.Add(a));
             }
             catch (Exception ex)
@@ -381,9 +369,16 @@ namespace Shoko.Server
 
         public void RefreshFolderProviders()
         {
-            FolderProviders.Clear();
-            FolderProviders.Add(SVR_CloudAccount.CreateLocalFileSystemAccount());
-            RepoFactory.CloudAccount.GetAll().ForEach(a => FolderProviders.Add(a));
+            try
+            {
+                FolderProviders.Clear();
+                FolderProviders.Add(SVR_CloudAccount.CreateLocalFileSystemAccount());
+                RepoFactory.CloudAccount.GetAll().ForEach(a => FolderProviders.Add(a));
+            }
+            catch (Exception ex)
+            {
+                Utils.ShowErrorMessage(ex);
+            }
         }
 
         #endregion
