bdeec1a6cbcd9fde87912462112b9deb7099554d
da3dsoul <da3dsoul@gmail.com>
2017-10-14 19:18:52 -0400

    Locks on VideoLocalRepository. I missed it apparently

--- Shoko.Server/Repositories/Cached/VideoLocalRepository.cs
+++ Shoko.Server/Repositories/Cached/VideoLocalRepository.cs
@@ -262,59 +262,80 @@ namespace Shoko.Server.Repositories.Cached
 
         public SVR_VideoLocal GetByHash(string hash)
         {
-            return Hashes.GetOne(hash);
+            lock (Cache)
+            {
+                return Hashes.GetOne(hash);
+            }
         }
 
         public SVR_VideoLocal GetByMD5(string hash)
         {
-            return MD5.GetOne(hash);
+            lock (Cache)
+            {
+                return MD5.GetOne(hash);
+            }
         }
 
         public SVR_VideoLocal GetBySHA1(string hash)
         {
-            return SHA1.GetOne(hash);
+            lock (Cache)
+            {
+                return SHA1.GetOne(hash);
+            }
         }
 
         public long GetTotalRecordCount()
         {
-            return Cache.Keys.Count;
+            lock (Cache)
+            {
+                return Cache.Keys.Count;
+            }
         }
 
         public SVR_VideoLocal GetByHashAndSize(string hash, long fsize)
         {
-            return Hashes.GetMultiple(hash).FirstOrDefault(a => a.FileSize == fsize);
+            lock (Cache)
+            {
+                return Hashes.GetMultiple(hash).FirstOrDefault(a => a.FileSize == fsize);
+            }
         }
 
         public List<SVR_VideoLocal> GetByName(string fileName)
         {
-            //return Paths.GetMultiple(fileName);
-            //return Cache.Values.Where(store => store.FilePath.Contains(fileName, StringComparison.InvariantCultureIgnoreCase)).ToList();
-
-            return Cache.Values.Where(p => p.Places.Any(
-                    a => a.FilePath.Contains(fileName, StringComparison.InvariantCultureIgnoreCase)))
-                .ToList();
+            lock (Cache)
+            {
+                return Cache.Values.Where(p => p.Places.Any(
+                        a => a.FilePath.Contains(fileName, StringComparison.InvariantCultureIgnoreCase)))
+                    .ToList();
+            }
         }
 
         public List<SVR_VideoLocal> GetMostRecentlyAdded(int maxResults)
         {
-            if (maxResults == -1)
-                return Cache.Values.OrderByDescending(a => a.DateTimeCreated).ToList();
-            return Cache.Values.OrderByDescending(a => a.DateTimeCreated).Take(maxResults).ToList();
+            lock (Cache)
+            {
+                if (maxResults == -1)
+                    return Cache.Values.OrderByDescending(a => a.DateTimeCreated).ToList();
+                return Cache.Values.OrderByDescending(a => a.DateTimeCreated).Take(maxResults).ToList();
+            }
         }
 
         public List<SVR_VideoLocal> GetRandomFiles(int maxResults)
         {
-            IEnumerator<int> en = new UniqueRandoms(0, Cache.Values.Count - 1).GetEnumerator();
-            List<SVR_VideoLocal> vids = new List<SVR_VideoLocal>();
-            if (maxResults > Cache.Values.Count)
-                maxResults = Cache.Values.Count;
-            for (int x = 0; x < maxResults; x++)
+            lock (Cache)
             {
-                en.MoveNext();
-                vids.Add(Cache.Values.ElementAt(en.Current));
+                IEnumerator<int> en = new UniqueRandoms(0, Cache.Values.Count - 1).GetEnumerator();
+                List<SVR_VideoLocal> vids = new List<SVR_VideoLocal>();
+                if (maxResults > Cache.Values.Count)
+                    maxResults = Cache.Values.Count;
+                for (int x = 0; x < maxResults; x++)
+                {
+                    en.MoveNext();
+                    vids.Add(Cache.Values.ElementAt(en.Current));
+                }
+                en.Dispose();
+                return vids;
             }
-            en.Dispose();
-            return vids;
         }
 
         public class UniqueRandoms : IEnumerable<int>
@@ -362,12 +383,6 @@ namespace Shoko.Server.Repositories.Cached
                 .Select(a => GetByHash(a.Hash))
                 .Where(a => a != null)
                 .ToList();
-            /*
-            return
-                session.CreateQuery(
-                    "Select vl.VideoLocalID FROM VideoLocal as vl, CrossRef_File_Episode as xref WHERE vl.Hash = xref.Hash AND xref.EpisodeID= :episodeid")
-                    .SetParameter("episodeid", episodeID)
-                    .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();*/
         }
 
 
@@ -380,13 +395,6 @@ namespace Shoko.Server.Repositories.Cached
                     .OrderByDescending(a => a.DateTimeCreated)
                     .Take(maxResults)
                     .ToList();
-            /*
-                        return
-                            session.CreateQuery(
-                                "Select vl.VideoLocalID FROM VideoLocal as vl, CrossRef_File_Episode as xref WHERE vl.Hash = xref.Hash AND xref.AnimeID= :animeid ORDER BY vl.DateTimeCreated Desc")
-                                .SetParameter("animeid", animeID)
-                                .SetMaxResults(maxResults)
-                                .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();*/
         }
 
 
@@ -396,15 +404,6 @@ namespace Shoko.Server.Repositories.Cached
                 .Select(a => GetByHash(a.Hash))
                 .Where(a => a != null)
                 .ToList();
-            /*
-            using (var session = JMMService.SessionFactory.OpenSession())
-            {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal as vl, AniDB_File as xref WHERE vl.Hash = xref.Hash AND vl.FileSize = xref.FileSize AND xref.File_VideoResolution= :fileres")
-                        .SetParameter("fileres", res)
-                        .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();
-            }*/
         }
 
         public List<SVR_VideoLocal> GetByInternalVersion(int iver)
@@ -413,15 +412,6 @@ namespace Shoko.Server.Repositories.Cached
                 .Select(a => GetByHash(a.Hash))
                 .Where(a => a != null)
                 .ToList();
-            /*
-            using (var session = JMMService.SessionFactory.OpenSession())
-            {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal as vl, AniDB_File as xref WHERE vl.Hash = xref.Hash AND vl.FileSize = xref.FileSize AND xref.InternalVersion= :iver")
-                        .SetParameter("iver", iver)
-                        .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();
-            }*/
         }
 
         public List<SVR_VideoLocal> GetWithMissingChapters()
@@ -430,15 +420,6 @@ namespace Shoko.Server.Repositories.Cached
                 .Select(a => GetByHash(a.Hash))
                 .Where(a => a != null)
                 .ToList();
-            /*
-            using (var session = JMMService.SessionFactory.OpenSession())
-            {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal as vl, AniDB_File as xref WHERE vl.Hash = xref.Hash AND vl.FileSize = xref.FileSize AND xref.InternalVersion= :iver")
-                        .SetParameter("iver", iver)
-                        .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();
-            }*/
         }
 
         /// <summary>
@@ -453,82 +434,40 @@ namespace Shoko.Server.Repositories.Cached
                     .Select(a => GetByHash(a.Hash))
                     .Where(a => a != null)
                     .ToList();
-            /*
-            return
-                session.CreateQuery(
-                    "Select vl.VideoLocalID FROM VideoLocal as vl, CrossRef_File_Episode as xref WHERE vl.Hash = xref.Hash AND xref.AnimeID= :animeID")
-                    .SetParameter("animeID", animeID)
-                    .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();*/
         }
 
-
-        /*
-        public List<VideoLocal> GetVideosWithoutImportFolder()
-        {
-            using (var session = JMMService.SessionFactory.OpenSession())
-            {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal vl WHERE vl.ImportFolderID NOT IN (select ImportFolderID from ImportFolder fldr)")
-                        .List<int>().Select(a => Cache.Get(a)).Where(a => a != null).ToList();
-            }
-        }
-        */
         public List<SVR_VideoLocal> GetVideosWithoutHash()
         {
-            return Hashes.GetMultiple("").ToList();
+            lock (Cache)
+            {
+                return Hashes.GetMultiple("").ToList();
+            }
         }
 
         public List<SVR_VideoLocal> GetVideosWithoutVideoInfo()
         {
-            return Cache.Values.Where(a => a.Media == null || a.MediaVersion < SVR_VideoLocal.MEDIA_VERSION ||
-                                           a.Duration == 0)
-                .ToList();
+            lock (Cache)
+            {
+                return Cache.Values.Where(a => a.Media == null || a.MediaVersion < SVR_VideoLocal.MEDIA_VERSION ||
+                                               a.Duration == 0)
+                    .ToList();
+            }
         }
 
         public List<SVR_VideoLocal> GetVideosWithoutEpisode()
         {
-            HashSet<string> hashes = new HashSet<string>(RepoFactory.CrossRef_File_Episode.GetAll()
-                .Select(a => a.Hash));
-            HashSet<string> vlocals = new HashSet<string>(Cache.Values.Where(a => a.IsIgnored == 0)
-                .Select(a => a.Hash));
-            return vlocals.Except(hashes)
-                .SelectMany(a => Hashes.GetMultiple(a))
-                .OrderBy(a => a.DateTimeCreated)
-                .ToList();
-
-            /*
-            using (var session = JMMService.SessionFactory.OpenSession())
+            lock (Cache)
             {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal vl WHERE vl.Hash NOT IN (Select Hash FROM CrossRef_File_Episode xref) AND vl.IsIgnored = 0")
-                        .List<int>()
-                        .Select(a => Cache.Get(a))
-                        .Where(a => a != null)
-                        .OrderBy(a => a.DateTimeCreated)
-                        .ToList();
-            }*/
+                HashSet<string> hashes = new HashSet<string>(RepoFactory.CrossRef_File_Episode.GetAll()
+                    .Select(a => a.Hash));
+                HashSet<string> vlocals = new HashSet<string>(Cache.Values.Where(a => a.IsIgnored == 0)
+                    .Select(a => a.Hash));
+                return vlocals.Except(hashes)
+                    .SelectMany(a => Hashes.GetMultiple(a))
+                    .OrderBy(a => a.DateTimeCreated)
+                    .ToList();
+            }
         }
-
-        // This is impossible with current db, as there is no record of what used added file and VideoLocalUser store only usere related data about known eps
-        //public List<VideoLocal> GetVideosWithoutEpisode(int user_id)
-        //{
-        // get all vl_id for user
-        // HashSet<int> user_files = new HashSet<int>(RepoFactory.VideoLocalUser.GetAll().Where(a => a.JMMUserID == user_id).Select(a => a.VideoLocalID));
-        // get all hashes for user knowing its vl_id
-        // HashSet<string> vlocals = new HashSet<string>(Cache.Values.Where(a => user_files.Contains(a.VideoLocalID)).Where(a => a.IsIgnored == 0).Select(a => a.Hash));
-        // get all recognized episode hashes
-        // HashSet<string> hashes = new HashSet<string>(RepoFactory.CrossRef_File_Episode.GetAll().Select(a => a.Hash));
-        // substract all recognized episode hashes from all hashes for user
-        // return vlocals.Except(hashes).SelectMany(a => Hashes.GetMultiple(a)).OrderBy(a => a.DateTimeCreated).ToList();
-        // HashSet<string> ret = new HashSet<string>();
-        //foreach (string x in vlocals)
-        //{
-        //    if (!hashes.Contains(x)) { ret.Add(x); }
-        //}
-        //return ret.SelectMany(a => Hashes.GetMultiple(a)).OrderBy(a => a.DateTimeCreated).ToList();
-        //}
 
         public List<SVR_VideoLocal> GetManuallyLinkedVideos()
         {
@@ -538,24 +477,14 @@ namespace Shoko.Server.Repositories.Cached
                     .Select(a => GetByHash(a.Hash))
                     .Where(a => a != null)
                     .ToList();
-
-            /*
-            using (var session = JMMService.SessionFactory.OpenSession())
-            {
-                return
-                    session.CreateQuery(
-                        "Select vl.VideoLocalID FROM VideoLocal vl WHERE vl.Hash IN (Select Hash FROM CrossRef_File_Episode xref WHERE xref.CrossRefSource <> 1)")
-                        .List<int>()
-                        .Select(a => Cache.Get(a))
-                        .Where(a => a != null)
-                        .OrderBy(a => a.DateTimeCreated)
-                        .ToList();
-            }*/
         }
 
         public List<SVR_VideoLocal> GetIgnoredVideos()
         {
-            return Ignored.GetMultiple(1);
+            lock (Cache)
+            {
+                return Ignored.GetMultiple(1);
+            }
         }
     }
 }
