bfd8243286cda0fab00dd96840f56d06d6edd0da
Cayde Dixon <me@cazzar.net>
2017-10-26 17:42:24 +1100

    Add a version of GetActivator for compiled methods

--- Shoko.Server/Utilities/ReflectionUtils.cs
+++ Shoko.Server/Utilities/ReflectionUtils.cs
@@ -11,6 +11,7 @@ namespace Shoko.Server.Utilities
     internal class ReflectionUtils
     {
         public delegate T ObjectActivator<T>(params object[] args);
+        public delegate T ObjectMethodActivator<T>(object instance, params object[] args);
         public static ObjectActivator<T> GetActivator<T>
             (ConstructorInfo ctor)
         {
@@ -18,11 +19,8 @@ namespace Shoko.Server.Utilities
             ParameterInfo[] paramsInfo = ctor.GetParameters();
 
             //create a single param of type object[]
-            ParameterExpression param =
-                Expression.Parameter(typeof(object[]), "args");
-
-            Expression[] argsExp =
-                new Expression[paramsInfo.Length];
+            ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
+            Expression[] argsExp = new Expression[paramsInfo.Length];
 
             //pick each arg from the params array 
             //and create a typed expression of them
@@ -30,12 +28,8 @@ namespace Shoko.Server.Utilities
             {
                 Expression index = Expression.Constant(i);
                 Type paramType = paramsInfo[i].ParameterType;
-
-                Expression paramAccessorExp =
-                    Expression.ArrayIndex(param, index);
-
-                Expression paramCastExp =
-                    Expression.Convert(paramAccessorExp, paramType);
+                Expression paramAccessorExp = Expression.ArrayIndex(param, index);
+                Expression paramCastExp = Expression.Convert(paramAccessorExp, paramType);
 
                 argsExp[i] = paramCastExp;
             }
@@ -46,12 +40,33 @@ namespace Shoko.Server.Utilities
 
             //create a lambda with the New
             //Expression as body and our param object[] as arg
-            LambdaExpression lambda =
-                Expression.Lambda(typeof(ObjectActivator<T>), newExp, param);
+            LambdaExpression lambda = Expression.Lambda(typeof(ObjectActivator<T>), newExp, param);
 
             //compile it
             ObjectActivator<T> compiled = (ObjectActivator<T>)lambda.Compile();
             return compiled;
         }
+
+        public static ObjectMethodActivator<T> GetMethodActivator<T>(MethodInfo mtd)
+        {
+            Type type = mtd.DeclaringType;
+            ParameterInfo[] paramInfos = mtd.GetParameters();
+            ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
+            Expression[] argsExp = new Expression[paramInfos.Length];
+
+            for (int i = 0; i < paramInfos.Length; i++)
+            {
+                var parameterType = paramInfos[i].ParameterType;
+                argsExp[i] = Expression.Convert(Expression.ArrayIndex(param, Expression.Constant(i)), parameterType);
+            }
+            Expression instanceExp = Expression.Parameter(type, "instance");
+            MethodCallExpression mtdExp = Expression.Call(instanceExp, mtd, argsExp);
+            LambdaExpression lambda = Expression.Lambda<ObjectMethodActivator<T>>(mtdExp, param);
+            return (ObjectMethodActivator<T>) lambda.Compile();
+        }
+
+        public static ObjectMethodActivator<T> GetMethodActivator<T>(Type type, string methodName) =>
+            GetMethodActivator<T>(type.GetMethod(methodName));
+
     }
 }
