ab757883bf2d2f167c00124580c78fa72019df62
da3dsoul <da3dsoul@gmail.com>
2017-10-27 07:45:23 -0400

    Hopefully fix nullpointer in AsyncObservableCollection.

--- Shoko.Server/Utilities/AsyncObservableCollection.cs
+++ Shoko.Server/Utilities/AsyncObservableCollection.cs
@@ -8,19 +8,21 @@ namespace Shoko.Server
 {
     public class AsyncObservableCollection<T> : ObservableCollection<T>
     {
-        private readonly SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+        private readonly SynchronizationContext _synchronizationContext;
 
         public AsyncObservableCollection()
         {
+            _synchronizationContext = SynchronizationContext.Current;
         }
 
         public AsyncObservableCollection(IEnumerable<T> list) : base(list)
         {
+            _synchronizationContext = SynchronizationContext.Current;
         }
 
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            if (SynchronizationContext.Current == _synchronizationContext)
+            if (_synchronizationContext == null || SynchronizationContext.Current == _synchronizationContext)
                 RaiseCollectionChanged(e);
             else
                 _synchronizationContext.Send(RaiseCollectionChanged, e);
@@ -33,7 +35,7 @@ namespace Shoko.Server
 
         protected override void OnPropertyChanged(PropertyChangedEventArgs e)
         {
-            if (SynchronizationContext.Current == _synchronizationContext)
+            if (_synchronizationContext == null || SynchronizationContext.Current == _synchronizationContext)
                 RaisePropertyChanged(e);
             else
                 _synchronizationContext.Send(RaisePropertyChanged, e);
