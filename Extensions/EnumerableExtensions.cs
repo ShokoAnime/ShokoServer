a7a7c1e630de527fcd901b29ee586630d7a4f116
da3dsoul <da3dsoul@gmail.com>
2018-01-28 00:47:20 -0500

    Allow null selected items in OrderByNatural, but concat them to the end

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -24,12 +24,17 @@ namespace Shoko.Commons.Extensions
         public static IEnumerable<T> OrderByNatural<T>(this IEnumerable<T> items, Func<T, string> selector, StringComparer stringComparer = null)
         {
             var regex = new Regex(@"\d+", RegexOptions.Compiled);
+            // all not null selected items
+            var result = items.Where(i => selector(i) != null);
 
-            int maxDigits = items
-                                .SelectMany(i => regex.Matches(selector(i)).Cast<Match>().Select(digitChunk => (int?)digitChunk.Value.Length))
+            // process numbers
+            int maxDigits = result.SelectMany(i => regex.Matches(selector(i)).Cast<Match>().Select(digitChunk => (int?)digitChunk.Value.Length))
                                 .Max() ?? 0;
 
-            return items.OrderBy(i => regex.Replace(selector(i), match => match.Value.PadLeft(maxDigits, '0')), stringComparer ?? StringComparer.CurrentCulture);
+            // pad numbers, order by, then concatenate the null selected items to the end
+            return result
+                .OrderBy(i => regex.Replace(selector(i), match => match.Value.PadLeft(maxDigits, '0')),
+                    stringComparer ?? StringComparer.CurrentCulture).Concat(items.Where(i => selector(i) == null));
         }
 
         public static string ToRanges(this List<int> ints) {
