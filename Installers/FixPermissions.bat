fa69c1092ce10d708e58e2a10ffa446ae76b1122
Unknown <richard@richardvanhorn.com>
2018-02-16 13:19:12 -0800

    Add Advanced Installer install file.

--- Installers/FixPermissions.bat
+++ Installers/FixPermissions.bat
@@ -1,6 +1,21 @@
-netsh http add urlacl url=http://+:8111/JMMServerPlex sddl=D:(A;;GA;;;S-1-1-0)
-netsh http add urlacl url=http://+:8111/JMMServerStreaming sddl=D:(A;;GA;;;S-1-1-0)
-netsh http add urlacl url=http://+:8111/JMMServerImage sddl=D:(A;;GA;;;S-1-1-0)
-netsh http add urlacl url=http://+:8111/ sddl=D:(A;;GA;;;S-1-1-0)
-icacls C:\ProgramData\ShokoServer /inheritance:r
-icacls C:\ProgramData\ShokoServer /grant *S-1-1-0:(OI)(CI)F /T /inheritance:e
+@echo off
+title Setting Shoko Server Permissions
+
+if exist "C:\ProgramData\ShokoServer" (
+  echo Shoko Server folder found, no need to set permissions.
+)
+  
+if not exist "C:\ProgramData\ShokoServer" (
+  netsh advfirewall firewall add rule name="Shoko Server Port" dir=in action=allow protocol=TCP localport=8111
+  netsh http add urlacl url=http://+:8111/JMMServerPlex sddl=D:(A;;GA;;;S-1-1-0)
+  netsh http add urlacl url=http://+:8111/JMMServerStreaming sddl=D:(A;;GA;;;S-1-1-0)
+  netsh http add urlacl url=http://+:8111/JMMServerImage sddl=D:(A;;GA;;;S-1-1-0)
+  netsh http add urlacl url=http://+:8111/ sddl=D:(A;;GA;;;S-1-1-0)
+  mkdir C:\ProgramData\ShokoServer	
+  icacls C:\ProgramData\ShokoServer /inheritance:r
+  icacls C:\ProgramData\ShokoServer /grant *S-1-1-0:(OI)(CI)F /T /inheritance:e
+)
+
+echo.
+echo.
+echo Shoko Server permissions correctly applied.  
