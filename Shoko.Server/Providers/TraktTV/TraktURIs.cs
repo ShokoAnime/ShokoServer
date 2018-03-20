61013645260b704b574b538423bd2c6feab8e2b6
da3dsoul <da3dsoul@gmail.com>
2018-03-20 18:40:17 -0400

    Fix an issue with Trakt

--- Shoko.Server/Providers/TraktTV/TraktURIs.cs
+++ Shoko.Server/Providers/TraktTV/TraktURIs.cs
@@ -30,11 +30,11 @@
 
         // http://docs.trakt.apiary.io/#reference/shows/summary/get-a-single-show
         // {0} trakt ID, trakt slug, or IMDB ID Example: game-of-thrones
-        public const string ShowSummary = TraktConstants.BaseAPIURL + @"/shows/{0}?extended=full,images";
+        public const string ShowSummary = TraktConstants.BaseAPIURL + @"/shows/{0}?extended=full";
 
         // http://docs.trakt.apiary.io/#reference/seasons/summary/get-all-seasons-for-a-show
         // {0} trakt ID, trakt slug, or IMDB ID Example: game-of-thrones
-        public const string ShowSeasons = TraktConstants.BaseAPIURL + @"/shows/{0}/seasons?extended=episodes,images";
+        public const string ShowSeasons = TraktConstants.BaseAPIURL + @"/shows/{0}/seasons?extended=episodes";
 
         // get comments for a show
         // http://docs.trakt.apiary.io/#reference/shows/watched-progress/get-all-show-comments
