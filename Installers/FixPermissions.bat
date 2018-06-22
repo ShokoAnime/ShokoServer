c34eadfd61b3621258410fe7ea418c810fde77e3
Unknown <richard@richardvanhorn.com>
2018-06-21 21:25:04 -0700

    Update FixPermissions.bat.

--- Installers/FixPermissions.bat
+++ Installers/FixPermissions.bat
@@ -3,6 +3,7 @@ title Setting Shoko Server Permissions
 
 if exist "%ProgramData%\ShokoServer" (
   echo Shoko Server folder found, no need to set permissions.
+  exit 0
 ) else (
   netsh advfirewall firewall add rule name="Shoko Server Port" dir=in action=allow protocol=TCP localport=8111
   netsh http add urlacl url=http://+:8111/JMMServerPlex sddl=D:(A;;GA;;;S-1-1-0)
@@ -12,8 +13,10 @@ if exist "%ProgramData%\ShokoServer" (
   mkdir  "%ProgramData%\ShokoServer"
   icacls "%ProgramData%\ShokoServer" /inheritance:r
   icacls "%ProgramData%\ShokoServer" /grant *S-1-1-0:(OI)(CI)F /T /inheritance:e
+  exit 0
 )
 
 echo.
 echo.
 echo Shoko Server permissions correctly applied.
+exit 0
