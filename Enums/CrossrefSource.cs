7326dee9c069f48a4872278d640b117bf145e394
da3dsoul <da3dsoul@gmail.com>
2018-06-29 02:32:28 -0400

    Add a value to Match Rating

--- Enums/CrossrefSource.cs
+++ Enums/CrossrefSource.cs
@@ -13,9 +13,10 @@ namespace Shoko.Models.Enums
     public enum MatchRating
     {
         UserVerified = 1,
-        Good = 2,
-        Bad = 3,
-        Ugly = 4,
-        SarahJessicaParker = 5, // http://southpark.wikia.com/wiki/Sarah_Jessica_Parker
+        Good = 2, // Dates and Titles match, give or take
+        Mkay = 3, // Dates Matched
+        Bad = 4, // Titles Matched
+        Ugly = 5, // Neither Matched, but we could fill from adjacent episodes
+        SarahJessicaParker = 6, // http://southpark.wikia.com/wiki/Sarah_Jessica_Parker
     }
 }
