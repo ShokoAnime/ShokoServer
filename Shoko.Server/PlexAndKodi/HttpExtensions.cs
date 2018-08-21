67a057dc6d59575e1e657defec1545a2bf63014b
Cayde Dixon <me@cazzar.net>
2018-08-21 18:53:33 +1000

    Hopefully this fixes AppVeyor

--- Shoko.Server/PlexAndKodi/HttpExtensions.cs
+++ Shoko.Server/PlexAndKodi/HttpExtensions.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Formatting;
-using Nancy;
 using Shoko.Server.PlexAndKodi.Plex;
 using UPnP;
 
