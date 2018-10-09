0bb613edbffa1f1b458a9612136a1ddfeffde25b
da3dsoul <da3dsoul@gmail.com>
2018-10-09 19:38:28 -0400

    Cleanup and Match Versions of NLog

--- Shoko.CLI/Program.cs
+++ Shoko.CLI/Program.cs
@@ -1,11 +1,6 @@
 ﻿using Shoko.Server;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NLog;
-using Shoko.Server.Commands;
 
 namespace Shoko.CLI
 {
