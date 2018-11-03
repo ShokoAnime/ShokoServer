67fac9fc1f7f7521535eac4254c93763c206df15
da3dsoul <da3dsoul@gmail.com>
2018-11-02 20:44:46 -0400

    Fix UTC conversion on AddToMyList and UpdateMyList

--- Shoko.Server/Models/SVR_VideoLocal.cs
+++ Shoko.Server/Models/SVR_VideoLocal.cs
@@ -269,7 +269,7 @@ namespace Shoko.Server.Models
                     {
                         CommandRequest_UpdateMyListFileStatus cmd = new CommandRequest_UpdateMyListFileStatus(
                             Hash, watched, false,
-                            watchedDate.HasValue ? AniDB.GetAniDBDateAsSeconds(watchedDate) : 0);
+                            AniDB.GetAniDBDateAsSeconds(watchedDate?.ToUniversalTime()));
                         cmd.Save();
                     }
             }
