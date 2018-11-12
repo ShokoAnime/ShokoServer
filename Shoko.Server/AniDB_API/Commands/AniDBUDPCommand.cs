379ebda656840901bb2194deaa166aa42aa15fd4
Cayde Dixon <me@cazzar.net>
2018-11-13 03:16:32 +1100

    Fix Deflate compression from anidb api

--- Shoko.Server/AniDB_API/Commands/AniDBUDPCommand.cs
+++ Shoko.Server/AniDB_API/Commands/AniDBUDPCommand.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.IO;
-using System.IO.Compression;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using Shoko.Server;
 using NLog;
 using Shoko.Server.AniDB_API;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 
 namespace AniDBAPI.Commands
 {
@@ -136,15 +135,14 @@ namespace AniDBAPI.Commands
                         if ((received > 2) && (byReceivedAdd[0] == 0) && (byReceivedAdd[1] == 0))
                         {
                             //deflate
-                            Byte[] input = new byte[received - 2];
+                            var buff = new byte[65536];
+                            var input = new byte[received - 2];
                             Array.Copy(byReceivedAdd, 2, input, 0, received - 2);
-                            MemoryStream mstream=new MemoryStream(input);
-                            MemoryStream output=new MemoryStream();
-                            mstream.Position = 0;
-                            DeflateStream stream = new DeflateStream(mstream, CompressionMode.Decompress);
-                            stream.CopyTo(output);
-                            byReceivedAdd = output.ToArray();
-                            received = (int) byReceivedAdd.Length;
+                            var inf = new Inflater(false);
+                            inf.SetInput(input);
+                            inf.Inflate(buff);
+                            byReceivedAdd = buff;
+                            received = (int)inf.TotalOut;
                         }
                     }
                     catch (SocketException sex)
