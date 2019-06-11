72c8cb142e9c38ea53f8f1b9af85683a85540f81
da3dsoul <da3dsoul@gmail.com>
2019-06-11 02:43:30 -0400

    Fix a bunch of stuff. - Remove StreamWithResponse. It may be reimplemented better later if needed. - Fix some bugs with playback. - Improve Desktop's image loading. - Fix ApiInUse and make it reliable.

--- Interfaces/IShokoServerImage.cs
+++ Interfaces/IShokoServerImage.cs
@@ -8,19 +8,19 @@ namespace Shoko.Models.Interfaces
     public interface IShokoServerImage
     {
         [Rest("{imageid}/{imageType}/{thumnbnailOnly?}", Verbs.Get)]
-        Stream GetImage(int imageid, int imageType, bool? thumnbnailOnly);
+        object GetImage(int imageid, int imageType, bool? thumnbnailOnly);
 
         [Rest("WithPath/{serverImagePath}", Verbs.Get)]
-        Stream GetImageUsingPath(string serverImagePath);
+        object GetImageUsingPath(string serverImagePath);
 
         [Rest("Blank", Verbs.Get)]
-        Stream BlankImage();
+        object BlankImage();
 
         [Rest("Support/{name}/{ratio}", Verbs.Get)]
-        Stream GetSupportImage(string name, float? ratio);
+        object GetSupportImage(string name, float? ratio);
 
         [Rest("Thumb/{imageId}/{imageType}/{ratio}", Verbs.Get)]
-        Stream GetThumb(int imageId, int imageType, float ratio);
+        object GetThumb(int imageId, int imageType, float ratio);
 
         [Rest("Path/{imageId}/{imageType}/{thumnbnailOnly?}", Verbs.Get)]
         string GetImagePath(int imageId, int imageType, bool? thumnbnailOnly);
