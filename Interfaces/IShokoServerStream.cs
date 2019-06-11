ef9174530f8247cd08ba435026f626766b9be133
da3dsoul <da3dsoul@gmail.com>
2019-06-11 02:00:33 -0400

    Make Stream Use object instead of Stream. Prepare for better handling.

--- Interfaces/IShokoServerStream.cs
+++ Interfaces/IShokoServerStream.cs
@@ -7,13 +7,13 @@ namespace Shoko.Models.Interfaces
     public interface IShokoServerStream
     {
         [Rest("/{videolocalid}/{userId?}/{autowatch?}/{fakename?}", Verbs.Get)]
-        System.IO.Stream StreamVideo(int videolocalid, int? userId, bool? autowatch, string fakename);
+        object StreamVideo(int videolocalid, int? userId, bool? autowatch, string fakename);
         [Rest("Filename/{base64filename}/{userId?}/{autowatch?}/{fakename?}", Verbs.Get)]
-        System.IO.Stream StreamVideoFromFilename(string base64filename, int? userId, bool? autowatch, string fakename);
+        object StreamVideoFromFilename(string base64filename, int? userId, bool? autowatch, string fakename);
         [Rest("/{videolocalid}/{userId?}/{autowatch?}/{fakename?}", Verbs.Head)]
-        System.IO.Stream InfoVideo(int videolocalid, int? userId, bool? autowatch, string fakename);
+        object InfoVideo(int videolocalid, int? userId, bool? autowatch, string fakename);
         [Rest("Filename/{base64filename}/{userId?}/{autowatch?}/{fakename?}", Verbs.Head)]
-        System.IO.Stream InfoVideoFromFilename(string base64filename, int? userId, bool? autowatch, string fakename);
+        object InfoVideoFromFilename(string base64filename, int? userId, bool? autowatch, string fakename);
 
     }
 }
