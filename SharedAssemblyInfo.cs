b5a859f9ba9ffb709301ec1b64e01b02d206453c
Cayde Dixon <me@cazzar.net>
2020-05-01 01:05:52 +1000

    Update version, change to Major.Minor.Release. Update launch config for vscode. Get build working

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("3.9.6.*")]
+[assembly: AssemblyVersion("3.10.0")]
 [assembly: AssemblyProduct("Shoko Server")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
