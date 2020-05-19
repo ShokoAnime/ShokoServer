f9b4e7441b9cb97978b5acd1990d6f0096d6ae77
da3dsoul <da3dsoul@gmail.com>
2020-05-19 18:27:22 -0400

    Fix Dupe Percentage by not including Variations
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- Shoko.Server/API/v3/Controllers/DashboardController.cs
+++ Shoko.Server/API/v3/Controllers/DashboardController.cs
@@ -48,7 +48,7 @@ namespace Shoko.Server.API.v3
                 .Where(a => a != null).Sum(a => a.Duration) / 3600000, 1, MidpointRounding.AwayFromZero); // Duration in ms => 60m*60s*1000ms = 3600000
 
             List<SVR_VideoLocal_Place> places = files.SelectMany(a => a.Places).ToList();
-            int duplicate = places.SelectMany(a => RepoFactory.CrossRef_File_Episode.GetByHash(a.VideoLocal.Hash))
+            int duplicate = places.Where(a => a.VideoLocal.IsVariation == 0).SelectMany(a => RepoFactory.CrossRef_File_Episode.GetByHash(a.VideoLocal.Hash))
                 .GroupBy(a => a.EpisodeID).Count(a => a.Count() > 1);
 
             decimal percentDupe =
