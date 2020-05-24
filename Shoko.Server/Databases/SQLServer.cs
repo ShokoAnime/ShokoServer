a3540d50a2ce93a389f9077d0b94ca9a8bd7f9a7
da3dsoul <da3dsoul@gmail.com>
2020-05-24 03:19:30 -0400

    Fix SQL Server Migration. DEFAULT Constraints are apparently a bitch

--- Shoko.Server/Databases/SQLServer.cs
+++ Shoko.Server/Databases/SQLServer.cs
@@ -12,6 +12,7 @@ using Microsoft.Win32;
 using NHibernate;
 using NHibernate.AdoNet;
 using NHibernate.Cfg;
+using Shoko.Commons.Extensions;
 using Shoko.Commons.Properties;
 using Shoko.Server.Repositories;
 using Shoko.Server.Settings;
@@ -604,7 +605,7 @@ namespace Shoko.Server.Databases
             new DatabaseCommand(80, 1, DatabaseFixes.RegenTvDBMatches),
             new DatabaseCommand(81, 1, "ALTER TABLE AnimeSeries ADD UpdatedAt datetime NOT NULL DEFAULT '2000-01-01 00:00:00';"),
             new DatabaseCommand(82, 1, DatabaseFixes.MigrateAniDBToNet),
-            new DatabaseCommand(83, 1, "ALTER TABLE VideoLocal DROP COLUMN VideoCodec, VideoBitrate, VideoFrameRate, VideoResolution, AudioCodec, AudioBitrate, Duration")
+            new DatabaseCommand(83, 1, DropVideoLocalMediaColumns)
         };
 
         private List<DatabaseCommand> updateVersionTable = new List<DatabaseCommand>
@@ -616,6 +617,39 @@ namespace Shoko.Server.Databases
             new DatabaseCommand(
                 "CREATE INDEX IX_Versions_VersionType ON Versions(VersionType,VersionValue,VersionRevision);"),
         };
+
+        private static void DropVideoLocalMediaColumns()
+        {
+            string[] columns =
+            {
+                "VideoCodec", "VideoBitrate", "VideoBitDepth", "VideoFrameRate", "VideoResolution", "AudioCodec", "AudioBitrate",
+                "Duration"
+            };
+            columns.ForEach(a => DropColumnWithDefaultConstraint("VideoLocal", a));
+        }
+
+        private static void DropColumnWithDefaultConstraint(string table, string column)
+        {
+            using (var session = DatabaseFactory.SessionFactory.OpenStatelessSession())
+            {
+                using (var trans = session.BeginTransaction())
+                {
+                    string query = $@"DECLARE @ConstraintName nvarchar(200)
+SELECT @ConstraintName = Name FROM SYS.DEFAULT_CONSTRAINTS
+WHERE PARENT_OBJECT_ID = OBJECT_ID('{table}')
+AND PARENT_COLUMN_ID = (SELECT column_id FROM sys.columns
+                        WHERE NAME = N'{column}'
+                        AND object_id = OBJECT_ID(N'{table}'))
+IF @ConstraintName IS NOT NULL
+EXEC('ALTER TABLE {table} DROP CONSTRAINT ' + @ConstraintName)";
+                    session.CreateSQLQuery(query).ExecuteUpdate();
+
+                    query = $@"ALTER TABLE {table} DROP COLUMN {column}";
+                    session.CreateSQLQuery(query).ExecuteUpdate();
+                    trans.Commit();
+                }
+            }
+        }
 
         protected override Tuple<bool, string> ExecuteCommand(SqlConnection connection, string command)
         {
