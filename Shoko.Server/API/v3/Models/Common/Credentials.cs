e89bca29822c9fca440b9e4a86861e9f33f1328c
da3dsoul <da3dsoul@gmail.com>
2020-06-21 23:06:40 -0400

    Missed a spot
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- Shoko.Server/API/v3/Models/Common/Credentials.cs
+++ Shoko.Server/API/v3/Models/Common/Credentials.cs
@@ -10,26 +10,5 @@
         /// A generic password field
         /// </summary>
         public string Password { get; set; }
-        
-        /// <summary>
-        /// Credentials Specific to AniDB
-        /// </summary>
-        public class AniDBCredentials : Credentials
-        {
-            /// <summary>
-            /// The client port. default 4556
-            /// </summary>
-            public int ClientPort { get; set; }
-            
-            /// <summary>
-            /// The AVDump port. default 4557
-            /// </summary>
-            public int AVDumpClientPort { get; set; }
-            
-            /// <summary>
-            /// The AVDump key is set on the AniDB profile page
-            /// </summary>
-            public string AVDumpAPIKey { get; set; }
-        }
     }
 }
