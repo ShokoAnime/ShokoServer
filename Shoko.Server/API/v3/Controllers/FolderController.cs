7ea810fba713694bbfb3b3ad1832a53030b7ad67
Cayde Dixon <me@cazzar.net>
2020-07-13 20:04:20 +1000

    Folder API, should work in linux

---
+++ Shoko.Server/API/v3/Controllers/FolderController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoko.Server.API.Annotations;
+using Shoko.Server.Plugin;
+
+namespace Shoko.Server.API.v3
+{
+    [ApiController, Route("/api/v{version:apiVersion}/[controller]"), ApiV3]
+    [Authorize]
+    public class FolderController : Controller
+    {
+        [HttpGet("drives")]
+        public ActionResult<IEnumerable<Folder>> GetDrives()
+        {
+            var drives = DriveInfo.GetDrives();
+            return drives.Select(d =>
+            {
+                ChildItems childItems = null;
+                try
+                {
+                    childItems = new ChildItems()
+                    {
+                        Files = d.RootDirectory.GetFiles()?.Length ?? 0,
+                        Folders = d.RootDirectory.GetDirectories()?.Length ?? 0
+                    };
+                }
+                catch (UnauthorizedAccessException)
+                {
+                }
+
+                return new Folder()
+                {
+                    Path = d.RootDirectory.FullName,
+                    CanAccess = childItems != null,
+                    Sizes = childItems
+                };
+            }).ToList();
+        }
+        
+        [HttpGet("")]
+        public ActionResult<IEnumerable<Folder>> GetFolder([FromQuery] string path)
+        {
+            if (!Directory.Exists(path)) return NotFound("Directory not found");
+            
+            var root  = new DirectoryInfo(path);
+            return root.GetDirectories().Select(dir =>
+            {
+                ChildItems childItems = null;
+                try
+                {
+                    childItems = new ChildItems()
+                    {
+                        Files = dir.GetFiles()?.Length ?? 0,
+                        Folders = dir.GetDirectories()?.Length ?? 0
+                    };
+                }
+                catch (UnauthorizedAccessException)
+                {
+                }
+
+                return new Folder()
+                {
+                    Path = dir.FullName,
+                    CanAccess = childItems != null,
+                    Sizes = childItems
+                };
+            }).ToList();
+        }
+    }
+}
---
+++ Shoko.Server/API/v3/Models/Shoko/Folder.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace Shoko.Server.API.v3
+{
+    public class Folder
+    {
+        public string Path { get; set; }
+        
+        [DefaultValue(false)]            
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public bool CanAccess { get; set; }
+        public ChildItems Sizes { get; set; }
+    }
+}
