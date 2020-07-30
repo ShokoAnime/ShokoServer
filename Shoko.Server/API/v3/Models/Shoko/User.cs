39ab2f45f0d78088d925ad068a5e0e15476199d6
da3dsoul <da3dsoul@gmail.com>
2020-07-30 18:08:09 -0400

    Finish up the User model for APIv3
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- Shoko.Server/API/v3/Models/Shoko/User.cs
+++ Shoko.Server/API/v3/Models/Shoko/User.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Shoko.Models.Enums;
+
 namespace Shoko.Server.API.v3.Models.Shoko
 {
     public class User
@@ -11,6 +14,22 @@ namespace Shoko.Server.API.v3.Models.Shoko
         /// Pretty Self-explanatory. It's the Username of the user
         /// </summary>
         public string Username { get; set; }
+        
+        /// <summary>
+        /// Is the user an admin. Admins can perform all operations, including modification of users
+        /// </summary>
+        public bool IsAdmin { get; set; }
+        
+        /// <summary>
+        /// This is a list of services that the user is set to use. AniDB, Trakt, and Plex, for example
+        /// </summary>
+        public List<CommunitySites> CommunitySites { get; set; }
+        
+        /// <summary>
+        /// This is also called 'Hide Categories'. The current political atmosphere made me salty enough to call it what it is: a blacklist.
+        /// Tags that are here are not visible to the user. Any series with any of these tags will not be shown in any context
+        /// </summary>
+        public List<string> TagBlacklist { get; set; }
 
         public class FullUser
         {
@@ -18,6 +37,21 @@ namespace Shoko.Server.API.v3.Models.Shoko
             /// The password...Shoko is NOT secure, so don't assume this password is safe or even necessary to access the account
             /// </summary>
             public string Password { get; set; }
+        }
+        
+        /// <summary>
+        /// The Plex User Settings...
+        /// </summary>
+        public class PlexUserSettings
+        {
+            /// <summary>
+            /// This means something. Cazzar help me out here.
+            /// </summary>
+            public string PlexUsers { get; set; }
+            /// <summary>
+            /// The token for authentication with the Plex Server API
+            /// </summary>
+            public string PlexToken { get; set; }
         }
     }
 }
