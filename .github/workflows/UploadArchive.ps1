f1e68d9bb9bd13ff629b39543da81d9cd5ec0dc1
da3dsoul <da3dsoul@gmail.com>
2020-08-24 17:05:57 -0400

    Pointlessly cast a bunch of things because I don't know powershell
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -1,7 +1,7 @@
 $user = $env:FTP_USERNAME;
 $password = $env:FTP_PASSWORD;
-$server = $env:FTP_SERVER;
+$ftp_server = $env:FTP_SERVER;
 $current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile("$server/shoko-server/daily/ShokoServer.zip", "$current\\ShokoServer.zip");
+$client.UploadFile([string]($ftp_server) + "/shoko-server/daily/ShokoServer.zip", [string]($current) + "\\ShokoServer.zip");
