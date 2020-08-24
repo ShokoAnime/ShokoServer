e8e3af5b98873823e1ca81737d52db843520d86d
da3dsoul <da3dsoul@gmail.com>
2020-08-24 16:48:14 -0400

    Hopefully fix path not found
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -1,6 +1,7 @@
 $user = $env:FTP_USERNAME;
 $password = $env:FTP_PASSWORD;
 $server = $env:FTP_SERVER;
+$current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile("$server/files/shoko-server/daily/ShokoServer.zip", ".\\ShokoServer.zip");
+$client.UploadFile("$server/shoko-server/daily/ShokoServer.zip", "$current\\ShokoServer.zip");
