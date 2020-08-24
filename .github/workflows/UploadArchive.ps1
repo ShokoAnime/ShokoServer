33ff6ba4cd1a59406942b8edb64a9974bbf8db8c
da3dsoul <da3dsoul@gmail.com>
2020-08-24 16:18:25 -0400

    Still figuring out powershell.... Progress, though
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -3,4 +3,4 @@ $password = $env:FTP_PASSWORD;
 $server = $env:FTP_SERVER;
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile("$(server)/files/shoko-server/daily/ShokoServer.zip", ".\\ShokoServer.zip");
+$client.UploadFile("$server/files/shoko-server/daily/ShokoServer.zip", ".\\ShokoServer.zip");
