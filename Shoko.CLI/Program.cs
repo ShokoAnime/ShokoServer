c6849dfdec9c5d99b84221cfbad0d498a652e665
Swtrse <mg@swtrse.eu>
2020-11-18 13:39:29 +0100

    Code Reformating for an consistent look and feel (Overview: https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines).
    Most noticable are:
    Rename Shoko.CLI to Shoko.Cli (->https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/capitalization-conventions#capitalization-rules-for-identifiers)
    Use Tab for Indent (->https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/inside-a-program/coding-conventions#layout-conventions)

--- Shoko.CLI/Program.cs
+++ Shoko.CLI/Program.cs
@@ -1,89 +1,125 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using NLog.Web;
+﻿// ******************************* Module Header *******************************
+// Module Name:   Program.cs
+// Project:       Shoko.Cli
+// 
+// MIT License
+// 
+// Copyright © 2020 Shoko
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// *****************************************************************************
 
-namespace Shoko.CLI
+namespace Shoko.Cli
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            // here we would do things that need to happen after init, but before shutdown, while blocking the main thread.
-            host.Run();
-        }
+	using System;
+	using System.IO;
+	using System.Reflection;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
+	using NLog.Web;
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            string instance = null;
-            for (int x = 0; x < args.Length; x++)
-            {
-                if (!args[x].Equals("instance", StringComparison.InvariantCultureIgnoreCase)) continue;
-                if (x + 1 >= args.Length) continue;
-                instance = args[x + 1];
-                break;
-            }
+	public class Program
+	{
+		private static IHostBuilder CreateBuilder(string[] args)
+		{
+			var builder = new HostBuilder();
 
-            var arguments = new ProgramArguments {Instance = instance};
-            return CreateBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton(arguments);
-                    services.AddHostedService<Worker>();
-                });
-        }
+			builder.UseContentRoot(Directory.GetCurrentDirectory());
+			builder.ConfigureHostConfiguration(config =>
+			                                   {
+				                                   config.AddEnvironmentVariables("DOTNET_");
+				                                   if (args != null)
+				                                   {
+					                                   config.AddCommandLine(args);
+				                                   }
+			                                   });
 
-        private static IHostBuilder CreateBuilder(string[] args)
-        {
-            var builder = new HostBuilder();
+			builder.ConfigureAppConfiguration((hostingContext, config) =>
+			                                  {
+				                                  IHostEnvironment env = hostingContext.HostingEnvironment;
 
-            builder.UseContentRoot(Directory.GetCurrentDirectory());
-            builder.ConfigureHostConfiguration(config =>
-            {
-                config.AddEnvironmentVariables(prefix: "DOTNET_");
-                if (args != null)
-                {
-                    config.AddCommandLine(args);
-                }
-            });
+				                                  bool reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", true);
 
-            builder.ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;
-                
-                var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
+				                                  config.AddJsonFile("appsettings.json", true, reloadOnChange)
+				                                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, reloadOnChange);
 
-                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: reloadOnChange)
-                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: reloadOnChange);
+				                                  if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
+				                                  {
+					                                  Assembly appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+					                                  if (appAssembly != null)
+					                                  {
+						                                  config.AddUserSecrets(appAssembly, true);
+					                                  }
+				                                  }
 
-                if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
-                {
-                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                    if (appAssembly != null)
-                    {
-                        config.AddUserSecrets(appAssembly, optional: true);
-                    }
-                }
+				                                  config.AddEnvironmentVariables();
 
-                config.AddEnvironmentVariables();
+				                                  if (args != null)
+				                                  {
+					                                  config.AddCommandLine(args);
+				                                  }
+			                                  })
+			       .UseDefaultServiceProvider((context, options) =>
+			                                  {
+				                                  bool isDevelopment = context.HostingEnvironment.IsDevelopment();
+				                                  options.ValidateScopes = isDevelopment;
+				                                  options.ValidateOnBuild = isDevelopment;
+			                                  })
+			       .UseNLog();
 
-                if (args != null)
-                {
-                    config.AddCommandLine(args);
-                }
-            })
-            .UseDefaultServiceProvider((context, options) =>
-            {
-                var isDevelopment = context.HostingEnvironment.IsDevelopment();
-                options.ValidateScopes = isDevelopment;
-                options.ValidateOnBuild = isDevelopment;
-            }).UseNLog();
+			return builder;
+		}
 
-            return builder;
-        }
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			string instance = null;
+			for (var x = 0; x < args.Length; x++)
+			{
+				if (!args[x].Equals("instance", StringComparison.InvariantCultureIgnoreCase))
+				{
+					continue;
+				}
+
+				if (x + 1 >= args.Length)
+				{
+					continue;
+				}
+
+				instance = args[x + 1];
+				break;
+			}
+
+			var arguments = new ProgramArguments {Instance = instance};
+			return CreateBuilder(args)
+				.ConfigureServices((hostContext, services) =>
+				                   {
+					                   services.AddSingleton(arguments);
+					                   services.AddHostedService<Worker>();
+				                   });
+		}
+
+		public static void Main(string[] args)
+		{
+			IHost host = CreateHostBuilder(args).Build();
+			// here we would do things that need to happen after init, but before shutdown, while blocking the main thread.
+			host.Run();
+		}
+	}
 }
--- Shoko.CLI/Worker.cs
+++ Shoko.CLI/Worker.cs
@@ -1,70 +1,101 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using NLog;
-using Shoko.Server;
-using Shoko.Server.Server;
-using Shoko.Server.Settings;
-using Shoko.Server.Utilities;
+// ******************************* Module Header *******************************
+// Module Name:   Worker.cs
+// Project:       Shoko.Cli
+// 
+// MIT License
+// 
+// Copyright © 2020 Shoko
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// *****************************************************************************
 
-namespace Shoko.CLI
+namespace Shoko.Cli
 {
-    public class Worker : BackgroundService
-    {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+	using System;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using Microsoft.Extensions.Hosting;
+	using NLog;
+	using Server.Server;
+	using Server.Settings;
+	using Server.Utilities;
 
-        public ProgramArguments Args { get; set; }
-        private IHostApplicationLifetime AppLifetime { get; set; }
-        
-        public Worker()
-        {
-            Args = null;
-        }
+	public class Worker : BackgroundService
+	{
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        public Worker(ProgramArguments args, IHostApplicationLifetime lifetime)
-        {
-            Args = args;
-            AppLifetime = lifetime;
-        }
+		public Worker() => Args = null;
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            if (!string.IsNullOrEmpty(Args?.Instance)) ServerSettings.DefaultInstance = Args.Instance;
+		public Worker(ProgramArguments args, IHostApplicationLifetime lifetime)
+		{
+			Args = args;
+			AppLifetime = lifetime;
+		}
 
-            ShokoServer.Instance.InitLogger();
-            
-            ServerSettings.LoadSettings();
-            ServerState.Instance.LoadSettings();
-            if (!ShokoServer.Instance.StartUpServer()) return;
+		private IHostApplicationLifetime AppLifetime { get; }
 
-            // Ensure that the AniDB socket is initialized. Try to Login, then start the server if successful.
-            ShokoServer.Instance.RestartAniDBSocket();
-            if (!ServerSettings.Instance.FirstRun)
-                ShokoServer.RunWorkSetupDB();
-            else Logger.Warn("The Server is NOT STARTED. It needs to be configured via webui or the settings.json");
+		public ProgramArguments Args { get; set; }
 
-            ShokoServer.Instance.ServerShutdown += (sender, eventArgs) => AppLifetime.StopApplication();
-            Utils.YesNoRequired += (sender, e) =>
-            {
-                e.Cancel = true;
-            };
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			if (!string.IsNullOrEmpty(Args?.Instance))
+			{
+				ServerSettings.DefaultInstance = Args.Instance;
+			}
 
-            ServerState.Instance.PropertyChanged += (sender, e) =>
-            {
-                if (e.PropertyName == "StartupFailedMessage" && ServerState.Instance.StartupFailed)
-                {
-                    Console.WriteLine("Startup failed! Error message: " + ServerState.Instance.StartupFailedMessage);
-                }
-            };
-            ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent +=
-                ev => Console.WriteLine($"General Queue state change: {ev.QueueState.formatMessage()}");
-        }
+			ShokoServer.Instance.InitLogger();
 
-        public override async Task StopAsync(CancellationToken cancellationToken)
-        {
-            ShokoService.CancelAndWaitForQueues();
-            await base.StopAsync(cancellationToken);
-        }
-    }
+			ServerSettings.LoadSettings();
+			ServerState.Instance.LoadSettings();
+			if (!ShokoServer.Instance.StartUpServer())
+			{
+				return;
+			}
+
+			// Ensure that the AniDB socket is initialized. Try to Login, then start the server if successful.
+			ShokoServer.Instance.RestartAniDBSocket();
+			if (!ServerSettings.Instance.FirstRun)
+			{
+				ShokoServer.RunWorkSetupDB();
+			}
+			else
+			{
+				Logger.Warn("The Server is NOT STARTED. It needs to be configured via webui or the settings.json");
+			}
+
+			ShokoServer.Instance.ServerShutdown += (sender, eventArgs) => AppLifetime.StopApplication();
+			Utils.YesNoRequired += (sender, e) => { e.Cancel = true; };
+
+			ServerState.Instance.PropertyChanged += (sender, e) =>
+			                                        {
+				                                        if (e.PropertyName == "StartupFailedMessage" && ServerState.Instance.StartupFailed)
+				                                        {
+					                                        Console.WriteLine("Startup failed! Error message: " + ServerState.Instance.StartupFailedMessage);
+				                                        }
+			                                        };
+			ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent += ev => Console.WriteLine($"General Queue state change: {ev.QueueState.formatMessage()}");
+		}
+
+		public override async Task StopAsync(CancellationToken cancellationToken)
+		{
+			ShokoService.CancelAndWaitForQueues();
+			await base.StopAsync(cancellationToken);
+		}
+	}
 }
