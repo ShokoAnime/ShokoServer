28ebcdcf90d344653ef5b87578cf0383d1619b94
Cayde Dixon <me@cazzar.net>
2020-12-07 00:25:17 +1100

    ...

--- Shoko.Server/Repositories/Direct/RenameScriptRepository.cs
+++ Shoko.Server/Repositories/Direct/RenameScriptRepository.cs
@@ -8,9 +8,16 @@ namespace Shoko.Server.Repositories
 {
     public class RenameScriptRepository : BaseDirectRepository<RenameScript, int>
     {
-        public RenameScript GetDefaultEnabledScript()
+        public RenameScript GetDefaultScript()
         {
-            return GetAll().FirstOrDefault(a => a.IsEnabledOnImport == 1);
+            using (var session = DatabaseFactory.SessionFactory.OpenSession())
+            {
+                RenameScript cr = session
+                    .CreateCriteria(typeof(RenameScript))
+                    .Add(Restrictions.Eq("IsEnabledOnImport", 1))
+                    .UniqueResult<RenameScript>();
+                return cr;
+            }
         }
 
         public RenameScript GetDefaultOrFirst()
