c5f2a0de33249f5d310b62a44b571a509e3d9207
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2021-01-31 14:42:39 -0800

    Don't ignore case when sorting.

--- Shoko.Server/Renamer/RenameFileHelper.cs
+++ Shoko.Server/Renamer/RenameFileHelper.cs
@@ -128,7 +128,7 @@ namespace Shoko.Server
         public static IList<IRenamer> GetPluginRenamersSorted(string renamerName) => 
             _getEnabledRenamers(renamerName).OrderBy(a => renamerName == a.Key ? 0 : int.MaxValue)
                 .ThenBy(a => ServerSettings.Instance.Plugins.RenamerPriorities.ContainsKey(a.Key) ? ServerSettings.Instance.Plugins.RenamerPriorities[a.Key] : int.MaxValue)
-                .ThenBy(a => a.Key, StringComparer.InvariantCultureIgnoreCase)
+                .ThenBy(a => a.Key, StringComparer.InvariantCulture)
                 .Select(a => (IRenamer)ActivatorUtilities.CreateInstance(ShokoServer.ServiceContainer, a.Value.type)).ToList();
 
         private static IEnumerable<KeyValuePair<string, (Type type, string description)>> _getEnabledRenamers(string renamerName)
