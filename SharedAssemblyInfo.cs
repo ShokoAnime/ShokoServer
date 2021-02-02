56d072c8aa9185f91c251d10644dfe33128e381b
Elemental Crisis <9443295+ElementalCrisis@users.noreply.github.com>
2021-02-01 17:12:33 -0800

    Update Installer.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyVersion("4.1.0")]
 [assembly: AssemblyProduct("Shoko Server")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyInformationalVersion("release")]
