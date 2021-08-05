265c022ab060fbb7be7cc8fe567a1323d6d42220
Elemental Crisis <9443295+ElementalCrisis@users.noreply.github.com>
2021-08-05 12:13:29 -0700

    Update Renamer link.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Shoko.Commons.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -6829,7 +6829,7 @@ namespace Shoko.Commons.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://docs.shokoanime.com/desktop/utilities/#file-renaming.
+        ///   Looks up a localized string similar to https://wiki.anidb.net/WebAOM#Move.2Frename_system.
         /// </summary>
         public static string Link_FileRenaming {
             get {
