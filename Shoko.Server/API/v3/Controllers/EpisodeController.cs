d77cf50fdc9bf4384f78035edbdf3de44cd3d43a
Mikal Stordal <mikalstordal@gmail.com>
2022-02-01 00:08:02 +0100

    Update the episode controller in the v3 api Add the endpoint to find episodes with multiple files.

--- Shoko.Server/API/v3/Controllers/EpisodeController.cs
+++ Shoko.Server/API/v3/Controllers/EpisodeController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Shoko.Commons.Extensions;
 using Shoko.Server.API.Annotations;
 using Shoko.Server.API.v3.Models.Common;
 using Shoko.Server.API.v3.Models.Shoko;
+using Shoko.Server.Models;
 using Shoko.Server.Repositories;
 
 namespace Shoko.Server.API.v3.Controllers
@@ -89,6 +92,32 @@ namespace Shoko.Server.API.v3.Controllers
 
             ep.ToggleWatchedStatus(watched, true, DateTime.Now, true, User.JMMUserID, true);
             return Ok();
+        }
+        
+        /// <summary>
+        /// Get episodes with multiple files attached.
+        /// </summary>
+        /// <param name="ignoreVariations">Ignore manually toggled variations in the results. Defaults to false.</param>
+        /// <param name="onlyFinishedSeries">Only show finished series. Defaults to true.</param>
+        /// <param name="pageSize">Limits the number of results per page. Default is 100. Set to 0 to disable the limit.</param>
+        /// <param name="page">Page number. Default is 0.</param>
+        /// <returns></returns>
+        [HttpGet("WithMultipleFiles")]
+        public List<Episode> GetSoftDuplicatesForEpisode([FromQuery] bool ignoreVariations = true, [FromQuery] bool onlyFinishedSeries = false, [FromQuery] int pageSize = 100, [FromQuery] int page = 0)
+        {
+            IEnumerable<SVR_AnimeEpisode> enumerable = RepoFactory.AnimeEpisode.GetEpisodesWithMultipleFiles(ignoreVariations);
+            if (onlyFinishedSeries)
+            {
+                var dictSeriesFinishedAiring = RepoFactory.AnimeSeries.GetAll()
+                    .Select(a => new Tuple<int, bool>(a.AnimeSeriesID, a.GetAnime().GetFinishedAiring())).ToDictionary(k => k.Item1, v => v.Item2);
+                enumerable = enumerable.Where(episode => !(dictSeriesFinishedAiring.TryGetValue(episode.AnimeSeriesID, out var finishedAiring) && finishedAiring));
+            }
+
+            if (pageSize <= 0)
+                return enumerable.Select(a => new Episode(HttpContext, a)).ToList();
+            if (page <= 0) page = 0;
+            return enumerable.Skip(pageSize * page).Take(pageSize)
+                .Select(a => new Episode(HttpContext, a)).ToList();
         }
     }
 }
