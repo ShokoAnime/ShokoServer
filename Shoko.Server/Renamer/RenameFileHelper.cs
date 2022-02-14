f8c288be84a0928494e53235d9d3cde32c7f09b2
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2022-02-14 14:27:41 -0800

    Log when errors are deferred

--- Shoko.Server/Renamer/RenameFileHelper.cs
+++ Shoko.Server/Renamer/RenameFileHelper.cs
@@ -78,8 +78,8 @@ namespace Shoko.Server
                 }
                 catch (Exception e)
                 {
-                    if (ServerSettings.Instance.Plugins.DeferOnError && !args.Cancel) continue;
-                    throw;
+                    if (!ServerSettings.Instance.Plugins.DeferOnError || args.Cancel) throw;
+                    Logger.Warn($"Renamer: {renamer.GetType().Name} threw an error while renaming, deferring to next renamer. Filename: \"{result}\" Error message: \"{e.Message}\"");
                 }
             }
 
@@ -118,9 +118,7 @@ namespace Shoko.Server
                     var importFolder = RepoFactory.ImportFolder.GetByImportLocation(destFolder.Location);
                     if (importFolder == null)
                     {
-                        Logger.Error(
-                            $"Renamer returned a Destination Import Folder, but it could not be found. The offending plugin was: {renamer.GetType().GetAssemblyName()} with renamer: {renamer.GetType().Name}"
-                        );
+                        Logger.Error($"Renamer returned a Destination Import Folder, but it could not be found. The offending plugin was: {renamer.GetType().GetAssemblyName()} with renamer: {renamer.GetType().Name}");
                         continue;
                     }
 
@@ -128,8 +126,8 @@ namespace Shoko.Server
                 }
                 catch (Exception e)
                 {
-                    if (ServerSettings.Instance.Plugins.DeferOnError && !args.Cancel) continue;
-                    throw;
+                    if (!ServerSettings.Instance.Plugins.DeferOnError || args.Cancel) throw;
+                    Logger.Warn($"Renamer: {renamer.GetType().Name} threw an error while moving, deferring to next renamer. Path: \"{place.FullServerPath}\" Error message: \"{e.Message}\"");
                 }
             }
 
