5af0105482a8a3835ccbe17c0e3a2bd024496ad5
BigRetroMike <bigretromike@users.noreply.github.com>
2022-05-27 23:58:02 +0200

    namespace was missing { }, prevent it from compiling;

--- Shoko.Plugin.Abstractions/Enums/DataSourceEnum.cs
+++ Shoko.Plugin.Abstractions/Enums/DataSourceEnum.cs
@@ -1,14 +1,16 @@
-namespace Shoko.Plugin.Abstractions.Enums;
-
-/// <summary>
-/// Just a list of possible data sources. Not all are going to be used...probably
-/// </summary>
-public enum DataSourceEnum
+namespace Shoko.Plugin.Abstractions.Enums
 {
-    AniDB,
-    MovieDB,
-    TvDB,
-    AniList,
-    Animeshon,
-    Trakt,
+
+    /// <summary>
+    /// Just a list of possible data sources. Not all are going to be used...probably
+    /// </summary>
+    public enum DataSourceEnum
+    {
+        AniDB,
+        MovieDB,
+        TvDB,
+        AniList,
+        Animeshon,
+        Trakt,
+    }
 }
