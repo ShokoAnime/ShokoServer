3e1eee05b5691b513a6bd3ce764c957283ea5b6b
Mikal Stordal <mikalstordal@gmail.com>
2022-07-24 23:31:02 +0200

    refactor: convert enums to strings instead of integers in the api

--- Shoko.Server/API/v3/Models/Shoko/User.cs
+++ Shoko.Server/API/v3/Models/Shoko/User.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Shoko.Commons.Extensions;
 using Shoko.Models.Enums;
 using Shoko.Server.Models;
@@ -26,6 +28,7 @@ namespace Shoko.Server.API.v3.Models.Shoko
         /// <summary>
         /// This is a list of services that the user is set to use. AniDB, Trakt, and Plex, for example
         /// </summary>
+        [JsonProperty(ItemConverterType = typeof(StringEnumConverter))]
         public List<CommunitySites> CommunitySites { get; set; }
         
         /// <summary>
