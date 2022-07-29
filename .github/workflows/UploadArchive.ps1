ac9738f993c009ed984eae03da57d42be0801085
da3dsoul <da3dsoul@gmail.com>
2022-07-28 21:53:55 -0400

    Workflows

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -1,7 +1,12 @@
+Param(
+    [string] $filename = "ShokoServer.zip"
+)
+
 $user = $env:FTP_USERNAME;
 $password = $env:FTP_PASSWORD;
 $ftp_server = $env:FTP_SERVER;
+
 $current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile([string]($ftp_server) + "/shoko-server/daily/ShokoServer.zip", [string]($current) + "\\ShokoServer.zip");
+$client.UploadFile([string]($ftp_server) + "/shoko-server/daily/" + $filename, [string]($current) + "\\ShokoServer.zip");
