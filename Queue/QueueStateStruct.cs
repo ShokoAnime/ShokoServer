91680ea531744e9b93334c9850381a8905a47354
da3dsoul <da3dsoul@gmail.com>
2022-08-11 15:45:07 -0400

    Remove a bunch of Command Types and Un-Localize Command Strings

--- Queue/QueueStateStruct.cs
+++ Queue/QueueStateStruct.cs
@@ -12,11 +12,14 @@ namespace Shoko.Commons.Queue
     {
         public QueueStateEnum queueState;
         public string[] extraParams;
+        public string Message { get; set; }
 
         [Pure]
         public string formatMessage()
         {
-            string formatString = getFormatString(queueState);
+            if (!string.IsNullOrEmpty(Message)) return Message;
+
+            var formatString = getFormatString(queueState);
             // ReSharper disable once CoVariantArrayConversion
             return string.Format(formatString, extraParams);
         }
@@ -24,144 +27,62 @@ namespace Shoko.Commons.Queue
         [Pure]
         private string getFormatString(QueueStateEnum id)
         {
-            switch (id)
+            return id switch
             {
-                case QueueStateEnum.Actions_SyncVotes:
-                    return Resources.Actions_SyncVotes;
-                case QueueStateEnum.AniDB_GetTitles:
-                    return Resources.AniDB_GetTitles;
-                case QueueStateEnum.AniDB_MyListAdd:
-                    return Resources.AniDB_MyListAdd;
-                case QueueStateEnum.AniDB_MyListDelete:
-                    return Resources.AniDB_MyListDelete;
-                case QueueStateEnum.AniDB_MyListGetFile:
-                    return Resources.AniDB_MyListGetFile;
-                case QueueStateEnum.AnimeInfo:
-                    return Resources.Command_AnimeInfo;
-                case QueueStateEnum.CheckingFile:
-                    return Resources.Command_CheckingFile;
-                case QueueStateEnum.DeleteError:
-                    return Resources.Command_DeleteError;
-                case QueueStateEnum.DownloadImage:
-                    return Resources.Command_DownloadImage;
-                case QueueStateEnum.DownloadMalWatched:
-                    return Resources.Command_DownloadMalWatched;
-                case QueueStateEnum.DownloadTvDBImages:
-                    return Resources.Command_DownloadTvDBImages;
-                case QueueStateEnum.FileInfo:
-                    return Resources.Command_FileInfo;
-                case QueueStateEnum.GetCalendar:
-                    return Resources.Command_GetCalendar;
-                case QueueStateEnum.GetEpisodeList:
-                    return Resources.Command_GetEpisodeList;
-                case QueueStateEnum.GetFileInfo:
-                    return Resources.Command_GetFileInfo;
-                case QueueStateEnum.GetReleaseGroup:
-                    return Resources.Command_GetReleaseGroup;
-                case QueueStateEnum.GetReleaseInfo:
-                    return Resources.Command_GetReleaseInfo;
-                case QueueStateEnum.GetReviewInfo:
-                    return Resources.Command_GetReviewInfo;
-                case QueueStateEnum.GetUpdatedAnime:
-                    return Resources.Command_GetUpdatedAnime;
-                case QueueStateEnum.GettingTvDBEpisode:
-                    return Resources.Command_GettingTvDBEpisode;
-                case QueueStateEnum.GettingTvDBSeries:
-                    return Resources.Command_GettingTvDBSeries;
-                case QueueStateEnum.HashingFile:
-                    return Resources.Command_HashingFile;
-                case QueueStateEnum.Idle:
-                    return Resources.Command_Idle;
-                case QueueStateEnum.LinkAniDBTvDB:
-                    return Resources.Command_LinkAniDBTvDB;
-                case QueueStateEnum.LinkFileManually:
-                    return Resources.Command_LinkFileManually;
-                case QueueStateEnum.Paused:
-                    return Resources.Command_Paused;
-                case QueueStateEnum.Queued:
-                    return Resources.Command_Queued;
-                case QueueStateEnum.ReadingMedia:
-                    return Resources.Command_ReadingMedia;
-                case QueueStateEnum.Refresh:
-                    return Resources.Command_Refresh;
-                case QueueStateEnum.RefreshGroupFilter:
-                    return Resources.Command_RefreshGroupFilter;
-                case QueueStateEnum.SearchMal:
-                    return Resources.Command_SearchMal;
-                case QueueStateEnum.SearchTMDb:
-                    return Resources.Command_SearchTMDb;
-                case QueueStateEnum.SearchTrakt:
-                    return Resources.Command_SearchTrakt;
-                case QueueStateEnum.SearchTvDB:
-                    return Resources.Command_SearchTvDB;
-                case QueueStateEnum.SendAnimeAzure:
-                    return Resources.Command_SendAnimeAzure;
-                case QueueStateEnum.SendAnimeFull:
-                    return Resources.Command_SendAnimeFull;
-                case QueueStateEnum.SendAnimeTitle:
-                    return Resources.Command_SendAnimeTitle;
-                case QueueStateEnum.SendAnonymousData:
-                    return Resources.Command_SendAnonymousData;
-                case QueueStateEnum.StartingGeneral:
-                    return Resources.Command_StartingGeneral;
-                case QueueStateEnum.StartingHasher:
-                    return Resources.Command_StartingHasher;
-                case QueueStateEnum.StartingImages:
-                    return Resources.Command_StartingImages;
-                case QueueStateEnum.SyncMyList:
-                    return Resources.Command_SyncMyList;
-                case QueueStateEnum.SyncPlex:
-                    return Resources.Command_SyncPlex;
-                case QueueStateEnum.SyncTrakt:
-                    return Resources.Command_SyncTrakt;
-                case QueueStateEnum.SyncTraktEpisodes:
-                    return Resources.Command_SyncTraktEpisodes;
-                case QueueStateEnum.SyncTraktSeries:
-                    return Resources.Command_SyncTraktSeries;
-                case QueueStateEnum.SyncVotes:
-                    return Resources.Command_SyncVotes;
-                case QueueStateEnum.TraktAddHistory:
-                    return Resources.Command_TraktAddHistory;
-                case QueueStateEnum.UpdateMALWatched:
-                    return Resources.Command_UpdateMALWatched;
-                case QueueStateEnum.UpdateMyListInfo:
-                    return Resources.Command_UpdateMyListInfo;
-                case QueueStateEnum.UpdateMyListStats:
-                    return Resources.Command_UpdateMyListStats;
-                case QueueStateEnum.UpdateTrakt:
-                    return Resources.Command_UpdateTrakt;
-                case QueueStateEnum.UpdateTraktData:
-                    return Resources.Command_UpdateTraktData;
-                case QueueStateEnum.UploadMALWatched:
-                    return Resources.Command_UploadMALWatched;
-                case QueueStateEnum.ValidateAllImages:
-                    return Resources.Command_ValidateAllImages;
-                case QueueStateEnum.VoteAnime:
-                    return Resources.Command_VoteAnime;
-                case QueueStateEnum.WebCacheDeleteXRefAniDBMAL:
-                    return Resources.Command_WebCacheDeleteXRefAniDBMAL;
-                case QueueStateEnum.WebCacheDeleteXRefAniDBOther:
-                    return Resources.Command_WebCacheDeleteXRefAniDBOther;
-                case QueueStateEnum.WebCacheDeleteXRefAniDBTrakt:
-                    return Resources.Command_WebCacheDeleteXRefAniDBTrakt;
-                case QueueStateEnum.WebCacheDeleteXRefAniDBTvDB:
-                    return Resources.Command_WebCacheDeleteXRefAniDBTvDB;
-                case QueueStateEnum.WebCacheDeleteXRefFileEpisode:
-                    return Resources.Command_WebCacheDeleteXRefFileEpisode;
-                case QueueStateEnum.WebCacheSendXRefAniDBMAL:
-                    return Resources.Command_WebCacheSendXRefAniDBMAL;
-                case QueueStateEnum.WebCacheSendXRefAniDBOther:
-                    return Resources.Command_WebCacheSendXRefAniDBOther;
-                case QueueStateEnum.WebCacheSendXRefAniDBTrakt:
-                    return Resources.Command_WebCacheSendXRefAniDBTrakt;
-                case QueueStateEnum.WebCacheSendXRefAniDBTvDB:
-                    return Resources.Command_WebCacheSendXRefAniDBTvDB;
-                case QueueStateEnum.WebCacheSendXRefFileEpisode:
-                    return Resources.Command_WebCacheSendXRefFileEpisode;
-                default:
-                    throw new Exception("Unknown queue state format string");
-            }
-
+                QueueStateEnum.Actions_SyncVotes => "Upload Local Votes To AniDB.",
+                QueueStateEnum.AniDB_GetTitles => "Getting AniDB titles",
+                QueueStateEnum.AniDB_MyListAdd => "Adding file to MyList: {0}",
+                QueueStateEnum.AniDB_MyListDelete => "Deleting file from MyList: {0}",
+                QueueStateEnum.AniDB_MyListGetFile => "Getting File Status from MyList: {0} ({1})",
+                QueueStateEnum.AnimeInfo => "Getting anime info from HTTP API: {0}",
+                QueueStateEnum.CheckingFile => "Checking File for Hashes: {0}",
+                QueueStateEnum.DeleteError => "Error deleting image: ({0}) - {1}",
+                QueueStateEnum.DownloadImage => "Downloading Image {0}: {1}",
+                QueueStateEnum.DownloadMalWatched => "Downloading watched states from MAL",
+                QueueStateEnum.DownloadTvDBImages => "Getting images from The TvDB: {0}",
+                QueueStateEnum.FileInfo => "Getting file info from UDP API: {0}",
+                QueueStateEnum.GetCalendar => "Getting calendar info from UDP API",
+                QueueStateEnum.GetEpisodeList => "Getting episode info from UDP API: {0}",
+                QueueStateEnum.GetFileInfo => "Getting file info from UDP API: {0}",
+                QueueStateEnum.GetReleaseGroup => "Getting group status info from UDP API for Anime: {0}",
+                QueueStateEnum.GetReleaseInfo => "Getting release group info from UDP API: {0}",
+                QueueStateEnum.GetReviewInfo => "Getting review info from UDP API for Anime: {0}",
+                QueueStateEnum.GetUpdatedAnime => "Getting list of updated anime from UDP API",
+                QueueStateEnum.GettingTvDBEpisode => "Updating TvDB Episode: {0}",
+                QueueStateEnum.GettingTvDBSeries => "Updating TvDB Series: {0}",
+                QueueStateEnum.HashingFile => "Hashing File: {0}",
+                QueueStateEnum.Idle => "Idle",
+                QueueStateEnum.LinkAniDBTvDB => "Updating Changed TvDB association: {0}",
+                QueueStateEnum.LinkFileManually => "Linking File: {0} to Episode: {1}",
+                QueueStateEnum.Paused => "Paused",
+                QueueStateEnum.Queued => "Queued",
+                QueueStateEnum.ReadingMedia => "Reading media info for file: {0}",
+                QueueStateEnum.Refresh => "Refreshing anime stats: {0}",
+                QueueStateEnum.RefreshGroupFilter => "Refreshing Group Filter: {0}",
+                QueueStateEnum.SearchMal => "Searching for anime on MAL: {0}",
+                QueueStateEnum.SearchTMDb => "Searching for anime on The MovieDB: {0}",
+                QueueStateEnum.SearchTrakt => "Searching for anime on Trakt.TV: {0}",
+                QueueStateEnum.SearchTvDB => "Searching for anime on The TvDB: {0}",
+                QueueStateEnum.StartingGeneral => "Starting general command worker",
+                QueueStateEnum.StartingHasher => "Starting hasher command worker",
+                QueueStateEnum.StartingImages => "Starting image downloading command worker",
+                QueueStateEnum.SyncMyList => "Syncing MyList info from HTTP API",
+                QueueStateEnum.SyncPlex => "Syncing Plex for user: {0}",
+                QueueStateEnum.SyncTrakt => "Syncing Trakt collection",
+                QueueStateEnum.SyncTraktEpisodes => "Sync episode to collection on Trakt: {0} - {1}",
+                QueueStateEnum.SyncTraktSeries => "Syncing Trakt collection for series: {0}",
+                QueueStateEnum.SyncVotes => "Syncing vote info from HTTP API",
+                QueueStateEnum.TraktAddHistory => "Add episode to history on Trakt: {0}",
+                QueueStateEnum.UpdateMALWatched => "Updating watched status on MAL: {0}",
+                QueueStateEnum.UpdateMyListInfo => "Updating MyList info from UDP API for File: {0}",
+                QueueStateEnum.UpdateMyListStats => "Updating AniDB MyList Stats",
+                QueueStateEnum.UpdateTrakt => "Updating all Trakt series info added to queue",
+                QueueStateEnum.UpdateTraktData => "Updating info/images on Trakt.TV: {0}",
+                QueueStateEnum.UploadMALWatched => "Uploading watched states to MAL",
+                QueueStateEnum.ValidateAllImages => "Validating Images {0}",
+                QueueStateEnum.VoteAnime => "Voting: {0} - {1}",
+                _ => throw new Exception("Unknown queue state format string")
+            };
         }
     }
 
