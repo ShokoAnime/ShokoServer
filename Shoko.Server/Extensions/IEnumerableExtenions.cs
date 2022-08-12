d048e6e7525fff859c5c6d2297c6c96ef662a4cb
Mikal Stordal <mikalstordal@gmail.com>
2022-08-12 14:15:11 +0200

    refactor: udate IEnumerable extensions

--- Shoko.Server/Extensions/IEnumerableExtenions.cs
+++ Shoko.Server/Extensions/IEnumerableExtenions.cs
@@ -11,36 +11,17 @@ namespace Shoko.Server.Extensions
     {
         public static IOrderedEnumerable<SVR_AnimeSeries> OrderByAirDate(this IEnumerable<SVR_AnimeSeries> enumerable)
         {
-            return enumerable.OrderBy(OrderByAirDate);
-        }
-
-        private static DateTime? OrderByAirDate(SVR_AnimeSeries series)
-        {
-            var airdate = series.GetAnime()?.AirDate;
-            if (airdate.HasValue && airdate.Value != DateTime.MinValue)
-                return airdate;
-
-            return null;
+            return enumerable.OrderBy(series => series.GetAnime()?.AirDate ?? DateTime.MaxValue);
         }
 
         public static IOrderedEnumerable<SVR_AnimeGroup> OrderByName(this IEnumerable<SVR_AnimeGroup> enumerable)
         {
-            return enumerable.OrderBy(OrderByName);
+            return enumerable.OrderBy(group => group.GroupName);
         }
 
         public static IOrderedEnumerable<SVR_GroupFilter> OrderByName(this IEnumerable<SVR_GroupFilter> enumerable)
         {
-            return enumerable.OrderBy(OrderByName);
-        }
-
-        private static string OrderByName(SVR_AnimeGroup group)
-        {
-            return group.SortName;
-        }
-
-        private static string OrderByName(SVR_GroupFilter group)
-        {
-            return group.GroupFilterName;
+            return enumerable.OrderBy(filter => filter.GroupFilterName);
         }
     }
 }
