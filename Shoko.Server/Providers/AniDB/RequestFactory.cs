9d55ad8d1dbe2bf3022c50c0732751c811bc96a9
da3dsoul <da3dsoul@gmail.com>
2022-08-13 12:12:42 -0400

    Start Example for RequestFactory

---
+++ Shoko.Server/Providers/AniDB/RequestFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shoko.Server.Providers.AniDB.Interfaces;
+
+namespace Shoko.Server.Providers.AniDB
+{
+    public class RequestFactory : IRequestFactory
+    {
+        private readonly ServiceProvider _provider;
+
+        public RequestFactory(ServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public T1 Create<T1, T2>(Action<T1> ctor = null) where T1 : IRequest<T2>, new() where T2 : IResponse
+        {
+            var obj = ActivatorUtilities.CreateInstance<T1>(_provider);
+            ctor?.Invoke(obj);
+            return obj;
+        }
+    }
+}
