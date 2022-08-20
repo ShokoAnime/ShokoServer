92640f08c08a9654c6595a0246481d1a2fa2f362
da3dsoul <da3dsoul@gmail.com>
2022-08-20 17:25:35 -0400

    Remove Language

--- Server/CrossRef/CrossRef_Languages_AniDB_File.cs
+++ Server/CrossRef/CrossRef_Languages_AniDB_File.cs
@@ -4,6 +4,6 @@
     {
         public int CrossRef_Languages_AniDB_FileID { get; set; }
         public int FileID { get; set; }
-        public int LanguageID { get; set; }
+        public string LanguageName { get; set; }
     }
 }
--- Server/ShokoInternal/Language.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shoko.Models.Server
-{
-    public class Language
-    {
-        public int LanguageID { get; set; }
-        public string LanguageName { get; set; }
-    }
-}
