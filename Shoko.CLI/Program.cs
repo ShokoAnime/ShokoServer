cd24b37141286e0c9cdbb73008c0d67d8bc5ab96
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-09-30 19:45:52 +0200

    Refactor CLI Project + possible breaking change: ProgramArguments-Singleton was removed (needs to be tested)

--- Shoko.CLI/Program.cs
+++ Shoko.CLI/Program.cs
@@ -1,89 +1,70 @@
-﻿using System;
+﻿#region
+using System;
 using System.IO;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NLog.Web;
+#endregion
+namespace Shoko.CLI;
 
-namespace Shoko.CLI
+public static class Program
 {
-    public class Program
+    public static void Main()
+        => new HostBuilder().UseContentRoot(Directory.GetCurrentDirectory())
+                            .ConfigureHostConfiguration(ConfigureHost)
+                            .ConfigureAppConfiguration(ConfigureApp)
+                            .UseDefaultServiceProvider(ConfigureDefaultServiceProvider)
+                            .UseNLog()
+                            .ConfigureServices(ConfigureServices)
+                            .Build()
+                            .Run();
+
+    private static void ConfigureHost(IConfigurationBuilder configurationBuilder)
+        => configurationBuilder.AddEnvironmentVariables(prefix: "DOTNET_")
+                               .AddCommandLineIfNotNull();
+
+    private static void ConfigureApp(HostBuilderContext hostingContext, IConfigurationBuilder configurationBuilder)
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            // here we would do things that need to happen after init, but before shutdown, while blocking the main thread.
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            string instance = null;
-            for (int x = 0; x < args.Length; x++)
-            {
-                if (!args[x].Equals("instance", StringComparison.InvariantCultureIgnoreCase)) continue;
-                if (x + 1 >= args.Length) continue;
-                instance = args[x + 1];
-                break;
-            }
-
-            var arguments = new ProgramArguments {Instance = instance};
-            return CreateBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton(arguments);
-                    services.AddHostedService<Worker>();
-                });
-        }
-
-        private static IHostBuilder CreateBuilder(string[] args)
-        {
-            var builder = new HostBuilder();
-
-            builder.UseContentRoot(Directory.GetCurrentDirectory());
-            builder.ConfigureHostConfiguration(config =>
-            {
-                config.AddEnvironmentVariables(prefix: "DOTNET_");
-                if (args != null)
-                {
-                    config.AddCommandLine(args);
-                }
-            });
-
-            builder.ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                var env = hostingContext.HostingEnvironment;
-                
-                var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
-
-                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: reloadOnChange)
-                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: reloadOnChange);
-
-                if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
-                {
-                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                    if (appAssembly != null)
-                    {
-                        config.AddUserSecrets(appAssembly, optional: true);
-                    }
-                }
-
-                config.AddEnvironmentVariables();
-
-                if (args != null)
-                {
-                    config.AddCommandLine(args);
-                }
-            })
-            .UseDefaultServiceProvider((context, options) =>
-            {
-                var isDevelopment = context.HostingEnvironment.IsDevelopment();
-                options.ValidateScopes = isDevelopment;
-                options.ValidateOnBuild = isDevelopment;
-            }).UseNLog();
-
-            return builder;
-        }
+        var shouldReloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
+        configurationBuilder.AddJsonFile("appsettings.json",
+                                         optional: true,
+                                         reloadOnChange: shouldReloadOnChange)
+                            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",
+                                         optional: true,
+                                         reloadOnChange: shouldReloadOnChange)
+                            .AddDevelopmentAssembly(hostingContext.HostingEnvironment)
+                            .AddEnvironmentVariables()
+                            .AddCommandLineIfNotNull();
     }
+
+    private static void ConfigureDefaultServiceProvider(HostBuilderContext context, ServiceProviderOptions options)
+    {
+        var isDevelopment = context.HostingEnvironment.IsDevelopment();
+        options.ValidateScopes  = isDevelopment;
+        options.ValidateOnBuild = isDevelopment;
+    }
+
+    private static void ConfigureServices(IServiceCollection services) => services.AddHostedService<Worker>();
+
+#region IConfigurationBuilderExtensions
+    private static IConfigurationBuilder AddCommandLineIfNotNull(this IConfigurationBuilder configurationBuilder)
+    {
+        var args = Environment.GetCommandLineArgs();
+        if (args is not null)
+            configurationBuilder.AddCommandLine(args);
+        return configurationBuilder;
+    }
+
+    private static IConfigurationBuilder AddDevelopmentAssembly(this IConfigurationBuilder configurationBuilder, IHostEnvironment env)
+    {
+        if (env.IsDevelopment() is false || string.IsNullOrWhiteSpace(env.ApplicationName))
+            return configurationBuilder;
+
+        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+        configurationBuilder.AddUserSecrets(appAssembly, optional: true);
+        return configurationBuilder;
+    }
+#endregion
 }
--- Shoko.CLI/Worker.cs
+++ Shoko.CLI/Worker.cs
@@ -1,69 +1,101 @@
+#region
 using System;
+using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using NLog;
-using Shoko.Server;
+using Shoko.Server.Commands;
 using Shoko.Server.Server;
 using Shoko.Server.Settings;
 using Shoko.Server.Utilities;
+#endregion
+namespace Shoko.CLI;
 
-namespace Shoko.CLI
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private static readonly Logger                    Logger = LogManager.GetCurrentClassLogger();
+    private readonly        IHostApplicationLifetime? _appLifetime;
+
+    public Worker() { }
+    public Worker(IHostApplicationLifetime lifetime) => _appLifetime = lifetime;
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        SetInstanceIfNeeded();
+        InitInstanceLogger();
+        LoadSettings();
+        if (ServerCouldNotStart())
+            return Task.CompletedTask;
+        EnsureAniDBSocketInitialized();
+        AddEventHandlers();
+        return Task.CompletedTask;
+    }
 
-        public ProgramArguments Args { get; set; }
-        private IHostApplicationLifetime AppLifetime { get; set; }
-        
-        public Worker()
-        {
-            Args = null;
-        }
+    /**
+     * Ensure that the AniDB socket is initialized. Try to Login, then start the server if successful.
+     */
+    private static void EnsureAniDBSocketInitialized()
+    {
+        if (ServerSettings.Instance.FirstRun is false)
+            ShokoServer.RunWorkSetupDB();
+        else
+            Logger.Warn("The Server is NOT STARTED. It needs to be configured via webui or the settings.json");
+    }
 
-        public Worker(ProgramArguments args, IHostApplicationLifetime lifetime)
-        {
-            Args = args;
-            AppLifetime = lifetime;
-        }
+    private static bool ServerCouldNotStart() => ShokoServer.Instance.StartUpServer() is false;
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            if (!string.IsNullOrEmpty(Args?.Instance)) ServerSettings.DefaultInstance = Args.Instance;
+    private static void InitInstanceLogger() => ShokoServer.Instance.InitLogger();
+    
+    private static string? GetInstanceFromCommandLineArguments()
+    {
+        const int notFound = -1;
+        var       args     = Environment.GetCommandLineArgs();
+        var       idx      = Array.FindIndex(args, x => string.Equals(x, "instance", StringComparison.InvariantCultureIgnoreCase));
+        if (idx is notFound)
+            return null;
+        if (idx >= args.Length - 1)
+            return null;
+        return args[idx + 1];
+    }
 
-            ShokoServer.Instance.InitLogger();
-            
-            ServerSettings.LoadSettings();
-            ServerState.Instance.LoadSettings();
-            if (!ShokoServer.Instance.StartUpServer()) return;
+    private static void SetInstanceIfNeeded()
+    {
+        var instance = GetInstanceFromCommandLineArguments();
+        if (string.IsNullOrWhiteSpace(instance) is false)
+            ServerSettings.DefaultInstance = instance;
+    }
 
-            // Ensure that the AniDB socket is initialized. Try to Login, then start the server if successful.
-            if (!ServerSettings.Instance.FirstRun)
-                ShokoServer.RunWorkSetupDB();
-            else Logger.Warn("The Server is NOT STARTED. It needs to be configured via webui or the settings.json");
+    private static void LoadSettings()
+    {
+        ServerSettings.LoadSettings();
+        ServerState.Instance.LoadSettings();
+    }
 
-            ShokoServer.Instance.ServerShutdown += (sender, eventArgs) => AppLifetime.StopApplication();
-            Utils.YesNoRequired += (sender, e) =>
-            {
-                e.Cancel = true;
-            };
+    private void AddEventHandlers()
+    {
+        ShokoServer.Instance.ServerShutdown                       += OnInstanceOnServerShutdown;
+        Utils.YesNoRequired                                       += OnUtilsOnYesNoRequired;
+        ServerState.Instance.PropertyChanged                      += OnInstanceOnPropertyChanged;
+        ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent += OnCmdProcessorGeneralOnOnQueueStateChangedEvent;
+    }
 
-            ServerState.Instance.PropertyChanged += (sender, e) =>
-            {
-                if (e.PropertyName == "StartupFailedMessage" && ServerState.Instance.StartupFailed)
-                {
-                    Console.WriteLine("Startup failed! Error message: " + ServerState.Instance.StartupFailedMessage);
-                }
-            };
-            ShokoService.CmdProcessorGeneral.OnQueueStateChangedEvent +=
-                ev => Console.WriteLine($"General Queue state change: {ev.QueueState.formatMessage()}");
-        }
+    private static void OnCmdProcessorGeneralOnOnQueueStateChangedEvent(QueueStateEventArgs ev) 
+        => Console.WriteLine($"General Queue state change: {ev.QueueState.formatMessage()}");
 
-        public override async Task StopAsync(CancellationToken cancellationToken)
-        {
-            ShokoService.CancelAndWaitForQueues();
-            await base.StopAsync(cancellationToken);
-        }
+    private static void OnInstanceOnPropertyChanged(object? _, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == "StartupFailedMessage" && ServerState.Instance.StartupFailed)
+            Console.WriteLine("Startup failed! Error message: " + ServerState.Instance.StartupFailedMessage);
+    }
+
+    private static void OnUtilsOnYesNoRequired(object? _, Utils.CancelReasonEventArgs e) => e.Cancel = true;
+
+    private void OnInstanceOnServerShutdown(object? _, EventArgs eventArgs) => _appLifetime?.StopApplication();
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        ShokoService.CancelAndWaitForQueues();
+        await base.StopAsync(cancellationToken);
     }
 }
