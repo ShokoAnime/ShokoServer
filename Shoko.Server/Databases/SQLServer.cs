c18d255f33ec5b8bb350ad7ead389b6898e20aa8
Kerberos <57227244+krbrs@users.noreply.github.com>
2022-10-10 14:53:48 +0200

    File_Source DB Migrations
    Added DB migrations for File_Source to match the old entries with current values

--- Shoko.Server/Databases/SQLServer.cs
+++ Shoko.Server/Databases/SQLServer.cs
@@ -22,7 +22,7 @@ namespace Shoko.Server.Databases;
 public class SQLServer : BaseDatabase<SqlConnection>, IDatabase
 {
     public string Name { get; } = "SQLServer";
-    public int RequiredVersion { get; } = 94;
+    public int RequiredVersion { get; } = 95;
 
     public void BackupDatabase(string fullfilename)
     {
@@ -644,6 +644,7 @@ public class SQLServer : BaseDatabase<SqlConnection>, IDatabase
         new DatabaseCommand(94, 8, "DROP TABLE CrossRef_AniDB_Trakt"),
         new DatabaseCommand(94, 9, "DROP TABLE Trakt_Friend"),
         new DatabaseCommand(94, 10, "CREATE UNIQUE INDEX UIX_AniDB_File_FileID ON AniDB_File(FileID);"),
+        new DatabaseCommand(95, 1, "UPDATE AniDB_File SET File_Source = Web WHERE File_Source = www; UPDATE AniDB_File SET File_Source = BluRay WHERE File_Source = Blu-ray; UPDATE AniDB_File SET File_Source = LaserDisc WHERE File_Source = LD; UPDATE AniDB_File SET File_Source = Unknown WHERE File_Source = unknown;")
     };
 
     private static Tuple<bool, string> DropDefaultsOnAnimeEpisode_User(object connection)
