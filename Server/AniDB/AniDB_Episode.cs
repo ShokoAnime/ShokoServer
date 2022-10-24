88a2fb652e09f56659c02d10108410ba76a002df
da3dsoul <da3dsoul@gmail.com>
2022-10-24 16:41:01 -0400

    Add Equals() to AniDB_Episode

--- Server/AniDB/AniDB_Episode.cs
+++ Server/AniDB/AniDB_Episode.cs
@@ -19,5 +19,49 @@ namespace Shoko.Models.Server
         public DateTime DateTimeUpdated { get; set; }
 
         #endregion
+
+        protected bool Equals(AniDB_Episode other)
+        {
+            return EpisodeID == other.EpisodeID && AnimeID == other.AnimeID && LengthSeconds == other.LengthSeconds &&
+                   Rating == other.Rating && Votes == other.Votes && EpisodeNumber == other.EpisodeNumber &&
+                   EpisodeType == other.EpisodeType && Description == other.Description && AirDate == other.AirDate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((AniDB_Episode)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = EpisodeID;
+                hashCode = (hashCode * 397) ^ AnimeID;
+                hashCode = (hashCode * 397) ^ LengthSeconds;
+                hashCode = (hashCode * 397) ^ (Rating != null ? Rating.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Votes != null ? Votes.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ EpisodeNumber;
+                hashCode = (hashCode * 397) ^ EpisodeType;
+                hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ AirDate;
+                return hashCode;
+            }
+        }
     }
 }
