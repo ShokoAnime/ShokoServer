ef98c1792dfda771173a1a8bcb17cf115358fa30
Mikal Stordal <mikalstordal@gmail.com>
2022-11-17 22:02:49 +0100

    fix: inherit user/group ids from environment if set to a negative value.

--- Shoko.Server/Utilities/LinuxFS.cs
+++ Shoko.Server/Utilities/LinuxFS.cs
@@ -10,11 +10,20 @@ public static class LinuxFS
         return Environment.OSVersion.Platform == PlatformID.MacOSX || Environment.OSVersion.Platform == PlatformID.Unix;
     }
 
-    public static void SetLinuxPermissions(string path, int uid, int gid, int mode)
+    public static void SetLinuxPermissions(string path, long uid, long gid, int mode)
     {
         if (!CanRun())
         {
             return;
+        }
+
+        if (uid < 0 || gid < 0)
+        {
+            var user = UnixUserInfo.GetRealUser();
+            if (uid < 0)
+                uid = user.UserId;
+            if (gid < 0)
+                gid = user.GroupId;
         }
 
         var file = new UnixFileInfo(path);
