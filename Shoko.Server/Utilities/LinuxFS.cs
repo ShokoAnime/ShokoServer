e967007d6afd10a08545828d93cee8e4a37ec566
da3dsoul <da3dsoul@gmail.com>
2022-11-18 10:18:26 -0500

    Better comments and logic for Linux Permissions

--- Shoko.Server/Utilities/LinuxFS.cs
+++ Shoko.Server/Utilities/LinuxFS.cs
@@ -20,6 +20,9 @@ public static class LinuxFS
 
         RealUser ??= UnixUserInfo.GetRealUser();
 
+        // if uid is not valid (< 0) and we are not root (RealUser.UserId != 0), then set it to the current user
+        // if uid = 0 (trying to set as root), and we are not root (RealUser.UserId != 0), then set it to the current user
+        // if uid = 0 and we are root (RealUser.UserId = 0), then nothing changes, and it can use those values
         if (uid <= 0 && RealUser.UserId != 0)
             uid = RealUser.UserId;
 
@@ -27,33 +30,29 @@ public static class LinuxFS
             gid = RealUser.GroupId;
 
         var file = new UnixFileInfo(path);
-        var changed = false;
-        if (file.OwnerUserId != uid || file.OwnerGroupId != gid)
-        {
-            file.SetOwner(uid, gid);
-            changed = true;
-        }
-        if (mode > 0 && file.FileAccessPermissions != (FileAccessPermissions)mode)
-        {
-            file.FileAccessPermissions = (FileAccessPermissions)mode;
-            changed = true;
-        }
-        if (changed)
-        {
-            // guarantee immediate flush
-            file.Refresh();
+        // new user ID is valid (>= 0) and has changed (or same for group ID)
+        var shouldChangeOwner = uid >= 0 && uid != file.OwnerUserId || gid >= 0 && file.OwnerGroupId != gid;
+        // mode is valid and different
+        var shouldChangePermissions = mode > 0 && file.FileAccessPermissions != (FileAccessPermissions)mode;
+
+        if (shouldChangeOwner) file.SetOwner(uid, gid);
+        if (shouldChangePermissions) file.FileAccessPermissions = (FileAccessPermissions)mode;
+        // if we didn't change anything, then return
+        if (!shouldChangeOwner && !shouldChangePermissions) return;
+
+        // guarantee immediate flush
+        file.Refresh();
             
-            // spinwait to ensure the status is updated
-            var time = 2000;
-            var interval = 50;
-            var limit = time / interval;
-            var count = 0;
-            while (file.OwnerUserId != uid || file.OwnerGroupId != gid || file.FileAccessPermissions != (FileAccessPermissions)mode && count < limit)
-            {
-                Thread.Sleep(interval);
-                count++;
-                file.Refresh();
-            }
+        // spinwait to ensure the status is updated
+        const int Time = 2000;
+        const int Interval = 50;
+        const int Limit = Time / Interval;
+        var count = 0;
+        while (file.OwnerUserId != uid || file.OwnerGroupId != gid || file.FileAccessPermissions != (FileAccessPermissions)mode && count < Limit)
+        {
+            Thread.Sleep(Interval);
+            count++;
+            file.Refresh();
         }
     }
 }
