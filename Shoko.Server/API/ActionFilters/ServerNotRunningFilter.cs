d2a30269cfd61dc953ed1fc4777caf5acc78c585
Mikal Stordal <mikalstordal@gmail.com>
2022-12-01 22:03:44 +0100

    fix: respond with 503 when server is not started

--- Shoko.Server/API/ActionFilters/ServerNotRunningFilter.cs
+++ Shoko.Server/API/ActionFilters/ServerNotRunningFilter.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Shoko.Server.API.Annotations;
@@ -14,7 +15,10 @@ public class ServerNotRunningFilter : IActionFilter
         if (!ServerState.Instance.ServerOnline && !exempt)
         {
             context.Result = new BadRequestObjectResult(
-                "The Server is not running. Use the First Time Setup Wizard or check your settings and logs to ensure the server is starting correctly.");
+                "The Server is not running. Use the First Time Setup Wizard or check your settings and logs to ensure the server is starting correctly.")
+            {
+                StatusCode = (int)HttpStatusCode.ServiceUnavailable,
+            };
         }
     }
 
