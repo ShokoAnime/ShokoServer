f07d1b34d4a500f04cdce82547b2f159049bc9ad
Mikal Stordal <mikalstordal@gmail.com>
2023-02-11 20:22:04 +0100

    Update data source type

--- Enums/ShokoInternal.cs
+++ Enums/ShokoInternal.cs
@@ -36,10 +36,61 @@ namespace Shoko.Models.Enums
         NoFiles = 3
     }
 
+    /// <summary>
+    /// Available data sources to chose from.
+    /// </summary>
+    [Flags]
     public enum DataSourceType
     {
+        /// <summary>
+        /// No source.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// AniDB.
+        /// </summary>
         AniDB = 1,
-        TheTvDB = 2
+
+        /// <summary>
+        /// The Tv Database (TvDB).
+        /// </summary>
+        TvDB = 2,
+
+        /// <summary>
+        /// The Movie Database (TMDB).
+        /// </summary>
+        TMDB = 4,
+
+        /// <summary>
+        /// Trakt.
+        /// </summary>
+        Trakt = 8,
+
+        /// <summary>
+        /// My Anime List (MAL).
+        /// </summary>
+        MAL = 16,
+
+        /// <summary>
+        /// AniList (AL).
+        /// </summary>
+        AniList = 32,
+
+        /// <summary>
+        /// Animeshon.
+        /// </summary>
+        Animeshon = 64,
+
+        /// <summary>
+        /// Kitsu.
+        /// </summary>
+        Kitsu = 128,
+
+        /// <summary>
+        /// Shoko.
+        /// </summary>
+        Shoko = 1024,
     }
 
     public enum EpisodeDisplayStyle
