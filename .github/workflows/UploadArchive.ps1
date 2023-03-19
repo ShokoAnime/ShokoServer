54b23ad32b8e1638c2c5ba7c7cc3115292d798a2
Mikal Stordal <mikalstordal@gmail.com>
2023-03-20 00:33:49 +0100

    chore: update upload location for dailies

--- .github/workflows/UploadArchive.ps1
+++ .github/workflows/UploadArchive.ps1
@@ -9,4 +9,4 @@ $ftp_server = $env:FTP_SERVER;
 $current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($user, $password);
-$client.UploadFile([string]($ftp_server) + "/shoko-server/daily/" + $filename, [string]($current) + "\\ShokoServer.zip");
+$client.UploadFile([string]($ftp_server) + "/files/shoko-server/daily/" + $filename, [string]($current) + "\\ShokoServer.zip");
