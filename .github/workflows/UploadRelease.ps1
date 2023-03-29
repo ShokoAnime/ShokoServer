0e95d0c5c08ebfa60a49bdb308190222d967cabe
Mikal Stordal <mikalstordal@gmail.com>
2023-03-29 20:49:13 +0200

    misc: fix uploading on linux it seems like pwsh is more strict about the protocol on linux compared windows.

--- .github/workflows/UploadRelease.ps1
+++ .github/workflows/UploadRelease.ps1
@@ -1,6 +1,6 @@
 Param(
-    [string] $remote = "Artifact.bin",
-    [string] $local = "Artifact.bin"
+    [string]$remote = "Artifact.bin",
+    [string]$local = "Artifact.bin"
 )
 
 $username = $env:FTP_USERNAME;
@@ -10,4 +10,15 @@ $ftp_server = $env:FTP_SERVER;
 $current = [string](Get-Location);
 $client = New-Object System.Net.WebClient;
 $client.Credentials = New-Object System.Net.NetworkCredential($username, $password);
-$client.UploadFile("$ftp_server/files/shoko-server/$remote", "$current\\$local");
+
+Write-Output "Starting file upload...";
+Write-Output "Remote file: $remote";
+Write-Output "Local file: $local";
+
+try {
+    $client.UploadFile("ftp://$ftp_server/files/shoko-server/$remote", "$current/$local");
+    Write-Output "Done!";
+}
+catch {
+    Write-Error $_.Exception.Message;
+}
