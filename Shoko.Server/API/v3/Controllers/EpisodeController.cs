13fdd558912e41071f4d5214f463df3c41e670d0
Mikal Stordal <mikalstordal@gmail.com>
2023-04-17 00:52:48 +0200

    feat: add get all episode endpoints Added three new endpoints to get all shoko, anidb and tvdb episodes. Because i needed them for debugging purposes.

--- Shoko.Server/API/v3/Controllers/EpisodeController.cs
+++ Shoko.Server/API/v3/Controllers/EpisodeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shoko.Commons.Extensions;
+using Shoko.Plugin.Abstractions.DataModels;
+using Shoko.Plugin.Abstractions.Extensions;
 using Shoko.Server.API.Annotations;
 using Shoko.Server.API.ModelBinders;
 using Shoko.Server.API.v3.Helpers;
@@ -13,6 +15,12 @@ using Shoko.Server.API.v3.Models.Shoko;
 using Shoko.Server.Models;
 using Shoko.Server.Repositories;
 using Shoko.Server.Settings;
+using Shoko.Server.Utilities;
+
+using EpisodeType = Shoko.Server.API.v3.Models.Shoko.EpisodeType;
+using AniDBEpisodeType = Shoko.Models.Enums.EpisodeType;
+using Shoko.Models.Server;
+using System.Collections.Concurrent;
 
 namespace Shoko.Server.API.v3.Controllers;
 
@@ -31,6 +39,245 @@ public class EpisodeController : BaseController
     internal const string AnidbNotFoundForAnidbEpisodeID = "No Episode.Anidb entry for the given anidbEpisodeID";
 
     internal const string EpisodeForbiddenForUser = "Accessing Episode is not allowed for the current user";
+
+    /// <summary>
+    /// Get all <see cref="Episode"/>s for the given filter.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="Filter"/> or <see cref="Group"/> is irrelevant at this level.
+    /// </remarks>
+    /// <param name="pageSize">The page size. Set to <code>0</code> to disable pagination.</param>
+    /// <param name="page">The page index.</param>
+    /// <param name="includeMissing">Include missing episodes in the list.</param>
+    /// <param name="includeHidden">Include hidden episodes in the list.</param>
+    /// <param name="includeDataFrom">Include data from selected <see cref="DataSource"/>s.</param>
+    /// <param name="includeWatched">Include watched episodes in the list.</param>
+    /// <param name="type">Filter episodes by the specified <see cref="EpisodeType"/>s.</param>
+    /// <param name="search">An optional search query to filter episodes based on their titles.</param>
+    /// <param name="fuzzy">Indicates that fuzzy-matching should be used for the search query.</param>
+    /// <returns>A list of episodes based on the specified filters.</returns>
+    [HttpGet]
+    public ActionResult<ListResult<Episode>> GetAllEpisodes(
+        [FromQuery] [Range(0, 1000)] int pageSize = 20, [FromQuery] [Range(1, int.MaxValue)] int page = 1,
+        [FromQuery] IncludeOnlyFilter includeMissing = IncludeOnlyFilter.False, [FromQuery] IncludeOnlyFilter includeHidden = IncludeOnlyFilter.False,
+        [FromQuery, ModelBinder(typeof(CommaDelimitedModelBinder))] HashSet<DataSource> includeDataFrom = null,
+        [FromQuery] IncludeOnlyFilter includeWatched = IncludeOnlyFilter.True,
+        [FromQuery, ModelBinder(typeof(CommaDelimitedModelBinder))] HashSet<EpisodeType> type = null,
+        [FromQuery] string search = null, [FromQuery] bool fuzzy = true)
+    {
+        var user = User;
+        var hasSearch = !string.IsNullOrWhiteSpace(search);
+        var allowedSeriesDict = new ConcurrentDictionary<int, bool>();
+        var episodeAnidbDict = new Dictionary<SVR_AnimeEpisode, AniDB_Episode>();
+        var episodes = RepoFactory.AnimeEpisode.GetAll()
+            .AsParallel()
+            .Where(episode =>
+            {
+                // Only show episodes the user is allowed to view.
+                if (!allowedSeriesDict.TryGetValue(episode.AnimeSeriesID, out var isAllowed))
+                    allowedSeriesDict.TryAdd(episode.AnimeSeriesID, isAllowed = user.AllowedSeries(episode.GetAnimeSeries()));
+                if (!isAllowed)
+                    return false;
+
+                // Make sure we have an anidb entry for the episode, otherwise,
+                // just hide it.
+                var anidb = episode.AniDB_Episode;
+                if (anidb == null)
+                    return false;
+
+                // Filter by hidden state, if spesified
+                if (includeHidden != IncludeOnlyFilter.True)
+                {
+                    // If we should hide hidden episodes and the episode is hidden, then hide it.
+                    // Or if we should only show hidden episodes and the episode is not hidden, then hide it.
+                    var shouldHideHidden = includeHidden == IncludeOnlyFilter.False;
+                    if (shouldHideHidden == episode.IsHidden)
+                        return false;
+                }
+
+                // Filter by episode type, if specified
+                if (type != null && type.Count > 0)
+                {
+                    var mappedType = Episode.MapAniDBEpisodeType((AniDBEpisodeType)anidb.EpisodeType);
+                    if (!type.Contains(mappedType))
+                        return false;
+                }
+
+                // Filter by availability, if specified
+                if (includeMissing != IncludeOnlyFilter.True)
+                {
+                    // If we should hide missing episodes and the episode has no files, then hide it.
+                    // Or if we should only show missing episodes and the episode has files, the hide it.
+                    var shouldHideMissing = includeMissing == IncludeOnlyFilter.False;
+                    var noFiles = episode.GetVideoLocals().Count == 0;
+                    if (shouldHideMissing == noFiles)
+                        return false;
+                }
+
+                // Filter by user watched status, if specified
+                if (includeWatched != IncludeOnlyFilter.True)
+                {
+                    // If we should hide watched episodes and the episode is watched, then hide it.
+                    // Or if we should only show watched episodes and the the episode is not watched, then hide it.
+                    var shouldHideWatched = includeWatched == IncludeOnlyFilter.False;
+                    var isWatched = episode.GetUserRecord(user.JMMUserID)?.WatchedDate != null;
+                    if (shouldHideWatched == isWatched)
+                        return false;
+                }
+
+                if (!hasSearch)
+                    episodeAnidbDict.Add(episode, anidb);
+                return true;
+            });
+        if (hasSearch)
+        {
+            var languages = SettingsProvider.GetSettings()
+                .LanguagePreference
+                .Select(lang => lang.GetTitleLanguage())
+                .Concat(new TitleLanguage[] { TitleLanguage.English, TitleLanguage.Romaji })
+                .ToHashSet();
+            return episodes
+                .Search(
+                    search,
+                    ep => RepoFactory.AniDB_Episode_Title.GetByEpisodeID(ep.AniDB_EpisodeID)
+                        .Where(title => title != null && languages.Contains(title.Language))
+                        .Select(title => title.Title)
+                        .Append(ep.Title)
+                        .Distinct()
+                        .ToList(),
+                    fuzzy
+                )
+                // Cast to list, so it runs the query, then convert to a list-
+                // result afterwards, so it will use the count from the list.
+                .ToList()
+                .ToListResult(a => new Episode(HttpContext, a.Result, includeDataFrom), page, pageSize);
+        }
+
+        return episodes
+            // Order the episodes since we're not using the search ordering.
+            .OrderBy(episode => episode.AnimeSeriesID)
+            .ThenBy(episode => episodeAnidbDict[episode].EpisodeType)
+            .ThenBy(episode => episodeAnidbDict[episode].EpisodeNumber)
+            // Cast to list, so it runs the query, then convert to a list-result
+            // afterwards, so it will use the count from the list.
+            .ToList()
+            .ToListResult(a => new Episode(HttpContext, a, includeDataFrom), page, pageSize);
+    }
+
+    /// <summary>
+    /// Get all <see cref="Episode.AniDB"/>s. Admins only.
+    /// </summary>
+    /// <param name="pageSize">The page size. Set to <code>0</code> to disable pagination.</param>
+    /// <param name="page">The page index.</param>
+    /// <param name="type">Filter episodes by the specified <see cref="EpisodeType"/>s.</param>
+    /// <returns></returns>
+    [HttpGet("AniDB")]
+    [Authorize("admin")]
+    public ActionResult<ListResult<Episode.AniDB>> GetAllAniDBEpisodes(
+        [FromQuery] [Range(0, 1000)] int pageSize = 20, [FromQuery] [Range(1, int.MaxValue)] int page = 1,
+        [FromQuery, ModelBinder(typeof(CommaDelimitedModelBinder))] HashSet<EpisodeType> type = null)
+    {
+        var user = User;
+        var allowedAnimeDict = new ConcurrentDictionary<int, bool>();
+        return RepoFactory.AniDB_Episode.GetAll()
+            .AsParallel()
+            .Where(episode =>
+            {
+                // Only show episodes the user is allowed to view.
+                if (!allowedAnimeDict.TryGetValue(episode.AnimeID, out var isAllowed))
+                {
+                    // If this is an episode not tied to a missing anime, then
+                    // just hide it.
+                    var anime = RepoFactory.AniDB_Anime.GetByID(episode.AnimeID);
+                    isAllowed = anime == null ? false : user.AllowedAnime(anime);
+
+                    allowedAnimeDict.TryAdd(episode.AnimeID, isAllowed);
+                }
+                if (!isAllowed)
+                    return false;
+
+                // Filter by episode type, if specified
+                if (type != null && type.Count > 0)
+                {
+                    var mappedType = Episode.MapAniDBEpisodeType((AniDBEpisodeType)episode.EpisodeType);
+                    if (!type.Contains(mappedType))
+                        return false;
+                }
+
+                return true;
+            })
+            // Order the episodes.
+            .OrderBy(episode => episode.AnimeID)
+            .ThenBy(episode => episode.EpisodeType)
+            .ThenBy(episode => episode.EpisodeNumber)
+            // Cast to list, so it runs the query, then convert to a list-result
+            // afterwards, so it will use the count from the list.
+            .ToList()
+            .ToListResult(episode => new Episode.AniDB(episode), page, pageSize);
+    }
+
+    /// <summary>
+    /// Get all <see cref="Episode.TvDB"/>s. Admins only.
+    /// </summary>
+    /// <remarks>
+    /// It's admins only since i don't want to add the logic to
+    /// </remarks>
+    /// <param name="pageSize">The page size. Set to <code>0</code> to disable pagination.</param>
+    /// <param name="page">The page index.</param>
+    /// <returns></returns>
+    [HttpGet("TvDB")]
+    public ActionResult<ListResult<Episode.TvDB>> GetAllTvDBEpisodes(
+        [FromQuery] [Range(0, 1000)] int pageSize = 20, [FromQuery] [Range(1, int.MaxValue)] int page = 1)
+    {
+        var user = User;
+        var isAdmin = user.IsAdmin == 1;
+        var allowedShowDict = new ConcurrentDictionary<int, bool>();
+        return RepoFactory.TvDB_Episode.GetAll()
+            .Where(episode =>
+            {
+                // Only show episodes the user is allowed to view.
+                if (!allowedShowDict.TryGetValue(episode.SeriesID, out var isAllowed))
+                {
+                    // If this is an episode not tied to a missing show, then
+                    // just hide it.
+                    var show = RepoFactory.TvDB_Series.GetByTvDBID(episode.SeriesID);
+                    if (show == null)
+                    {
+                        isAllowed = false;
+                        goto addValue;
+                    }
+
+                    // If there are no cross-references, then hide it if the
+                    // user is not an admin.
+                    var xref = RepoFactory.CrossRef_AniDB_TvDB.GetByTvDBID(episode.SeriesID)
+                        .FirstOrDefault();
+                    if (xref == null)
+                    {
+                        isAllowed = isAdmin;
+                        goto addValue;
+                    }
+
+                    // Or if the cross-reference is broken then also hide it if
+                    // the user is not an admin, otherwise check if the user can
+                    // view the series.
+                    var anime = RepoFactory.AniDB_Anime.GetByAnimeID(xref.AniDBID);
+                    isAllowed = anime == null ? isAdmin : user.AllowedAnime(anime);
+
+                    addValue: allowedShowDict.TryAdd(episode.SeriesID, isAllowed);
+                }
+                if (!isAllowed)
+                    return false;
+
+                return true;
+            })
+            .OrderBy(episode => episode.SeriesID)
+            .ThenBy(episode => episode.SeasonNumber)
+            .ThenBy(episode => episode.EpisodeNumber)
+            // Cast to list, so it runs the query, then convert to a list-result
+            // afterwards, so it will use the count from the list.
+            .ToList()
+            .ToListResult(episode => new Episode.TvDB(episode), page, pageSize);
+    }
 
     /// <summary>
     /// Get the <see cref="Episode"/> entry for the given <paramref name="episodeID"/>.
