a0129483f4ce5e76889293f04a1a791ddc0ea921
krbrs <57227244+krbrs@users.noreply.github.com>
2023-05-25 12:49:18 +0200

    Fix Usings

--- Shoko.Server/Providers/TraktTV/TraktTVHelper.cs
+++ Shoko.Server/Providers/TraktTV/TraktTVHelper.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Sentry;
+using NHibernate;
 using Shoko.Commons.Extensions;
 using Shoko.Models.Client;
 using Shoko.Models.Enums;
@@ -20,7 +20,6 @@ using Shoko.Server.Providers.TraktTV.Contracts;
 using Shoko.Server.Repositories;
 using Shoko.Server.Settings;
 using Shoko.Server.Utilities;
-using ISession = NHibernate.ISession;
 
 namespace Shoko.Server.Providers.TraktTV;
 
