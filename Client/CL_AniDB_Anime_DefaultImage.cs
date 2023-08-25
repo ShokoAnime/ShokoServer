97bedd206afa7970878e27b9685376060b3c5f8f
Mikal Stordal <mikalstordal@gmail.com>
2023-08-26 01:11:16 +0200

    Remove AniDB_Anime_DefaultImage in favour of AniDB_Anime_PreferredImage

--- Client/CL_AniDB_Anime_DefaultImage.cs
+++ Client/CL_AniDB_Anime_DefaultImage.cs
@@ -3,8 +3,14 @@ using Shoko.Models.Server;
 
 namespace Shoko.Models.Client
 {
-    public class CL_AniDB_Anime_DefaultImage : AniDB_Anime_DefaultImage, ICloneable
+    public class CL_AniDB_Anime_DefaultImage : ICloneable
     {
+        public int AniDB_Anime_DefaultImageID { get; set; }
+        public int AnimeID { get; set; }
+        public int ImageParentID { get; set; }
+        public int ImageParentType { get; set; }
+        public int ImageType { get; set; }
+
         public MovieDB_Poster MoviePoster { get; set; }
         public MovieDB_Fanart MovieFanart { get; set; }
 
@@ -12,28 +18,24 @@ namespace Shoko.Models.Client
         public TvDB_ImageFanart TVFanart { get; set; }
         public TvDB_ImageWideBanner TVWideBanner { get; set; }
 
-        public CL_AniDB_Anime_DefaultImage(AniDB_Anime_DefaultImage obj)
-        {
-            AniDB_Anime_DefaultImageID = obj.AniDB_Anime_DefaultImageID;
-            AnimeID = obj.AnimeID;
-            ImageParentID = obj.ImageParentID;
-            ImageParentType = obj.ImageParentType;
-            ImageType = obj.ImageType;
-        }
-
         public CL_AniDB_Anime_DefaultImage()
         {
         }
 
-        public new object Clone()
+        public object Clone()
         {
-            var image = new CL_AniDB_Anime_DefaultImage(this)
+            var image = new CL_AniDB_Anime_DefaultImage()
             {
-                MoviePoster = (MovieDB_Poster) MoviePoster?.Clone(),
-                MovieFanart = (MovieDB_Fanart) MovieFanart?.Clone(),
-                TVPoster = (TvDB_ImagePoster) TVPoster?.Clone(),
-                TVFanart = (TvDB_ImageFanart) TVFanart?.Clone(),
-                TVWideBanner = (TvDB_ImageWideBanner) TVWideBanner?.Clone()
+                AniDB_Anime_DefaultImageID = AniDB_Anime_DefaultImageID,
+                AnimeID = AnimeID,
+                ImageParentID = ImageParentID,
+                ImageParentType = ImageParentType,
+                ImageType = ImageType,
+                MoviePoster = (MovieDB_Poster)MoviePoster?.Clone(),
+                MovieFanart = (MovieDB_Fanart)MovieFanart?.Clone(),
+                TVPoster = (TvDB_ImagePoster)TVPoster?.Clone(),
+                TVFanart = (TvDB_ImageFanart)TVFanart?.Clone(),
+                TVWideBanner = (TvDB_ImageWideBanner)TVWideBanner?.Clone()
             };
 
             return image;
