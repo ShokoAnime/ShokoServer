4b97f613ae44126d0bc6db4095407ca32f2f122d
da3dsoul <da3dsoul@gmail.com>
2024-03-23 22:14:42 -0400

    Add Used Implicitly to AniDB Requests

--- Shoko.Server/Providers/AniDB/Interfaces/IRequest.cs
+++ Shoko.Server/Providers/AniDB/Interfaces/IRequest.cs
@@ -1,10 +1,13 @@
-﻿namespace Shoko.Server.Providers.AniDB.Interfaces;
+﻿using JetBrains.Annotations;
+
+namespace Shoko.Server.Providers.AniDB.Interfaces;
 
 public interface IRequest<out T, T1> where T : IResponse<T1> where T1 : class
 {
     T Send();
 }
 
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 public interface IRequest
 {
     object Send();
