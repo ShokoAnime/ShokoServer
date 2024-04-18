05c1d85e1293e63488696635161a5460979d1131
Mikal Stordal <mikalstordal@gmail.com>
2024-04-18 22:28:27 +0000

    fix: update remaining net6 → net8

--- Publish_Release.bat
+++ Publish_Release.bat
@@ -1,1 +1,1 @@
-dotnet publish -c Release -r win10-x64 --self-contained true -f net6.0-windows Shoko.TrayService
+dotnet publish -c Release -r win10-x64 --self-contained true -f net8.0-windows Shoko.TrayService
