601c25ff7fdedfbe92948c8e81662ade6f7804e4
Terrails <github@sl.terrails.com>
2024-05-20 00:06:31 +0200

    skip already processed messages and add force refresh to notify job

--- Shoko.Server/Scheduling/Jobs/AniDB/GetAniDBNotifyJob.cs
+++ Shoko.Server/Scheduling/Jobs/AniDB/GetAniDBNotifyJob.cs
@@ -23,6 +23,7 @@ public class GetAniDBNotifyJob : BaseJob
     private readonly IRequestFactory _requestFactory;
     private readonly ISchedulerFactory _schedulerFactory;
     private readonly ISettingsProvider _settingsProvider;
+    public bool ForceRefresh { get; set; }
 
     public override string TypeName => "Get AniDB Unread Notifications and Messages";
 
@@ -48,7 +49,7 @@ public class GetAniDBNotifyJob : BaseJob
 
             // if we have run this in the last 12 hours and are not forcing it, then exit
             var tsLastRun = DateTime.Now - sched.LastUpdate;
-            if (tsLastRun.TotalHours < freqHours) return;
+            if (!ForceRefresh && tsLastRun.TotalHours < freqHours) return;
         }
 
         sched.LastUpdate = DateTime.Now;
@@ -57,8 +58,6 @@ public class GetAniDBNotifyJob : BaseJob
         var requestCount = _requestFactory.Create<RequestGetNotifyCount>(r => r.Buddies = false); // we do not care about the number of online buddies
         var responseCount = requestCount.Send();
         if (responseCount?.Response == null) return;
-
-        var scheduler = await _schedulerFactory.GetScheduler();
 
         var unreadCount = responseCount.Response.Files + responseCount.Response.Messages;
         if (unreadCount > 0)
@@ -75,6 +74,12 @@ public class GetAniDBNotifyJob : BaseJob
                 var type = RepoFactory.AniDB_NotifyQueue.GetByTypeID(notify.Type, notify.ID);
                 if (type is not null) continue; // if we already have it in the queue
 
+                if (!ForceRefresh && notify.Type == AniDBNotifyType.Message)
+                {
+                    var msg = RepoFactory.AniDB_Message.GetByMessageId(notify.ID);
+                    if (msg is not null) continue; // if we have already processed it
+                }
+
                 // save to db queue
                 type = new()
                 {
@@ -90,6 +95,7 @@ public class GetAniDBNotifyJob : BaseJob
         var messages = RepoFactory.AniDB_NotifyQueue.GetByType(AniDBNotifyType.Message);
         if (messages.Count > 0)
         {
+            var scheduler = await _schedulerFactory.GetScheduler();
             foreach (var msg in messages)
                 await scheduler.StartJob<GetAniDBMessageJob>(r => r.MessageID = msg.ID);
         }
