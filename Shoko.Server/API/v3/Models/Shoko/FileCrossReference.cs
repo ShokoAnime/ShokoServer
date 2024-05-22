0b04e197da3a9acc82a79ec759cb9848db207dda
Mikal Stordal <mikalstordal@gmail.com>
2024-05-22 20:44:58 +0000

    fix: better ordering of xrefs in APIv3

--- Shoko.Server/API/v3/Models/Shoko/FileCrossReference.cs
+++ Shoko.Server/API/v3/Models/Shoko/FileCrossReference.cs
@@ -86,6 +86,12 @@ public class FileCrossReference
         /// The raw percentage to "group" the cross-references by.
         /// </summary>
         public int Size { get; set; }
+
+        /// <summary>
+        /// The assumed number of groups in the release, to group the
+        /// cross-references by.
+        /// </summary>
+        public int Group { get; set; }
     }
 
     /// <summary>
@@ -109,6 +115,26 @@ public class FileCrossReference
         public List<int> TvDB { get; set; }
     }
 
+    private static int PercentageToFileCount(int percentage)
+        => percentage switch
+        {
+            100 => 1,
+            50 => 2,
+            34 => 3,
+            33 => 3,
+            25 => 4,
+            20 => 5,
+            17 => 6,
+            16 => 6,
+            15 => 7,
+            14 => 7,
+            13 => 8,
+            12 => 8,
+            // 11 => 9, // 9 also overlaps with 12%, so we skip this for now.
+            10 => 10,
+            _ => 0, // anything below this we can't reliably measure.
+        };
+
     public static List<FileCrossReference> From(IEnumerable<SVR_CrossRef_File_Episode> crossReferences)
         => crossReferences
                 .Select(xref =>
@@ -116,20 +142,31 @@ public class FileCrossReference
                     // Percentages.
                     Tuple<int, int> percentage = new(0, 100);
                     int? releaseGroup = xref.CrossRefSource == (int)CrossRefSource.AniDB ? RepoFactory.AniDB_File.GetByHashAndFileSize(xref.Hash, xref.FileSize)?.GroupID ?? 0 : null;
-                    if (xref.Percentage < 100)
+                    var assumedFileCount = PercentageToFileCount(xref.Percentage);
+                    if (assumedFileCount > 1)
                     {
                         var xrefs = RepoFactory.CrossRef_File_Episode.GetByEpisodeID(xref.EpisodeID)
                             // Filter to only cross-references which are partially linked in the same number of parts to the episode, and from the same group as the current cross-reference.
-                            .Where(xref2 => xref2.Percentage == xref.Percentage && (xref2.CrossRefSource == (int)CrossRefSource.AniDB ? RepoFactory.AniDB_File.GetByHashAndFileSize(xref2.Hash, xref2.FileSize)?.GroupID ?? -1 : null) == releaseGroup)
-                            // Order by episode order because it may be returned in semi-random order from the cache, and we need them in the order they arrived in from remote.
-                            .OrderBy(xref => xref.EpisodeOrder)
+                            .Where(xref2 => PercentageToFileCount(xref2.Percentage) == assumedFileCount && (xref2.CrossRefSource == (int)CrossRefSource.AniDB ? RepoFactory.AniDB_File.GetByHashAndFileSize(xref2.Hash, xref2.FileSize)?.GroupID ?? -1 : null) == releaseGroup)
+                            // This will order by the "full" episode if the xref is linked to both a "full" episode and "part" episode,
+                            // then fall back on the episode order if either a "full" episode is not available, or if it's for cross-references
+                            // for a single-file-multiple-episodes file.
+                            .Select(xref2 => (
+                                xref: xref2,
+                                episode: RepoFactory.CrossRef_File_Episode.GetByHash(xref2.Hash)
+                                    .FirstOrDefault(xref3 => xref3.Percentage == 100 && (xref3.CrossRefSource == (int)CrossRefSource.AniDB ? RepoFactory.AniDB_File.GetByHashAndFileSize(xref3.Hash, xref3.FileSize)?.GroupID ?? -1 : null) == releaseGroup)?
+                                    .GetEpisode()
+                            ))
+                            .OrderBy(tuple => tuple.episode?.EpisodeTypeEnum)
+                            .ThenBy(tuple => tuple.episode?.EpisodeNumber)
+                            .ThenBy(tuple => tuple.xref.EpisodeOrder)
                             .ToList();
-                        var index = xrefs.FindIndex(xref2 => xref2.CrossRef_File_EpisodeID == xref.CrossRef_File_EpisodeID);
+                        var index = xrefs.FindIndex(tuple => tuple.xref.CrossRef_File_EpisodeID == xref.CrossRef_File_EpisodeID);
                         if (index > 0)
                         {
                             // Note: this is bound to be inaccurate if we don't have all the files linked to the episode locally, but as long
                             // as we have all the needed files/cross-references then it will work 100% of the time (pun intended).
-                            var accumulatedPercentage = xrefs[..index].Sum(xref => xref.Percentage);
+                            var accumulatedPercentage = xrefs[..index].Sum(tuple => tuple.xref.Percentage);
                             var totalPercentage = accumulatedPercentage + xref.Percentage;
                             if (totalPercentage >= 95)
                                 totalPercentage = 100;
@@ -157,6 +194,7 @@ public class FileCrossReference
                             Percentage = new()
                             {
                                 Size = xref.Percentage,
+                                Group = assumedFileCount,
                                 Start = percentage.Item1,
                                 End = percentage.Item2,
                             },
