47f8535eef8356b3e37d5c115ef605811d1bbf1b
da3dsoul <da3dsoul@gmail.com>
2024-07-01 17:59:54 -0400

    Add Indexer to EpisodeCounts

--- Shoko.Plugin.Abstractions/DataModels/EpisodeCounts.cs
+++ Shoko.Plugin.Abstractions/DataModels/EpisodeCounts.cs
@@ -10,4 +10,14 @@ public class EpisodeCounts
     public int Trailers { get; set; }
     public int Others { get; set; }
     public int Parodies { get; set; }
+
+    public int this[EpisodeType type] => type switch
+    {
+        EpisodeType.Episode => Episodes,
+        EpisodeType.Special => Specials,
+        EpisodeType.Credits => Credits,
+        EpisodeType.Trailer => Trailers,
+        EpisodeType.Parody => Parodies,
+        _ => Others
+    };
 }
