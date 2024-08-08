814280559f9ffbaf5a624ab517d35c8682a059ce
Mikal Stordal <mikalstordal@gmail.com>
2024-08-09 01:11:46 +0200

    fix: automagically create series entries if they're not available locally and the `AutomaticallyImportSeries` setting is set to `true` in the settings when fetching the info about which anime has been updated recently. Also modernized the job and the calendar job.

--- Shoko.Server/Scheduling/Jobs/AniDB/GetUpdatedAniDBAnimeJob.cs
+++ Shoko.Server/Scheduling/Jobs/AniDB/GetUpdatedAniDBAnimeJob.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Quartz;
 using Shoko.Models.Server;
 using Shoko.Server.Providers.AniDB.Interfaces;
+using Shoko.Server.Providers.AniDB.Titles;
 using Shoko.Server.Providers.AniDB.UDP.Generic;
 using Shoko.Server.Providers.AniDB.UDP.Info;
 using Shoko.Server.Repositories;
@@ -26,6 +27,7 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
     private readonly IRequestFactory _requestFactory;
     private readonly ISchedulerFactory _schedulerFactory;
     private readonly ISettingsProvider _settingsProvider;
+    private readonly AniDBTitleHelper _titleHelper;
 
     public bool ForceRefresh { get; set; }
 
@@ -38,51 +40,51 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
         _logger.LogInformation("Processing {Job}", nameof(GetUpdatedAniDBAnimeJob));
 
         // check the automated update table to see when the last time we ran this command
-        var sched = RepoFactory.ScheduledUpdate.GetByUpdateType((int)ScheduledUpdateType.AniDBUpdates);
-        if (sched != null)
+        var schedule = RepoFactory.ScheduledUpdate.GetByUpdateType((int)ScheduledUpdateType.AniDBUpdates);
+        if (schedule is not null)
         {
             var settings = _settingsProvider.GetSettings();
             var freqHours = Utils.GetScheduledHours(settings.AniDb.Anime_UpdateFrequency);
 
             // if we have run this in the last 12 hours and are not forcing it, then exit
-            var tsLastRun = DateTime.Now - sched.LastUpdate;
+            var tsLastRun = DateTime.Now - schedule.LastUpdate;
             if (tsLastRun.TotalHours < freqHours && !ForceRefresh) return;
         }
 
         DateTime webUpdateTime;
-        if (sched == null)
+        if (schedule is null)
         {
             // if this is the first time, lets ask for last 3 days
             webUpdateTime = DateTime.UtcNow.AddDays(-3);
 
-            sched = new ScheduledUpdate { UpdateType = (int)ScheduledUpdateType.AniDBUpdates };
+            schedule = new ScheduledUpdate { UpdateType = (int)ScheduledUpdateType.AniDBUpdates };
         }
         else
         {
-            _logger.LogTrace("Last AniDB info update was : {UpdateDetails}", sched.UpdateDetails);
-            webUpdateTime = DateTime.UnixEpoch.AddSeconds(long.Parse(sched.UpdateDetails));
+            _logger.LogTrace("Last AniDB info update was : {UpdateDetails}", schedule.UpdateDetails);
+            webUpdateTime = DateTime.UnixEpoch.AddSeconds(long.Parse(schedule.UpdateDetails));
 
             _logger.LogInformation("{UpdateTime} since last UPDATED command", DateTime.UtcNow - webUpdateTime);
         }
 
-        var (response, countAnime, countSeries) = await Update(webUpdateTime, sched, 0, 0);
+        var (response, countAnime, countSeries) = await Update(webUpdateTime, schedule, 0, 0);
 
         while (response?.Response?.Count > 200)
         {
-            (response, countAnime, countSeries) = await Update(response.Response.LastUpdated, sched, countAnime, countSeries);
+            (response, countAnime, countSeries) = await Update(response.Response.LastUpdated, schedule, countAnime, countSeries);
         }
 
         _logger.LogInformation("Updating {Count} anime records, and {CountSeries} group status records", countAnime,
             countSeries);
     }
 
-    private async Task<(UDPResponse<ResponseUpdatedAnime> response, int countAnime, int countSeries)> Update(DateTime webUpdateTime, ScheduledUpdate sched, int countAnime, int countSeries)
+    private async Task<(UDPResponse<ResponseUpdatedAnime> response, int countAnime, int countSeries)> Update(DateTime webUpdateTime, ScheduledUpdate schedule, int countAnime, int countSeries)
     {
         // get a list of updates from AniDB
         // startTime will contain the date/time from which the updates apply to
         var request = _requestFactory.Create<RequestUpdatedAnime>(r => r.LastUpdated = webUpdateTime);
         var response = request.Send();
-        if (response?.Response == null)
+        if (response?.Response is null)
         {
             return (null, countAnime, countSeries);
         }
@@ -91,9 +93,9 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
 
         // now save the update time from AniDB
         // we will use this next time as a starting point when querying the web cache
-        sched.LastUpdate = DateTime.Now;
-        sched.UpdateDetails = ((int)(response.Response.LastUpdated - DateTime.UnixEpoch).TotalSeconds).ToString();
-        RepoFactory.ScheduledUpdate.Save(sched);
+        schedule.LastUpdate = DateTime.Now;
+        schedule.UpdateDetails = ((int)(response.Response.LastUpdated - DateTime.UnixEpoch).TotalSeconds).ToString();
+        RepoFactory.ScheduledUpdate.Save(schedule);
 
         if (animeIDsToUpdate.Count == 0)
         {
@@ -106,13 +108,27 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
         {
             // update the anime from HTTP
             var anime = RepoFactory.AniDB_Anime.GetByAnimeID(animeID);
-            if (anime == null)
+            if (anime is null)
             {
-                _logger.LogTrace("No local record found for Anime ID: {AnimeID}, so skipping...", animeID);
+                var name = _titleHelper.SearchAnimeID(animeID)?.MainTitle ?? "<Unknown>";
+                if (settings.AniDb.AutomaticallyImportSeries)
+                {
+                    _logger.LogInformation("Scheduling update for anime: {AnimeTitle} ({AnimeID})", name, animeID);
+                    await (await _schedulerFactory.GetScheduler()).StartJob<GetAniDBAnimeJob>(c =>
+                    {
+                        c.AnimeID = animeID;
+                        c.CreateSeriesEntry = true;
+                    });
+                    countAnime++;
+                }
+                else
+                {
+                    _logger.LogTrace("Skipping update for anime because it's not in the local collection: {AnimeTitle} ({AnimeID})", name, animeID);
+                }
                 continue;
             }
 
-            _logger.LogInformation("Scheduling Update for {AnimeID} ", anime.MainTitle);
+            _logger.LogInformation("Scheduling update for anime: {AnimeTitle} ({AnimeID})", anime.MainTitle, animeID);
             var update = RepoFactory.AniDB_AnimeUpdate.GetByAnimeID(animeID);
 
             // but only if it hasn't been recently updated
@@ -131,7 +147,7 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
             // this will allow us to determine which anime has missing episodes
             // we only get by an anime where we also have an associated series
             var ser = RepoFactory.AnimeSeries.GetByAnimeID(animeID);
-            if (ser == null) continue;
+            if (ser is null) continue;
 
             await (await _schedulerFactory.GetScheduler()).StartJob<GetAniDBReleaseGroupStatusJob>(c =>
             {
@@ -143,12 +159,13 @@ public class GetUpdatedAniDBAnimeJob : BaseJob
 
         return (response, countAnime, countSeries);
     }
-    
-    public GetUpdatedAniDBAnimeJob(IRequestFactory requestFactory, ISchedulerFactory schedulerFactory, ISettingsProvider settingsProvider)
+
+    public GetUpdatedAniDBAnimeJob(IRequestFactory requestFactory, ISchedulerFactory schedulerFactory, ISettingsProvider settingsProvider, AniDBTitleHelper titleHelper)
     {
         _requestFactory = requestFactory;
         _schedulerFactory = schedulerFactory;
         _settingsProvider = settingsProvider;
+        _titleHelper = titleHelper;
     }
 
     protected GetUpdatedAniDBAnimeJob()
