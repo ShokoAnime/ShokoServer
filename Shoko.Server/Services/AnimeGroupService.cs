8d9c8b29ab37a7616a5992371c740b0af23fcef6
Filip <github@sl.terrails.com>
2024-08-24 02:34:18 +0200

    partly fix "Has Missing Files" filter and counters (#1170)

--- Shoko.Server/Services/AnimeGroupService.cs
+++ Shoko.Server/Services/AnimeGroupService.cs
@@ -150,7 +150,7 @@ public class AnimeGroupService
     /// </summary>
     public void UpdateStatsFromTopLevel(SVR_AnimeGroup group, bool watchedStats, bool missingEpsStats)
     {
-        if (group?.AnimeGroupParentID == null)
+        if (group is not { AnimeGroupParentID: null })
         {
             return;
         }
