39b4a86cb392f36cd8040d8cb0dbb34fc8b2b0e1
da3dsoul <da3dsoul@gmail.com>
2024-09-11 22:07:12 -0400

    Reschedule Jobs on Login or Ban Failure

--- Shoko.Server/Scheduling/Jobs/BaseJob.cs
+++ Shoko.Server/Scheduling/Jobs/BaseJob.cs
@@ -6,6 +6,7 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Quartz;
+using Shoko.Server.Providers.AniDB;
 using Shoko.Server.Providers.AniDB.UDP.Exceptions;
 
 namespace Shoko.Server.Scheduling.Jobs;
@@ -31,10 +32,13 @@ public abstract class BaseJob : IJob
         {
             await Process();
         }
-        catch (NotLoggedInException ex)
+        catch (NotLoggedInException)
         {
-            // INVALID SESSION or UNKNOWN COMMAND returned, re-fire job immediately to try again with re-login
-            throw new JobExecutionException(ex.Message, ex, refireImmediately: true);
+            await context.RescheduleJob();
+        }
+        catch (AniDBBannedException)
+        {
+            await context.RescheduleJob();
         }
         catch (Exception ex)
         {
