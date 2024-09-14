be661fa69d4639285fecf2af670d216802e776e1
Cazzar <Cazzar@users.noreply.github.com>
2024-09-14 19:07:25 +1000

    Revert "Fix cross-platform plex sync, get episode data using both parent fold…"
    This reverts commit 34f870d559d976a22e28c92a3430bf43f8e8a200.

--- Shoko.Server/Plex/TVShow/SVR_Episode.cs
+++ Shoko.Server/Plex/TVShow/SVR_Episode.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Shoko.Models.Plex.TVShow;
 using Shoko.Server.Models;
 using Shoko.Server.Repositories;
@@ -16,24 +14,8 @@ internal class SVR_Episode : Episode
         Helper = helper;
     }
 
-    public SVR_AnimeEpisode AnimeEpisode
-    {
-        get
-        {
-            var normalizedPath = Media[0].Part[0].File.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
-
-            var finalPath = Path.Combine(
-                Path.GetFileName(Path.GetDirectoryName(normalizedPath)) ?? string.Empty,
-                Path.GetFileName(normalizedPath)
-                );
-
-            var file = RepoFactory.VideoLocalPlace
-                .GetAll()
-                .FirstOrDefault(location => location.FullServerPath?.EndsWith(finalPath, StringComparison.OrdinalIgnoreCase) ?? false);
-
-            return string.IsNullOrEmpty(file?.Hashes.ED2K) ? null : RepoFactory.AnimeEpisode.GetByHash(file.Hashes.ED2K).FirstOrDefault();
-        }
-    }
+    public SVR_AnimeEpisode AnimeEpisode =>
+        RepoFactory.AnimeEpisode.GetByFilename(Path.GetFileName(Media[0].Part[0].File));
 
     public void Unscrobble()
     {
