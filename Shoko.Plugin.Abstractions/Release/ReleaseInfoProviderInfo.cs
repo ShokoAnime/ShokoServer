cb429914c17ae8c7ed6ceefb21748340b46c7a46
Mikal Stordal <mikalstordal@gmail.com>
2024-10-06 22:34:11 +0200

    feat: add plugin based release providers [WIP]
    - Added the initial plugin abstraction definitions for the release providers. Implementation of the service and the anidb release provider will come in a following commit.

---
+++ Shoko.Plugin.Abstractions/Release/ReleaseInfoProviderInfo.cs
@@ -0,0 +1,23 @@
+namespace Shoko.Plugin.Abstractions.Release;
+
+/// <summary>
+/// Contains information about a <see cref="IReleaseInfoProvider"/>.
+/// </summary>
+public class ReleaseInfoProviderInfo
+{
+    /// <summary>
+    /// The <see cref="IReleaseInfoProvider"/> that this info is for.
+    /// </summary>
+    public required IReleaseInfoProvider Provider { get; init; }
+
+    /// <summary>
+    /// Whether or not the provider is enabled for automatic usage.
+    /// </summary>
+    public required bool Enabled { get; init; }
+
+    /// <summary>
+    /// The priority of the provider during automatic usage.
+    /// </summary>
+    public required int Priority { get; init; }
+}
+
