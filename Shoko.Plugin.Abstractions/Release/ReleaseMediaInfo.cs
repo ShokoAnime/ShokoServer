57bcdfac9f73a817b3e2e52b8c55dca384d8a4fe
Mikal Stordal <mikalstordal@gmail.com>
2024-10-06 22:34:11 +0200

    feat: add plugin based release providers [WIP]
    - Added the initial plugin abstraction definitions for the release providers. Implementation of the service and the anidb release provider will come in a following commit.

---
+++ Shoko.Plugin.Abstractions/Release/ReleaseMediaInfo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Shoko.Plugin.Abstractions.DataModels;
+
+namespace Shoko.Plugin.Abstractions.Release;
+
+/// <summary>
+/// Represents the audio and subtitle languages associated with an <see cref="IReleaseInfo"/>.
+/// /// </summary>
+public class ReleaseMediaInfo : IReleaseMediaInfo
+{
+    /// <summary>
+    /// Gets the audio languages associated with the media file.
+    /// </summary>
+    public List<TitleLanguage> AudioLanguages { get; set; } = [];
+
+    /// <summary>
+    /// Gets the subtitle languages associated with the media file.
+    /// </summary>
+    public List<TitleLanguage> SubtitleLanguages { get; set; } = [];
+
+    /// <inheritdoc />
+    public ReleaseMediaInfo() { }
+
+    /// <summary>
+    /// Constructs a new <see cref="ReleaseMediaInfo"/> instance from a
+    /// <see cref="IReleaseMediaInfo"/>.
+    /// </summary>
+    /// <param name="info">The <see cref="IReleaseMediaInfo"/> to construct
+    /// from.</param>
+    public ReleaseMediaInfo(IReleaseMediaInfo info)
+    {
+        AudioLanguages = new List<TitleLanguage>(info.AudioLanguages);
+        SubtitleLanguages = new List<TitleLanguage>(info.SubtitleLanguages);
+    }
+
+    #region IReleaseMediaInfo implementation
+
+    /// <inheritdoc />
+    IReadOnlyList<TitleLanguage> IReleaseMediaInfo.AudioLanguages => AudioLanguages;
+
+    /// <inheritdoc />
+    IReadOnlyList<TitleLanguage> IReleaseMediaInfo.SubtitleLanguages => SubtitleLanguages;
+
+    #endregion
+}
