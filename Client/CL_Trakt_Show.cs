d2c1613cfb4a0fa4998583afc0272f9558d4d36b
Mikal Stordal <mikalstordal@gmail.com>
2024-10-13 04:11:38 +0200

    Remove trakt show server model

--- Client/CL_Trakt_Show.cs
+++ Client/CL_Trakt_Show.cs
@@ -3,8 +3,15 @@ using Shoko.Models.Server;
 
 namespace Shoko.Models.Client
 {
-    public class CL_Trakt_Show : Trakt_Show
+    public class CL_Trakt_Show
     {
+        public int Trakt_ShowID { get; set; }
+        public string TraktID { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string URL { get; set; }
+        public string Overview { get; set; }
+        public int? TvDB_ID { get; set; }
         public List<CL_Trakt_Season> Seasons { get; set; }
     }
 }
