61f3ae6b2cf23d66dba9ff86c45ec5c73e7f82b3
Mikal Stordal <mikalstordal@gmail.com>
2024-11-05 18:50:37 +0100

    misc: add more match ratings

--- Enums/CrossrefSource.cs
+++ Enums/CrossrefSource.cs
@@ -16,5 +16,7 @@ namespace Shoko.Models.Enums
         TitleMatches = 4, // Titles Matched
         FirstAvailable = 5, // Neither Matched, but we could fill from adjacent episodes
         SarahJessicaParker = 6, // http://southpark.wikia.com/wiki/Sarah_Jessica_Parker
+        TitleKindaMatches = 7, // Title is close, but not exact
+        DateAndTitleKindaMatches = 8, // Date and Title are close, but not exact
     }
 }
