f70db354172b67f8419f9a339bb14b541d5551ea
krbrs <57227244+krbrs@users.noreply.github.com>
2024-11-27 21:18:46 +0100

    Extend ProductionCountries Column length
    on MySQL and MSSQL Server to a (hopefully) long enough value

--- Shoko.Server/Databases/SQLServer.cs
+++ Shoko.Server/Databases/SQLServer.cs
@@ -28,7 +28,7 @@ namespace Shoko.Server.Databases;
 public class SQLServer : BaseDatabase<SqlConnection>
 {
     public override string Name { get; } = "SQLServer";
-    public override int RequiredVersion { get; } = 134;
+    public override int RequiredVersion { get; } = 135;
 
     public override void BackupDatabase(string fullfilename)
     {
@@ -785,6 +785,8 @@ public class SQLServer : BaseDatabase<SqlConnection>
         new DatabaseCommand(133, 3, "ALTER TABLE TMDB_Show ADD Keywords NVARCHAR(512) NULL DEFAULT NULL;"),
         new DatabaseCommand(133, 4, "ALTER TABLE TMDB_Show ADD ProductionCountries NVARCHAR(32) NULL DEFAULT NULL;"),
         new DatabaseCommand(134, 1, "CREATE INDEX IX_AniDB_Anime_Relation_RelatedAnimeID on AniDB_Anime_Relation(RelatedAnimeID);"),
+        new DatabaseCommand(135, 1, "ALTER TABLE TMDB_Movie ALTER COLUMN ProductionCountries NVARCHAR(128) NULL DEFAULT NULL;"),
+        new DatabaseCommand(135, 2, "ALTER TABLE TMDB_Show ALTER COLUMN ProductionCountries NVARCHAR(128) NULL DEFAULT NULL;"),
     };
 
     private static void AlterImdbMovieIDType()
