afcd85f70be05639f2e037fd82257a2dd2e49065
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-01-31 21:03:45 -0800

    Add punctuation to comments in IRelocationService

--- Shoko.Plugin.Abstractions/Services/IRelocationService.cs
+++ Shoko.Plugin.Abstractions/Services/IRelocationService.cs
@@ -9,27 +9,27 @@ namespace Shoko.Plugin.Abstractions.Services;
 public interface IRelocationService
 {
     /// <summary>
-    /// Get the first destination with enough space for the given file, if any
+    /// Get the first destination with enough space for the given file, if any.
     /// </summary>
-    /// <param name="args">The relocation event args</param>
+    /// <param name="args">The relocation event args.</param>
     /// <returns></returns>
     IImportFolder? GetFirstDestinationWithSpace(RelocationEventArgs args);
 
     /// <summary>
     /// Check if the given import folder has enough space for the given file.
     /// </summary>
-    /// <param name="folder">The import folder</param>
+    /// <param name="folder">The import folder.</param>
     /// <param name="file">The file</param>
     /// <returns></returns>
     bool ImportFolderHasSpace(IImportFolder folder, IVideoFile file);
 
     /// <summary>
-    /// Get the location of the folder that contains a file for the latest (airdate) episode in the current collection
+    /// Get the location of the folder that contains a file for the latest (airdate) episode in the current collection.
     /// </summary>
     /// <remarks>
-    /// Will only look for files in import folders of type <see cref="DropFolderType.Excluded"/> or <see cref="DropFolderType.Destination"/>
+    /// Will only look for files in import folders of type <see cref="DropFolderType.Excluded"/> or <see cref="DropFolderType.Destination"/>.
     /// </remarks>
-    /// <param name="args">The relocation event args</param>
+    /// <param name="args">The relocation event args.</param>
     /// <returns></returns>
     public (IImportFolder ImportFolder, string RelativePath)? GetExistingSeriesLocationWithSpace(RelocationEventArgs args);
 }
