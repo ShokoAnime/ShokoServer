a3abe834d02f716220efc1800714587d72c9fed4
Mikal Stordal <mikalstordal@gmail.com>
2025-02-23 18:36:57 +0100

    fix percentage ranges

--- Shoko.Plugin.Abstractions/Release/ReleaseVideoCrossReference.cs
+++ Shoko.Plugin.Abstractions/Release/ReleaseVideoCrossReference.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Shoko.Plugin.Abstractions.Release;
 
 /// <summary>
@@ -15,20 +17,24 @@ public class ReleaseVideoCrossReference : IReleaseVideoCrossReference
     /// <summary>
     /// Where in the <see cref="AnidbEpisodeID"/> the video starts covering.
     /// If null, then the video starts at the beginning of the episode.
-    /// Can be in the range [0, 100], but must be less than the
+    /// Can be in the range [0, 99], but must be less than the
     /// <see cref="PercentageEnd"/> if both are set.
     /// </summary>
+    [Range(0, 99)]
     public int? PercentageStart { get; set; }
 
     /// <summary>
     /// Where in the <see cref="AnidbEpisodeID"/> the video stops covering.
     /// If null, then the video stops at the end of the episode.
-    /// Can be in the range [0, 100], but must be greater than the
+    /// Can be in the range [1, 100], but must be greater than the
     /// <see cref="PercentageStart"/> if both are set.
     /// </summary>
+    [Range(1, 100)]
     public int? PercentageEnd { get; set; }
 
-    /// <inheritdoc />
+    /// <summary>
+    /// Constructs a new <see cref="ReleaseVideoCrossReference"/> instance.
+    /// </summary>
     public ReleaseVideoCrossReference() { }
 
     /// <summary>
@@ -41,8 +47,17 @@ public class ReleaseVideoCrossReference : IReleaseVideoCrossReference
     {
         AnidbEpisodeID = reference.AnidbEpisodeID;
         AnidbAnimeID = reference.AnidbAnimeID;
-        PercentageStart = reference.PercentageStart;
-        PercentageEnd = reference.PercentageEnd;
+        // Check if it's a ReleaseVideoCrossReference to preserve the nullable state.
+        if (reference is ReleaseVideoCrossReference vRef)
+        {
+            PercentageStart = vRef.PercentageStart;
+            PercentageEnd = vRef.PercentageEnd;
+        }
+        else
+        {
+            PercentageStart = reference.PercentageStart;
+            PercentageEnd = reference.PercentageEnd;
+        }
     }
 
     #region IReleaseVideoCrossReference Implementation
