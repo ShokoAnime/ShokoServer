ec7af04b952feec7221bd15db29050e6ff64410a
Mikal Stordal <mikalstordal@gmail.com>
2025-02-25 00:09:15 +0100

    refactor: rename ProviderID → ProviderName

--- Shoko.Server/Repositories/Cached/StoredReleaseInfo_MatchAttemptRepository.cs
+++ Shoko.Server/Repositories/Cached/StoredReleaseInfo_MatchAttemptRepository.cs
@@ -11,9 +11,9 @@ public class StoredReleaseInfo_MatchAttemptRepository(DatabaseFactory databaseFa
 {
     private PocoIndex<int, StoredReleaseInfo_MatchAttempt, string>? _ed2k;
 
-    private PocoIndex<int, StoredReleaseInfo_MatchAttempt, string>? _sourceProviderIDs;
+    private PocoIndex<int, StoredReleaseInfo_MatchAttempt, string>? _sourceProviderNames;
 
-    private PocoIndex<int, StoredReleaseInfo_MatchAttempt, string?>? _resultProviderIDs;
+    private PocoIndex<int, StoredReleaseInfo_MatchAttempt, string?>? _resultProviderNames;
 
     protected override int SelectKey(StoredReleaseInfo_MatchAttempt entity)
         => entity.StoredReleaseInfo_MatchAttemptID;
@@ -21,8 +21,8 @@ public class StoredReleaseInfo_MatchAttemptRepository(DatabaseFactory databaseFa
     public override void PopulateIndexes()
     {
         _ed2k = Cache.CreateIndex(a => a.ED2K);
-        _sourceProviderIDs = Cache.CreateIndex(a => a.AttemptedProviderIDs);
-        _resultProviderIDs = Cache.CreateIndex(a => a.ProviderID);
+        _sourceProviderNames = Cache.CreateIndex(a => a.AttemptedProviderNames);
+        _resultProviderNames = Cache.CreateIndex(a => a.ProviderName);
     }
 
     public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetByEd2k(string ed2k)
@@ -33,11 +33,11 @@ public class StoredReleaseInfo_MatchAttemptRepository(DatabaseFactory databaseFa
     public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetByEd2kAndFileSize(string ed2k, long fileSize)
         => GetByEd2k(ed2k).Where(a => a.FileSize == fileSize).ToList();
 
-    public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetBySourceProviderIDs(string providerId)
-        => !string.IsNullOrEmpty(providerId)
-            ? ReadLock(() => _sourceProviderIDs!.GetMultiple(providerId))
+    public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetBySourceProviderNames(string providerName)
+        => !string.IsNullOrEmpty(providerName)
+            ? ReadLock(() => _sourceProviderNames!.GetMultiple(providerName))
             : [];
 
-    public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetByResultProviderIDs(string? providerId)
-        => ReadLock(() => _resultProviderIDs!.GetMultiple(providerId));
+    public IReadOnlyList<StoredReleaseInfo_MatchAttempt> GetByResultProviderNames(string? providerName)
+        => ReadLock(() => _resultProviderNames!.GetMultiple(providerName));
 }
