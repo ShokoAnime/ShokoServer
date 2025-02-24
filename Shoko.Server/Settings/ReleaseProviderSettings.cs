694c8d23dacd296b5cb5b27792577eb76b2e519e
Mikal Stordal <mikalstordal@gmail.com>
2025-02-25 00:03:27 +0100

    feat: add parallel mode (disabled by default for now)

--- Shoko.Server/Settings/ReleaseProviderSettings.cs
+++ Shoko.Server/Settings/ReleaseProviderSettings.cs
@@ -4,6 +4,8 @@ namespace Shoko.Server.Settings;
 
 public class ReleaseProviderSettings
 {
+    public bool ParallelMode { get; set; } = false;
+
     public Dictionary<string, bool> Enabled { get; set; } = [];
 
     public List<string> Priority { get; set; } = [];
