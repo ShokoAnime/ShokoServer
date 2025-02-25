895fe728ab21cd0fd790b746d15ac86afa107030
Mikal Stordal <mikalstordal@gmail.com>
2025-02-26 00:23:39 +0100

    refactor: simplify event init

--- Shoko.Plugin.Abstractions/Events/VideoReleaseSearchCompletedEventArgs.cs
+++ Shoko.Plugin.Abstractions/Events/VideoReleaseSearchCompletedEventArgs.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Shoko.Plugin.Abstractions.DataModels;
 using Shoko.Plugin.Abstractions.Release;
@@ -9,36 +10,47 @@ namespace Shoko.Plugin.Abstractions.Events;
 /// <summary>
 /// Dispatched when a video release search is completed.
 /// </summary>
-/// <param name="video">The video.</param>
-/// <param name="releaseInfo">The release info.</param>
-/// <param name="isSaved">Indicates the found release is saved.</param>
-/// <param name="startedAt">The time the search started.</param>
-/// <param name="completedAt">The time the search completed.</param>
-public class VideoReleaseSearchCompletedEventArgs(IVideo video, IReleaseInfo? releaseInfo, bool isSaved, DateTime startedAt, DateTime completedAt) : VideoEventArgs(video)
+public class VideoReleaseSearchCompletedEventArgs : VideoEventArgs
 {
-    /// <summary>
-    /// The found release info, if successful.
-    /// </summary>
-    public IReleaseInfo? ReleaseInfo { get; } = releaseInfo;
-
     /// <summary>
     /// Indicates if the found releases should be saved.
     /// </summary>
-    public bool IsSaved { get; } = isSaved;
+    public required bool IsSaved { get; init; }
 
     /// <summary>
     /// Indicates if the search was successful.
     /// </summary>
     [MemberNotNullWhen(true, nameof(ReleaseInfo))]
-    public bool IsSuccessful => ReleaseInfo is not null;
+    [MemberNotNullWhen(true, nameof(ProviderID))]
+    public bool IsSuccessful => ReleaseInfo is not null && ProviderID is not { Length: > 0 };
+
+    /// <summary>
+    /// The IDs of the release providers that were attempted.
+    /// </summary>
+    public required IReadOnlyList<string> AttemptedProviderIDs { get; init; }
+
+    /// <summary>
+    /// The ID of the release provider that was selected for the successful
+    /// search.
+    /// </summary>
+    public required string? ProviderID { get; init; }
+    /// <summary>
+    /// The found release info, if successful.
+    /// </summary>
+    public required IReleaseInfo? ReleaseInfo { get; init; }
+
+    /// <summary>
+    /// The exception that occurred during the search, if any.
+    /// </summary>
+    public required Exception? Exception { get; init; }
 
     /// <summary>
     /// The time the search started.
     /// </summary>
-    public DateTime StartedAt { get; } = startedAt;
+    public required DateTime StartedAt { get; init; }
 
     /// <summary>
     /// The time the search completed.
     /// </summary>
-    public DateTime CompletedAt { get; } = completedAt;
+    public required DateTime CompletedAt { get; init; }
 }
