ShokoAnime/ShokoServer

commit ae2eabcdce808147a5f33f1200236ed6c770d023

fix: don't schedule ProcessFileJob if auto matching is disabled

Shoko.Server/Services/AbstractVideoReleaseService.cs
@@ -64,6 +64,11 @@ public class AbstractVideoReleaseService(
 
     public event EventHandler? ProvidersUpdated;
 
+    private bool? _autoMatchEnabled = null;
+
+    public bool AutoMatchEnabled
+        => _autoMatchEnabled ??= GetAvailableProviders(true).Any();
+
     public bool ParallelMode
     {
         get => _settings.Plugins.ReleaseProviders.ParallelMode;
@@ -72,7 +77,9 @@ public class AbstractVideoReleaseService(
             if (_settings.Plugins.ReleaseProviders.ParallelMode == value)
                 return;
 
+            _autoMatchEnabled = null;
             _settings.Plugins.ReleaseProviders.ParallelMode = value;
+            settingsProvider.SaveSettings(_settings);
             ProvidersUpdated?.Invoke(this, EventArgs.Empty);
         }
     }
@@ -198,6 +205,7 @@ public class AbstractVideoReleaseService(
 
         if (changed)
         {
+            _autoMatchEnabled = null;
             settingsProvider.SaveSettings(settings);
             ProvidersUpdated?.Invoke(this, EventArgs.Empty);
         }

Shoko.Plugin.Abstractions/Services/IVideoReleaseService.cs
@@ -39,6 +39,11 @@ public interface IVideoReleaseService
     event EventHandler? ProvidersUpdated;
 
     /// <summary>
+    ///   Gets a value indicating whether any release info providers are enabled for auto-matching.
+    /// </summary>
+    bool AutoMatchEnabled { get; }
+
+    /// <summary>
     ///   Gets or sets a value indicating whether to use parallel mode.
     /// </summary>
     /// <remarks>

Shoko.Server/API/v1/Implementations/ShokoServiceImplementation/ShokoServiceImplementation.cs
@@ -52,6 +52,7 @@ public partial class ShokoServiceImplementation : Controller, IShokoServer
     private readonly AnimeEpisodeService _episodeService;
     private readonly VideoLocalService _videoLocalService;
     private readonly IUserDataService _userDataService;
+    private readonly IVideoReleaseService _videoReleaseService;
 
     public ShokoServiceImplementation(
         TraktTVHelper traktHelper,
@@ -66,7 +67,8 @@ public partial class ShokoServiceImplementation : Controller, IShokoServer
         JobFactory jobFactory,
         AnimeEpisodeService episodeService,
         IUserDataService userDataService,
-        VideoLocalService videoLocalService
+        VideoLocalService videoLocalService,
+        IVideoReleaseService videoReleaseService
     )
     {
         _traktHelper = traktHelper;
@@ -82,6 +84,7 @@ public partial class ShokoServiceImplementation : Controller, IShokoServer
         _episodeService = episodeService;
         _userDataService = userDataService;
         _videoLocalService = videoLocalService;
+        _videoReleaseService = videoReleaseService;
     }
 
     #region Bookmarks

Shoko.Server/API/v1/Implementations/ShokoServiceImplementation/ShokoServiceImplementation_Entities.cs
@@ -565,9 +565,10 @@ public partial class ShokoServiceImplementation : IShokoServer
         {
             var vid = RepoFactory.VideoLocal.GetByID(videoLocalID);
             if (vid is null)
-            {
                 return "File could not be found";
-            }
+
+            if (!_videoReleaseService.AutoMatchEnabled)
+                return "Release auto-matching is currently disabled";
 
             var scheduler = _schedulerFactory.GetScheduler().GetAwaiter().GetResult();
             scheduler.StartJobNow<ProcessFileJob>(
@@ -594,14 +595,13 @@ public partial class ShokoServiceImplementation : IShokoServer
         {
             var vid = RepoFactory.VideoLocal.GetByID(videoLocalID);
             if (vid is null)
-            {
                 return "File could not be found";
-            }
 
             if (string.IsNullOrEmpty(vid.Hash))
-            {
                 return "Could not Update a cloud file without hash, hash it locally first";
-            }
+
+            if (!_videoReleaseService.AutoMatchEnabled)
+                return "Release auto-matching is currently disabled";
 
             var scheduler = _schedulerFactory.GetScheduler().GetAwaiter().GetResult();
             scheduler.StartJobNow<ProcessFileJob>(

Shoko.Server/API/v1/Implementations/ShokoServiceImplementation/ShokoServiceImplementation_Utilities.cs
@@ -770,6 +770,9 @@ public partial class ShokoServiceImplementation
     [HttpPost("File/Unlinked/Rescan")]
     public void RescanUnlinkedFiles()
     {
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return;
+
         try
         {
             // files which have been hashed, but don't have an associated episode

Shoko.Server/API/v2/Modules/Common.cs
@@ -53,6 +53,7 @@ public class Common : BaseController
     private readonly QueueHandler _queueHandler;
     private readonly IUserDataService _userDataService;
     private readonly VideoLocal_UserRepository _vlUsers;
+    private readonly IVideoReleaseService _videoReleaseService;
 
     public Common(
         ISchedulerFactory schedulerFactory,
@@ -63,7 +64,8 @@ public class Common : BaseController
         AnimeSeriesService seriesService,
         AnimeGroupService groupService,
         IUserDataService userDataService,
-        VideoLocal_UserRepository vlUsers) : base(settingsProvider)
+        VideoLocal_UserRepository vlUsers,
+        IVideoReleaseService videoReleaseService) : base(settingsProvider)
     {
         _schedulerFactory = schedulerFactory;
         _actionService = actionService;
@@ -73,6 +75,7 @@ public class Common : BaseController
         _groupService = groupService;
         _userDataService = userDataService;
         _vlUsers = vlUsers;
+        _videoReleaseService = videoReleaseService;
     }
     //class will be found automagically thanks to inherits also class need to be public (or it will 404)
 
@@ -274,14 +277,10 @@ public class Common : BaseController
         {
             var vid = RepoFactory.VideoLocal.GetByID(id);
             if (vid == null)
-            {
                 return NotFound();
-            }
 
-            if (string.IsNullOrEmpty(vid.Hash))
-            {
-                return BadRequest("Could not Update a cloud file without hash, hash it locally first");
-            }
+            if (!_videoReleaseService.AutoMatchEnabled)
+                return Ok();
 
             var scheduler = await _schedulerFactory.GetScheduler();
             await scheduler.StartJobNow<ProcessFileJob>(
@@ -306,6 +305,9 @@ public class Common : BaseController
     [HttpGet("rescanunlinked")]
     public async Task<ActionResult> RescanUnlinked()
     {
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return Ok();
+
         try
         {
             // files which have been hashed, but don't have an associated episode
@@ -338,6 +340,9 @@ public class Common : BaseController
     [HttpGet("rescanmanuallinks")]
     public async Task<ActionResult> RescanManualLinks()
     {
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return Ok();
+
         try
         {
             // files which have been hashed, but don't have an associated episode

Shoko.Server/API/v3/Controllers/FileController.cs
@@ -512,6 +512,9 @@ public class FileController : BaseController
         if (string.IsNullOrEmpty(filePath))
             return ValidationProblem(FileNoPath, "File");
 
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return ValidationProblem("Release auto-matching is currently disabled.", "IVideoReleaseService");
+
         var scheduler = await _schedulerFactory.GetScheduler();
         if (priority)
             await scheduler.StartJobNow<ProcessFileJob>(c =>
@@ -932,6 +935,9 @@ public class FileController : BaseController
         var filePath = file.FirstResolvedPlace?.FullServerPath;
         if (string.IsNullOrEmpty(filePath))
             return ValidationProblem(FileNoPath, "File");
+
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return ValidationProblem("Release auto-matching is currently disabled.", "IVideoReleaseService");
 
         var scheduler = await _schedulerFactory.GetScheduler();
         if (priority)

Shoko.Server/API/v3/Controllers/SeriesController.cs
@@ -28,7 +28,6 @@ using Shoko.Server.Models;
 using Shoko.Server.Providers.AniDB.Titles;
 using Shoko.Server.Providers.TMDB;
 using Shoko.Server.Repositories;
-using Shoko.Server.Repositories.Cached;
 using Shoko.Server.Scheduling;
 using Shoko.Server.Scheduling.Jobs.Shoko;
 using Shoko.Server.Scheduling.Jobs.Trakt;
@@ -64,12 +63,11 @@ public class SeriesController : BaseController
 
     private readonly TmdbMetadataService _tmdbMetadataService;
 
-
     private readonly TmdbSearchService _tmdbSearchService;
 
-    private readonly CrossRef_File_EpisodeRepository _crossRefFileEpisode;
-
     private readonly IUserDataService _userDataService;
+
+    private readonly IVideoReleaseService _videoReleaseService;
 
     public SeriesController(
         ISettingsProvider settingsProvider,
@@ -80,8 +78,8 @@ public class SeriesController : BaseController
         TmdbLinkingService tmdbLinkingService,
         TmdbMetadataService tmdbMetadataService,
         TmdbSearchService tmdbSearchService,
-        CrossRef_File_EpisodeRepository crossRefFileEpisode,
-        IUserDataService userDataService
+        IUserDataService userDataService,
+        IVideoReleaseService videoReleaseService
     ) : base(settingsProvider)
     {
         _seriesService = seriesService;
@@ -91,8 +89,8 @@ public class SeriesController : BaseController
         _tmdbLinkingService = tmdbLinkingService;
         _tmdbMetadataService = tmdbMetadataService;
         _tmdbSearchService = tmdbSearchService;
-        _crossRefFileEpisode = crossRefFileEpisode;
         _userDataService = userDataService;
+        _videoReleaseService = videoReleaseService;
     }
 
     #region Return messages
@@ -2304,6 +2302,9 @@ public class SeriesController : BaseController
 
         if (!User.AllowedSeries(series))
             return Forbid(SeriesForbiddenForUser);
+
+        if (!_videoReleaseService.AutoMatchEnabled)
+            return ValidationProblem("Release auto-matching is currently disabled.", "IVideoReleaseService");
 
         var scheduler = await _schedulerFactory.GetScheduler();
         foreach (var file in series.VideoLocals)

commit 30ba7a715e8918808d94a5247b1840de8a772a24

fix: break circular dependency
Break the circular dependency I introduced in ae2eabcdce808147a5f33f1200236ed6c770d023.

Shoko.Server/Services/AbstractVideoReleaseService.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Quartz;
 using Shoko.Plugin.Abstractions.DataModels;
@@ -38,7 +39,7 @@ public class AbstractVideoReleaseService(
     ISchedulerFactory schedulerFactory,
     IRequestFactory requestFactory,
     IUserService userService,
-    IUserDataService userDataService,
+    IServiceProvider serviceProvider,
     VideoLocalRepository videoRepository,
     StoredReleaseInfoRepository releaseInfoRepository,
     StoredReleaseInfo_MatchAttemptRepository releaseInfoMatchAttemptRepository,
@@ -50,6 +51,12 @@ public class AbstractVideoReleaseService(
     CrossRef_File_EpisodeRepository xrefRepository
 ) : IVideoReleaseService
 {
+    // We need to lazy init. the user data service since otherwise there will be
+    // a circular dependency between this service, the user data service, and
+    // the anime series service (which in turn depend on this service). So we
+    // lazy init the user data service to break the circle.
+    private IUserDataService? _userDataService = null;
+
     private IServerSettings _settings => settingsProvider.GetSettings();
 
     private Dictionary<Guid, IReleaseInfoProvider>? _releaseInfoProviders = null;
@@ -721,15 +728,16 @@ public class AbstractVideoReleaseService(
         if (otherVideos.Count == 0)
             return;
 
+        _userDataService ??= serviceProvider.GetRequiredService<IUserDataService>();
         foreach (var user in userService.GetUsers())
         {
             var watchedVideo = otherVideos
-                .FirstOrDefault(video => userDataService.GetVideoUserData(user.ID, video.ID)?.LastPlayedAt is not null);
+                .FirstOrDefault(video => _userDataService.GetVideoUserData(user.ID, video.ID)?.LastPlayedAt is not null);
             if (watchedVideo is null)
                 continue;
 
-            var watchedRecord = userDataService.GetVideoUserData(user.ID, watchedVideo.ID)!;
-            userDataService.SaveVideoUserData(user, video, new(watchedRecord), UserDataSaveReason.VideoReImport);
+            var watchedRecord = _userDataService.GetVideoUserData(user.ID, watchedVideo.ID)!;
+            _userDataService.SaveVideoUserData(user, video, new(watchedRecord), UserDataSaveReason.VideoReImport);
         }
     }
 