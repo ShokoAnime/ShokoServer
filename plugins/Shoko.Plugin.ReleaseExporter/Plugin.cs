3926c8f2b6c9020e74c24267ab8af7b485285713
Mikal Stordal <mikalstordal@gmail.com>
2025-03-02 08:29:01 +0100

    refactor: add `IConfigurationService`, `ConfigurationProvider<TConfig>` + more
    - Added new interfaces for plugins (and core) to more easily load/save configurations through injected configuration providers. All a plugin needs to do is to export one or more classes implementing the (empty) `IConfiguration` interface, and the core will register the configurations with the service and allow you to DI the `ConfigurationProvider<TConfig>` provider (where `TConfig` is the type of your defined configuration) and things will _just work™_.
    
    - Converted the existing `ISettingsProvider` to use the new service and config provider internally.

--- plugins/Shoko.Plugin.ReleaseExporter/Plugin.cs
+++ plugins/Shoko.Plugin.ReleaseExporter/Plugin.cs
@@ -1,12 +1,13 @@
+using System;
 using Shoko.Plugin.Abstractions;
 
 namespace Shoko.Plugin.ReleaseExporter;
 
 public class Plugin : IPlugin
 {
-    public string Name => "Release Exporter";
+    public Guid ID => Guid.Parse("df4ea747-98e6-587a-be14-3a71f9403a3e");
+
+    public string Name => "Release Importer/Exporter";
 
     public void Load() { }
-
-    public void OnSettingsLoaded(IPluginSettings settings) { }
 }
