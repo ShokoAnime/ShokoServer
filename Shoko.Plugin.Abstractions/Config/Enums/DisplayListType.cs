c9ffb3d7afca1f53f5291f1e148d7fde4df876ad
Mikal Stordal <mikalstordal@gmail.com>
2025-03-15 07:17:57 +0100

    refactor: better enum value definitions + add missing json schema ui fields

--- Shoko.Plugin.Abstractions/Config/Enums/DisplayListType.cs
+++ Shoko.Plugin.Abstractions/Config/Enums/DisplayListType.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 
 namespace Shoko.Plugin.Abstractions.Config.Enums;
 
@@ -9,16 +10,19 @@ public enum DisplayListType
     /// <summary>
     /// Auto behavior based on complexity and type.
     /// </summary>
+    [EnumMember(Value = "auto")]
     Auto = 0,
 
     /// <summary>
     /// A list where all the options are viewed at once, with checkboxes for each option.
     /// </summary>
+    [EnumMember(Value = "checkbox")]
     Checkbox = 1,
 
     /// <summary>
     /// A dropdown list where you select each existing entry in a drop down.
     /// Only usable by complex list types.
     /// </summary>
+    [EnumMember(Value = "dropdown")]
     Dropdown = 2,
 }
