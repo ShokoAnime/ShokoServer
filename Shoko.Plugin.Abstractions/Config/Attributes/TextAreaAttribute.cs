55bfaf1a21666123c348e24d1181ddd15a1eb084
Mikal Stordal <mikalstordal@gmail.com>
2025-03-17 07:15:32 +0100

    today's progress

--- Shoko.Plugin.Abstractions/Config/Attributes/TextAreaAttribute.cs
+++ Shoko.Plugin.Abstractions/Config/Attributes/TextAreaAttribute.cs
@@ -6,5 +6,5 @@ namespace Shoko.Plugin.Abstractions.Config.Attributes;
 /// <summary>
 /// Used to mark a property/field as a text-area in the UI.
 /// </summary>
-[AttributeUsage(AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public class TextAreaAttribute() : Attribute { }
