440281b117fbfd2f376f3e84ce8aca1115461c6a
Mikal Stordal <mikalstordal@gmail.com>
2025-03-23 20:16:38 +0100

    misc: add stub providers for testing the UI

---
+++ plugins/Shoko.Plugin.ConfigurationHell/Release/ProviderC.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Shoko.Plugin.Abstractions.Config;
+using Shoko.Plugin.Abstractions.DataModels;
+using Shoko.Plugin.Abstractions.Release;
+
+namespace Shoko.Plugin.ConfigurationHell.Release;
+
+/// <summary>
+/// Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor
+/// incididunt ut labore et dolore magna aliqua.
+/// </summary>
+public class ProviderC : IReleaseInfoProvider<ProviderB.ProviderBConfig>
+{
+    /// <inheritdoc />
+    public string Name => "Provider C";
+
+    /// <inheritdoc />
+    public Version Version => new(2, 3, 0);
+
+    /// <inheritdoc />
+    public Task<ReleaseInfo?> GetReleaseInfoById(string releaseId, CancellationToken cancellationToken)
+        => Task.FromResult<ReleaseInfo?>(null);
+
+    /// <inheritdoc />
+    public Task<ReleaseInfo?> GetReleaseInfoForVideo(IVideo video, CancellationToken cancellationToken)
+        => Task.FromResult<ReleaseInfo?>(null);
+}
