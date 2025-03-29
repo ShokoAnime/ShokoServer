e092d4a08125ebdaa6a49076f51d6f25dc9142a3
Mikal Stordal <mikalstordal@gmail.com>
2025-03-29 15:06:05 +0100

    feat: add user as arg when performing action on config

--- Shoko.Plugin.Abstractions/Config/ConfigurationProvider.cs
+++ Shoko.Plugin.Abstractions/Config/ConfigurationProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Shoko.Plugin.Abstractions.Config.Exceptions;
+using Shoko.Plugin.Abstractions.DataModels.Shoko;
 using Shoko.Plugin.Abstractions.Events;
 using Shoko.Plugin.Abstractions.Services;
 
@@ -17,20 +18,23 @@ public class ConfigurationProvider<TConfig> : IDisposable where TConfig : class,
     private ConfigurationInfo? _pluginConfigurationInfo = null;
 
     /// <summary>
-    /// Occurs when a configuration is saved.
+    ///   Occurs when a configuration is saved.
     /// </summary>
     public event EventHandler<ConfigurationSavedEventArgs<TConfig>>? Saved;
 
     /// <summary>
-    /// Gets the information about the configuration.
+    ///   Gets the information about the configuration.
     /// </summary>
     public ConfigurationInfo ConfigurationInfo
         => _pluginConfigurationInfo ??= _service.GetConfigurationInfo<TConfig>();
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ConfigurationProvider{TConfig}"/> class.
+    ///   Initializes a new instance of the
+    ///   <see cref="ConfigurationProvider{TConfig}"/> class.
     /// </summary>
-    /// <param name="service">The configuration service.</param>
+    /// <param name="service">
+    ///   The configuration service.
+    /// </param>
     public ConfigurationProvider(IConfigurationService service)
     {
         _service = service;
@@ -38,7 +42,8 @@ public class ConfigurationProvider<TConfig> : IDisposable where TConfig : class,
     }
 
     /// <summary>
-    /// Finalizes an instance of the <see cref="ConfigurationProvider{TConfig}"/> class.
+    ///   Finalizes an instance of the
+    ///   <see cref="ConfigurationProvider{TConfig}"/> class.
     /// </summary>
     public void Dispose()
     {
@@ -46,11 +51,6 @@ public class ConfigurationProvider<TConfig> : IDisposable where TConfig : class,
         GC.SuppressFinalize(this);
     }
 
-    /// <summary>
-    /// Handles the saved event.
-    /// </summary>
-    /// <param name="sender">The sender.</param>
-    /// <param name="eventArgs">The <see cref="ConfigurationSavedEventArgs"/> instance containing the event data.</param>
     private void OnSaved(object? sender, ConfigurationSavedEventArgs eventArgs)
     {
         if (eventArgs.ConfigurationInfo != ConfigurationInfo)
@@ -62,35 +62,61 @@ public class ConfigurationProvider<TConfig> : IDisposable where TConfig : class,
     /// <summary>
     /// Validates the configuration.
     /// </summary>
-    /// <param name="config">The configuration.</param>
-    /// <returns>A read-only dictionary of validation errors per property path.</returns>
+    /// <param name="config">
+    ///   The configuration to validate.
+    /// </param>
+    /// <returns>
+    ///   A read-only dictionary of validation errors per property path.
+    /// </returns>
     public IReadOnlyDictionary<string, IReadOnlyList<string>> Validate(TConfig config)
         => _service.Validate(config);
 
     /// <summary>
-    /// Perform an action on the configuration on the saved configuration.
+    ///   Perform an action on the configuration on the saved configuration.
     /// </summary>
-    /// <param name="path">The path to the configuration.</param>
-    /// <param name="action">The action to perform.</param>
+    /// <param name="path">
+    ///   The path to the configuration.
+    /// </param>
+    /// <param name="action">
+    ///   The action to perform.
+    /// </param>
+    /// <param name="user">
+    ///   The user performing the action, if applicable.
+    /// </param>
     /// <exception cref="InvalidConfigurationActionException">
-    /// Thrown when an action is invalid. Be it because the action does not exist or because the path for where to look for the action is invalid.
+    ///   Thrown when an action is invalid. Be it because the action does not
+    ///   exist or because the path for where to look for the action is invalid.
     /// </exception>
-    /// <returns>The result of the action.</returns>
-    public ConfigurationActionResult PerformAction(string path, string action)
-        => _service.PerformAction(Load(), path, action);
+    /// <returns>
+    ///   The result of the action.
+    /// </returns>
+    public ConfigurationActionResult PerformAction(string path, string action, IShokoUser? user = null)
+        => _service.PerformAction(Load(), path, action, user);
 
     /// <summary>
-    /// Perform an action on the configuration on a configuration instance.
+    ///   Perform an action on the configuration on a configuration instance.
     /// </summary>
-    /// <param name="config">The configuration instance to perform the action on.</param>
-    /// <param name="path">The path to the configuration.</param>
-    /// <param name="action">The action to perform.</param>
+    /// <param name="config">
+    ///   The configuration instance to perform the action on.
+    /// </param>
+    /// <param name="path">
+    ///   The path to the configuration.
+    /// </param>
+    /// <param name="action">
+    ///   The action to perform.
+    /// </param>
+    /// <param name="user">
+    ///   The user performing the action, if applicable.
+    /// </param>
     /// <exception cref="InvalidConfigurationActionException">
-    /// Thrown when an action is invalid. Be it because the action does not exist or because the path for where to look for the action is invalid.
+    ///   Thrown when an action is invalid. Be it because the action does not
+    ///   exist or because the path for where to look for the action is invalid.
     /// </exception>
-    /// <returns>The result of the action.</returns>
-    public ConfigurationActionResult PerformAction(TConfig config, string path, string action)
-        => _service.PerformAction(config, path, action);
+    /// <returns>
+    ///   The result of the action.
+    /// </returns>
+    public ConfigurationActionResult PerformAction(TConfig config, string path, string action, IShokoUser? user = null)
+        => _service.PerformAction(config, path, action, user);
 
     /// <summary>
     /// Creates a new configuration instance.
@@ -100,34 +126,52 @@ public class ConfigurationProvider<TConfig> : IDisposable where TConfig : class,
         => _service.New<TConfig>();
 
     /// <summary>
-    /// Loads the current in-memory configuration instance if it exists, otherwise loads a new instance from disk or creates a new one from defaults.
+    ///   Loads the current in-memory configuration instance if it exists,
+    ///   otherwise loads a new instance from disk or creates a new one from
+    ///   defaults.
     /// </summary>
-    /// <param name="copy">Set to true to get a copy of the configuration instead of the in-memory cached instance.</param>
+    /// <param name="copy">
+    ///   Set to true to get a copy of the configuration instead of the
+    ///   in-memory cached instance.
+    /// </param>
     /// <exception cref="ConfigurationValidationException">
-    /// Thrown when a configuration fails validation.
+    ///   Thrown when a configuration fails validation.
     /// </exception>
-    /// <returns>The configuration.</returns>
+    /// <returns>
+    ///   The loaded configuration.
+    /// </returns>
     public TConfig Load(bool copy = false)
         => _service.Load<TConfig>(copy);
 
     /// <summary>
-    /// Saves the current in-memory configuration instance. Also triggers the <see cref="Saved"/> event if the configuration has changed.
+    ///   Saves the current in-memory configuration instance. Also triggers the
+    /// <see cref="Saved"/> event if the configuration has changed.
     /// </summary>
     /// <exception cref="ConfigurationValidationException">
-    /// Thrown when a configuration fails validation.
+    ///   Thrown when a configuration fails validation.
     /// </exception>
-    /// <returns>A boolean indicating whether the configuration was saved to disk. <c>false</c> means there was no change to the configuration.</returns>
+    /// <returns>
+    ///   A boolean indicating whether the configuration was saved to disk.
+    ///   <c>false</c> means there was no change to the configuration.
+    /// </returns>
     public bool Save()
         => _service.Save<TConfig>();
 
     /// <summary>
-    /// Saves the configuration instance and sets it as the current in-memory configuration. Also triggers the <see cref="Saved"/> event if the configuration has changed.
+    ///   Saves the configuration instance and sets it as the current in-memory
+    ///   configuration. Also triggers the <see cref="Saved"/> event if the
+    ///   configuration has changed.
     /// </summary>
-    /// <param name="config">The configuration.</param>
+    /// <param name="config">
+    ///   The configuration to save.
+    /// </param>
     /// <exception cref="ConfigurationValidationException">
-    /// Thrown when a configuration fails validation.
+    ///   Thrown when a configuration fails validation.
     /// </exception>
-    /// <returns>A boolean indicating whether the configuration was saved to disk. <c>false</c> means there was no change to the configuration.</returns>
+    /// <returns>
+    ///   A boolean indicating whether the configuration was saved to disk.
+    ///   <c>false</c> means there was no change to the configuration.
+    /// </returns>
     public bool Save(TConfig config)
         => _service.Save(config);
 }
