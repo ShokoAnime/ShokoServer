8d1405e2d6eb9122ebc4a97fb149ad062573fcff
krbrs <57227244+krbrs@users.noreply.github.com>
2025-03-31 13:13:34 +0200

    Small update so failed login doesn't false-trigger true

--- Shoko.Server/Plex/PlexHelper.cs
+++ Shoko.Server/Plex/PlexHelper.cs
@@ -174,7 +174,7 @@ public class PlexHelper
                 isAuthenticated = RequestAsync("https://plex.tv/users/account.json", HttpMethod.Get,
                         AuthenticationHeaders).ConfigureAwait(false)
                     .GetAwaiter().GetResult().status == HttpStatusCode.OK;
-                _lastAuthenticated = DateTime.Now;
+                _lastAuthenticated = (bool)isAuthenticated ? DateTime.Now : null;
                 return (bool)isAuthenticated;
             }
             catch (Exception)
