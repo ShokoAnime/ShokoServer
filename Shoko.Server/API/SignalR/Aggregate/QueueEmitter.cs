c52c0121932bede1c5ea211432a5a9bfed86b426
Mikal Stordal <mikalstordal@gmail.com>
2025-04-17 01:19:34 +0200

    fix: emit queue state changed event to signalr when items are added to the queue

--- Shoko.Server/API/SignalR/Aggregate/QueueEmitter.cs
+++ Shoko.Server/API/SignalR/Aggregate/QueueEmitter.cs
@@ -5,22 +5,31 @@ using Shoko.Server.API.SignalR.Models;
 using Shoko.Server.API.v3.Models.Shoko;
 using Shoko.Server.Scheduling;
 
+#nullable enable
 namespace Shoko.Server.API.SignalR.Aggregate;
 
 public class QueueEmitter : BaseEmitter, IDisposable
 {
     private readonly QueueStateEventHandler _queueStateEventHandler;
     private readonly QueueHandler _queueHandler;
-    private QueueStateSignalRModel _lastQueueState;
-    
+    private QueueStateSignalRModel? _lastQueueState;
 
     public QueueEmitter(IHubContext<AggregateHub> hub, QueueStateEventHandler queueStateEventHandler, QueueHandler queueHandler) : base(hub)
     {
         _queueStateEventHandler = queueStateEventHandler;
         _queueHandler = queueHandler;
+        _queueStateEventHandler.QueueItemsAdded += OnQueueItemsAddedEvent;
         _queueStateEventHandler.ExecutingJobsChanged += OnExecutingJobsStateChangedEvent;
         _queueStateEventHandler.QueueStarted += OnQueueStarted;
         _queueStateEventHandler.QueuePaused += OnQueuePaused;
+    }
+
+    public void Dispose()
+    {
+        _queueStateEventHandler.ExecutingJobsChanged -= OnExecutingJobsStateChangedEvent;
+        _queueStateEventHandler.QueueStarted -= OnQueueStarted;
+        _queueStateEventHandler.QueuePaused -= OnQueuePaused;
+        GC.SuppressFinalize(this);
     }
 
     private QueueStateSignalRModel GetQueueState()
@@ -49,28 +58,37 @@ public class QueueEmitter : BaseEmitter, IDisposable
         return GetQueueState();
     }
 
-    private async void OnQueueStarted(object sender, EventArgs e)
+    private async void OnQueueStarted(object? sender, EventArgs e)
     {
         var state = GetQueueState();
 
         await SendAsync("QueueStateChanged", state);
     }
 
-    private async void OnQueuePaused(object sender, EventArgs e)
+    private async void OnQueuePaused(object? sender, EventArgs e)
     {
         var state = GetQueueState();
 
         await SendAsync("QueueStateChanged", state);
     }
 
-    public void Dispose()
+    private async void OnQueueItemsAddedEvent(object? sender, QueueItemsAddedEventArgs e)
     {
-        _queueStateEventHandler.ExecutingJobsChanged -= OnExecutingJobsStateChangedEvent;
-        _queueStateEventHandler.QueueStarted -= OnQueueStarted;
-        _queueStateEventHandler.QueuePaused -= OnQueuePaused;
+        var currentState = new QueueStateSignalRModel
+        {
+            Running = _queueStateEventHandler.Running,
+            WaitingCount = e.WaitingJobsCount,
+            BlockedCount = e.BlockedJobsCount,
+            TotalCount = e.WaitingJobsCount + e.BlockedJobsCount + e.ExecutingJobsCount,
+            ThreadCount = e.ThreadCount,
+            CurrentlyExecuting = _lastQueueState is { } ? _lastQueueState.CurrentlyExecuting : [],
+        };
+        if (Equals(_lastQueueState, currentState)) return;
+        _lastQueueState = currentState;
+        await SendAsync("QueueStateChanged", currentState);
     }
 
-    private async void OnExecutingJobsStateChangedEvent(object sender, QueueChangedEventArgs e)
+    private async void OnExecutingJobsStateChangedEvent(object? sender, QueueChangedEventArgs e)
     {
         var currentState = new QueueStateSignalRModel
         {
