5486742fdd3faa9dab5d4e3e3fef9e36bf85f2de
krbrs <57227244+krbrs@users.noreply.github.com>
2025-07-30 15:12:18 +0200

    Recreate TMDB_Show_Network Table in SQLite

--- Shoko.Server/Databases/SQLite.cs
+++ Shoko.Server/Databases/SQLite.cs
@@ -27,7 +27,7 @@ public class SQLite : BaseDatabase<SqliteConnection>
 {
     public override string Name => "SQLite";
 
-    public override int RequiredVersion => 135;
+    public override int RequiredVersion => 136;
 
     public override void BackupDatabase(string fullfilename)
     {
@@ -862,6 +862,8 @@ public class SQLite : BaseDatabase<SqliteConnection>
         new(134, 02, DatabaseFixes.MoveTmdbImagesOnDisc),
         new(135, 01, "DROP TABLE IF EXISTS DuplicateFile;"),
         new(135, 02, "DROP TABLE IF EXISTS AnimeCharacter;"),
+        new(136, 01, "DROP TABLE IF EXISTS Tmdb_Show_Network;"),
+        new(136, 02, "CREATE TABLE Tmdb_Show_Network ( TMDB_Show_NetworkID INTEGER PRIMARY KEY AUTOINCREMENT, TmdbShowID INTEGER NOT NULL, TmdbNetworkID INTEGER NOT NULL, Ordering INTEGER NOT NULL );"),
     };
 
     private static Tuple<bool, string> MigrateRenamers(object connection)
