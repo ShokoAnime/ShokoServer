74149b0843a6cf20de40f02826213735517fbcc9
Mikal Stordal <mikalstordal@gmail.com>
2025-10-27 03:45:36 +0100

    fix: fix last episode update for series + more
    - Fixed the `LastEpisodeUpdate` field on `AnimeSeries_User` to properly also be updated when _any_ video for the series has been updated, fixing the continue-watching section on the Web UI dashboard.
    
    - Documented each field on `AnimeSeries_User`, converted it from using a block-based to using a file-based namespace, and removed unnecessary baggage from when the model was shared with Shoko Desktop.
    
    It is recommended to refresh all stats after installing a version containing this commit.

--- Shoko.Models/Server/ShokoInternal/AnimeSeries_User.cs
+++ Shoko.Models/Server/ShokoInternal/AnimeSeries_User.cs
@@ -1,43 +1,65 @@
 ﻿using System;
 
-namespace Shoko.Models.Server
+namespace Shoko.Models.Server;
+
+public class AnimeSeries_User
 {
-    public class AnimeSeries_User : ICloneable
-    {
-        #region DB Columns
+    /// <summary>
+    /// Local DB Row ID.
+    /// </summary>
+    public int AnimeSeries_UserID { get; set; }
 
-        public int AnimeSeries_UserID { get; set; }
-        public int JMMUserID { get; set; }
-        public int AnimeSeriesID { get; set; }
+    /// <summary>
+    /// Shoko User ID.
+    /// </summary>
+    public int JMMUserID { get; set; }
 
-        public int UnwatchedEpisodeCount { get; set; }
-        public int HiddenUnwatchedEpisodeCount { get; set; }
-        public int WatchedEpisodeCount { get; set; }
-        public DateTime? WatchedDate { get; set; }
-        public int PlayedCount { get; set; }
-        public int WatchedCount { get; set; }
-        public int StoppedCount { get; set; }
-        public DateTime? LastEpisodeUpdate { get; set; }
+    /// <summary>
+    /// Shoko Series ID.
+    /// </summary>
+    public int AnimeSeriesID { get; set; }
 
-        #endregion
+    /// <summary>
+    /// The number of episodes that have not been watched to completion,
+    /// and are not hidden.
+    /// </summary>
+    public int UnwatchedEpisodeCount { get; set; }
 
+    /// <summary>
+    /// The number of episodes that have not been watched to completion,
+    /// and are hidden.
+    /// </summary>
+    public int HiddenUnwatchedEpisodeCount { get; set; }
 
-        public object Clone()
-        {
-            return new AnimeSeries_User
-            {
-                AnimeSeries_UserID = AnimeSeries_UserID,
-                JMMUserID = JMMUserID,
-                AnimeSeriesID = AnimeSeriesID,
-                UnwatchedEpisodeCount = UnwatchedEpisodeCount,
-                HiddenUnwatchedEpisodeCount = HiddenUnwatchedEpisodeCount,
-                WatchedEpisodeCount = WatchedEpisodeCount,
-                WatchedDate = WatchedDate,
-                PlayedCount = PlayedCount,
-                WatchedCount = WatchedCount,
-                StoppedCount = StoppedCount,
-                LastEpisodeUpdate = LastEpisodeUpdate,
-            };
-        }
-    }
+    /// <summary>
+    /// The number of episodes that have been watched to completion.
+    /// </summary>
+    public int WatchedEpisodeCount { get; set; }
+
+    /// <summary>
+    /// The last time the series was watched to completion.
+    /// </summary>
+    public DateTime? WatchedDate { get; set; }
+
+    /// <summary>
+    ///  How many times videos have been started/played for the series. Only used by Shoko Desktop and APIv1.
+    /// </summary>
+    public int PlayedCount { get; set; }
+
+    /// <summary>
+    /// How many videos have been played to completion for the series.
+    /// </summary>
+    public int WatchedCount { get; set; }
+
+    /// <summary>
+    ///  How many times videos have been stopped for the series. Only used by Shoko Desktop and APIv1.
+    /// </summary>
+    public int StoppedCount { get; set; }
+
+    /// <summary>
+    /// The last time an episode was updated, regardless of if it was
+    /// watched to completion or not. Used to determine continue watching
+    /// and next-up order for the series and on the dashboard.
+    /// </summary>
+    public DateTime? LastEpisodeUpdate { get; set; }
 }
