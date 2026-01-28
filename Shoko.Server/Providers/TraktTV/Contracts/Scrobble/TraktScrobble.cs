c731efeebdf4170953874430ac2f2fd52e187a68
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2026-01-28 23:16:01 +0530

    fix: don't send null movie or episode for scrobble

--- Shoko.Server/Providers/TraktTV/Contracts/Scrobble/TraktScrobble.cs
+++ Shoko.Server/Providers/TraktTV/Contracts/Scrobble/TraktScrobble.cs
@@ -5,10 +5,10 @@ namespace Shoko.Server.Providers.TraktTV.Contracts.Scrobble;
 [DataContract]
 internal class TraktScrobble
 {
-    [DataMember(Name = "episode")]
+    [DataMember(Name = "episode", EmitDefaultValue = false)]
     public TraktScrobbleItem Episode { get; set; }
 
-    [DataMember(Name = "movie")]
+    [DataMember(Name = "movie", EmitDefaultValue = false)]
     public TraktScrobbleItem Movie { get; set; }
 
     [DataMember(Name = "progress")]
