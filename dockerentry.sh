267d8c7e390e1395fe865f361718d573db25d009
Mikal Stordal <mikalstordal@gmail.com>
2026-02-17 22:35:13 +0100

    feat: improve Docker container user management and add SHOKO_HOME validation before starting server

--- dockerentry.sh
+++ dockerentry.sh
@@ -6,40 +6,71 @@ echo "Started Shoko Server bootstrapping process…"
 PUID=${PUID:-1000}
 PGID=${PGID:-100}
 
-[ $PUID -eq 0 ] && [ $PGID -eq 0 ] && echo "You really shouldn't be doing this..." && dotnet /usr/src/app/build/Shoko.CLI.dll
-
 GROUP="shokogroup"
 USER="shoko"
 
-if [ $(getent group $GROUP) ]; then #if group exists
-    if [ $(getent group $GROUP | cut -d: -f3) -ne $PGID ]; then #if gid of said group doesn't match
+# Well-known users.
+if [ "$PUID" -eq 0 ]; then
+    USER="root"
+fi
+
+# Well-known groups.
+if [ "$PGID" -eq 0 ]; then
+    GROUP="root"
+elif [ "$PGID" -eq 100 ]; then
+    GROUP="users"
+fi
+
+# Create or update group.
+if [ $(getent group $GROUP) ]; then
+    if [ $(getent group $GROUP | cut -d: -f3) -ne $PGID ]; then
         groupmod -g "$PGID" $GROUP
-        REDO_PERM=1
     fi
 else
     groupadd -o -g "$PGID" $GROUP
 fi
 
+# Create or update user.
 if [ $(getent passwd $USER) ]; then
     if [ $(getent passwd $USER | cut -d: -f3) -ne $PUID ]; then
         usermod -u "$PUID" $USER
-        REDO_PERM=1
     fi
     [ $(id -g $USER) -ne $PGID ] && usermod -g "$PGID" $USER
 else
     echo "Adding user $USER and changing ownership of /home/shoko and all it's sub-directories…"
     useradd  -N -o -u "$PUID" -g "$PGID" -d /home/shoko $USER
 
-    mkdir -p /home/shoko/.shoko/
-    chown -R $USER:$GROUP /home/shoko
+    mkdir -p /home/shoko/
+    chown $USER:$GROUP /home/shoko
 fi
 
-if [ $REDO_PERM ]; then
+# Make sure SHOKO_HOME directory is correctly set.
+SHOKO_HOME=${SHOKO_HOME:-/home/shoko/.shoko/}
+if [ "$PUID" -eq 0 ]; then
+    if [ "$SHOKO_HOME" == "/home/shoko/.shoko/" ]; then
+        echo "Error: Cannot use default SHOKO_HOME directory when running as root (PUID=0)."
+        echo "Please set a custom SHOKO_HOME directory."
+        exit 1
+    fi
+fi
+if [ ! -d "$SHOKO_HOME" ]; then
+    if [ "$SHOKO_HOME" == "/home/shoko/.shoko/" ]; then
+        echo "Creating default SHOKO_HOME directory: $SHOKO_HOME"
+        mkdir -p "$SHOKO_HOME"
+    else
+        echo "Error: SHOKO_HOME directory ($SHOKO_HOME) does not exist!"
+        exit 1
+    fi
+fi
+
+# Set ownership of application data to shoko user.
+OWNER=$(stat -c '%u:%g' "$SHOKO_HOME" 2>/dev/null)
+if [ "$OWNER" != "$PUID:$PGID" ]; then
     echo "Changing ownership of /home/shoko and all it's sub-directories…"
     chown -R $PUID:$PGID /home/shoko/
 fi
 
-# Set owership of shoko files to shoko user
+# Set ownership of shoko files to shoko user
 chown -R $USER:$GROUP /usr/src/app/build/
 if [ -d /root/.shoko ]; then
     echo "
@@ -61,9 +92,10 @@ fi
 
 echo "
 -------------------------------------
-User uid:    $(id -u $USER)
-User gid:    $(id -g $USER)
-UMASK set:    $(umask)
+User ID:   $(id -u $USER)
+Group ID:  $(id -g $USER)
+UMASK set: $(umask)
+Directory: \"$SHOKO_HOME\"
 -------------------------------------
 "
 
