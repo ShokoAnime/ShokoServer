e9ffe45d2a33866164d942db4a62a61f2faa9169
Cazzar <Cazzar@users.noreply.github.com>
2026-04-05 23:54:05 +1000

    fix: JTokenExtensions.ToJson returns empty string for empty string JValues, causing JToken.Parse to throw

--- Shoko.Server/Services/Configuration/JTokenExtensions.cs
+++ Shoko.Server/Services/Configuration/JTokenExtensions.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Shoko.Server.Services.Configuration;
@@ -5,7 +6,10 @@ namespace Shoko.Server.Services.Configuration;
 internal static class JTokenExtensions
 {
     internal static string ToJson(this JToken token)
-        => token.Type is JTokenType.Boolean
-            ? token.Value<bool>().ToString().ToLowerInvariant()
-            : token.ToString();
+        => token.Type switch
+        {
+            JTokenType.Boolean => token.Value<bool>().ToString().ToLowerInvariant(),
+            JTokenType.String => JsonConvert.SerializeObject(token.Value<string>()),
+            _ => token.ToString(),
+        };
 }
