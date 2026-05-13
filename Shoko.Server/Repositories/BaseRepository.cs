1db8cf055b55e9e3691a833782f60037f3b62161
krbrs <57227244+krbrs@users.noreply.github.com>
2026-05-13 14:23:40 +0200

    style(data): group BaseRepository lock overloads
    Reorder BaseRepository methods so all Lock(...) overloads remain adjacent
    and satisfy csharpsquid:S4136. No functional change.

--- Shoko.Server/Repositories/BaseRepository.cs
+++ Shoko.Server/Repositories/BaseRepository.cs
@@ -13,6 +13,41 @@ public class BaseRepository
         WriteLock(action);
     }
 
+    public static void Lock<T>(T arg, Action<T> action)
+    {
+        WriteLock(() => action(arg));
+    }
+
+    public static void Lock<T, T1>(T arg, T1 arg2, Action<T, T1> action)
+    {
+        WriteLock(() => action(arg, arg2));
+    }
+
+    public static void Lock<T, T1, T2>(T arg, T1 arg2, T2 arg3, Action<T, T1, T2> action)
+    {
+        WriteLock(() => action(arg, arg2, arg3));
+    }
+
+    public static T Lock<T>(Func<T> action)
+    {
+        return WriteLock(action);
+    }
+
+    public static T Lock<T, T1>(T1 arg, Func<T1, T> action)
+    {
+        return WriteLock(() => action(arg));
+    }
+
+    public static T Lock<T, T1, T2>(T1 arg, T2 arg2, Func<T1, T2, T> action)
+    {
+        return WriteLock(() => action(arg, arg2));
+    }
+
+    public static T Lock<T, T1, T2, T3>(T1 arg, T2 arg2, T3 arg3, Func<T1, T2, T3, T> action)
+    {
+        return WriteLock(() => action(arg, arg2, arg3));
+    }
+
     public static void ReadLock(Action action)
     {
         var useLock = Utils.SettingsProvider.GetSettings().Database.UseDatabaseLock;
@@ -101,40 +136,5 @@ public class BaseRepository
         }
 
         return result;
-    }
-
-    public static void Lock<T>(T arg, Action<T> action)
-    {
-        WriteLock(() => action(arg));
-    }
-
-    public static void Lock<T, T1>(T arg, T1 arg2, Action<T, T1> action)
-    {
-        WriteLock(() => action(arg, arg2));
-    }
-
-    public static void Lock<T, T1, T2>(T arg, T1 arg2, T2 arg3, Action<T, T1, T2> action)
-    {
-        WriteLock(() => action(arg, arg2, arg3));
-    }
-
-    public static T Lock<T>(Func<T> action)
-    {
-        return WriteLock(action);
-    }
-
-    public static T Lock<T, T1>(T1 arg, Func<T1, T> action)
-    {
-        return WriteLock(() => action(arg));
-    }
-
-    public static T Lock<T, T1, T2>(T1 arg, T2 arg2, Func<T1, T2, T> action)
-    {
-        return WriteLock(() => action(arg, arg2));
-    }
-
-    public static T Lock<T, T1, T2, T3>(T1 arg, T2 arg2, T3 arg3, Func<T1, T2, T3, T> action)
-    {
-        return WriteLock(() => action(arg, arg2, arg3));
     }
 }
