4c5ae8fca9358af2514bbd82ac9759531e7139b5
revam <noreply.git@revam.dev>
2026-05-26 04:47:17 +0200

    fix: add back missing APIv2 init endpoints
    Added back some of the missing APIv2 /api/init endpoints. With the endpoints which shouldn't be used such as /api/init/config, /api/init/database, /api/init/settings soft deprecated in favor of APIv3.

---
+++ Shoko.Server/API/v2/Modules/Init.cs
@@ -0,0 +1,373 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Shoko.Abstractions.Core.Services;
+using Shoko.Abstractions.Extensions;
+using Shoko.Abstractions.Web.Attributes;
+using Shoko.Server.API.Annotations;
+using Shoko.Server.API.v2.Models.core;
+using Shoko.Server.MediaInfo;
+using Shoko.Server.Providers.AniDB.Interfaces;
+using Shoko.Server.Settings;
+
+//using Microsoft.SqlServer.Management.Smo;
+
+namespace Shoko.Server.API.v2.Modules;
+
+// ReSharper disable once UnusedMember.Global
+[Route("/api/init")]
+[ApiController]
+[ApiVersion("2.0")]
+[DatabaseBlockedExempt]
+[InitFriendly]
+public class Init : BaseController
+{
+    private readonly ILogger<Init> _logger;
+
+    private readonly IServerSettings _settings;
+
+    private readonly ISystemService _systemService;
+    private readonly ISystemUpdateService _webUIUpdateService;
+
+    public Init(ILogger<Init> logger, ISettingsProvider settingsProvider, ISystemService systemService, ISystemUpdateService webUIUpdateService) : base(settingsProvider)
+    {
+        _logger = logger;
+        _systemService = systemService;
+        _webUIUpdateService = webUIUpdateService;
+        _settings = settingsProvider.GetSettings();
+    }
+
+    /// <summary>
+    /// Return current version of ShokoServer and several modules
+    /// This will work after init
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("version")]
+    public List<ComponentVersion> GetVersion()
+    {
+        List<ComponentVersion> list = new List<ComponentVersion>();
+
+        ComponentVersion version = new ComponentVersion
+        {
+            version = _systemService.Version.Version.ToSemanticVersioningString(),
+            name = "server"
+        };
+        list.Add(version);
+
+        if (_systemService.Version.SourceRevision is { Length: > 0 } versionExtra)
+        {
+            version = new ComponentVersion
+            {
+                version = versionExtra,
+                name = "servercommit"
+            };
+            list.Add(version);
+        }
+
+        version = new ComponentVersion
+        {
+            version = _systemService.Version.Version.ToSemanticVersioningString(),
+            name = "commons"
+        };
+        list.Add(version);
+
+        version = new ComponentVersion
+        {
+            version = _systemService.Version.Version.ToSemanticVersioningString(),
+            name = "models"
+        };
+        list.Add(version);
+
+        version = new ComponentVersion
+        {
+            version = MediaInfoUtility.GetVersion() is { Length: > 0 } mediaInfoVersion
+                ? mediaInfoVersion
+                : "DLL not found, using internal",
+            name = "MediaInfo",
+        };
+        list.Add(version);
+
+        var webuiVersion = _webUIUpdateService.LoadWebComponentVersionInformation();
+        if (webuiVersion != null)
+        {
+            version = new ComponentVersion
+            {
+                name = "webui/" + webuiVersion.Channel.ToString().ToLower(),
+                version = webuiVersion.Version.ToSemanticVersioningString(),
+            };
+            list.Add(version);
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Gets various information about the startup status of the server
+    /// This will work after init
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("status")]
+    public ServerStatus GetServerStatus()
+    {
+        var uptime = _systemService.Uptime;
+        var uptimeMessage = $"{(int)uptime.TotalHours:00}:{uptime.Minutes:00}:{uptime.Seconds:00}";
+        return new()
+        {
+            server_started = _systemService.IsStarted,
+            startup_state = _systemService.StartupMessage,
+            server_uptime = uptimeMessage,
+            first_run = _systemService.InSetupMode,
+            startup_failed = _systemService.StartupFailedException is not null,
+            startup_failed_error_message = _systemService.StartupFailedException?.Message,
+        };
+    }
+
+    /// <summary>
+    /// Gets whether anything is actively using the API
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("inuse")]
+    public bool ApiInUse() => ApiInUseAttribute.IsInUse;
+
+    /// <summary>
+    /// Gets the Default user's credentials. Will only return on first run
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("defaultuser")]
+    public ActionResult<Credentials> GetDefaultUserCredentials()
+    {
+        return new Credentials
+        {
+            login = _settings.Database.DefaultUserUsername,
+            password = _settings.Database.DefaultUserPassword
+        };
+    }
+
+    /// <summary>
+    /// Sets the default user's credentials
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpPost("defaultuser")]
+    public ActionResult SetDefaultUserCredentials(Credentials credentials)
+    {
+        try
+        {
+            _settings.Database.DefaultUserUsername = credentials.login;
+            _settings.Database.DefaultUserPassword = credentials.password;
+            return APIStatus.OK();
+        }
+        catch
+        {
+            return APIStatus.InternalError();
+        }
+    }
+
+    /// <summary>
+    /// Starts the server, or does nothing
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("startserver")]
+    public ActionResult StartServer()
+    {
+        if (_systemService.IsStarted)
+            return APIStatus.BadRequest("Already Running");
+        if (_systemService.StartupFailedException is not null)
+            return BadRequest("Startup Failed");
+        if (!_systemService.InSetupMode)
+            return APIStatus.BadRequest("Already Starting");
+        try
+        {
+            _systemService.CompleteSetup();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "There was an error starting the server");
+            return APIStatus.InternalError($"There was an error starting the server: {e}");
+        }
+        return APIStatus.OK();
+    }
+
+    #region 01. AniDB
+
+    /// <summary>
+    /// Set AniDB account credentials with a Credentials object
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpPost("anidb")]
+    public ActionResult SetAniDB(Credentials cred)
+    {
+        var details = new List<(string, string)>();
+        if (string.IsNullOrEmpty(cred.login))
+            details.Add(("login", "Username missing"));
+        if (string.IsNullOrEmpty(cred.password))
+            details.Add(("password", "Password missing"));
+        if (details.Count > 0) return new APIMessage(400, "Login or Password missing", details);
+
+        _settings.AniDb.Username = cred.login;
+        _settings.AniDb.Password = cred.password;
+        if (cred.port != 0)
+            _settings.AniDb.ClientPort = cred.port;
+        if (!string.IsNullOrEmpty(cred.apikey))
+            _settings.AniDb.AVDumpKey = cred.apikey;
+        if (cred.apiport != 0)
+            _settings.AniDb.AVDumpClientPort = cred.apiport;
+
+        return APIStatus.OK();
+    }
+
+    /// <summary>
+    /// Test AniDB Creditentials
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("anidb/test")]
+    public ActionResult TestAniDB()
+    {
+        var handler = HttpContext.RequestServices.GetRequiredService<IUDPConnectionHandler>();
+        handler.ForceLogout();
+        handler.CloseConnections();
+
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(_settings.Culture);
+
+        handler.Init(_settings.AniDb.Username, _settings.AniDb.Password,
+            _settings.AniDb.UDPServerAddress,
+            _settings.AniDb.UDPServerPort, _settings.AniDb.ClientPort);
+
+        if (!handler.Login()) return APIStatus.BadRequest("Failed to log in");
+        handler.ForceLogout();
+
+        return APIStatus.OK();
+    }
+
+    /// <summary>
+    /// Return existing login and ports for AniDB
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("anidb")]
+    public ActionResult<Credentials> GetAniDB()
+    {
+        try
+        {
+            return new Credentials
+            {
+                login = _settings.AniDb.Username,
+                port = _settings.AniDb.ClientPort,
+                apiport = _settings.AniDb.AVDumpClientPort
+            };
+        }
+        catch
+        {
+            return APIStatus.InternalError(
+                "The ports are not set as integers. Set them and try again.\n\rThe default values are:\n\rAniDB Client Port: 4556\n\rAniDB AVDump Client Port: 4557");
+        }
+    }
+
+    #endregion
+
+    #region 02. Database
+
+    /// <summary>
+    /// Get Database Settings
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("database")]
+    public ActionResult GetDatabaseSettings()
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to get the database settings instead.");
+    }
+
+    /// <summary>
+    /// Set Database Settings
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpPost("database")]
+    public ActionResult SetDatabaseSettings([FromBody] JToken body = null)
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to tweak the database settings instead.");
+    }
+
+    /// <summary>
+    /// Test Database Connection with Current Settings
+    /// </summary>
+    /// <returns>200 if connection successful, 400 otherwise</returns>
+    [Authorize("init")]
+    [HttpGet("database/test")]
+    public ActionResult TestDatabaseConnection()
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to test the database connection instead.");
+    }
+
+    /// <summary>
+    /// Get SQL Server Instances Running on this Machine
+    /// </summary>
+    /// <returns>List of strings that may be passed as sqlserver_databaseserver</returns>
+    [Authorize("init")]
+    [HttpGet("database/sqlserverinstance")]
+    public ActionResult GetMSSQLInstances()
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to get the SQL Server instances instead.");
+    }
+
+    #endregion
+
+    #region 03. Settings
+
+    /// <summary>
+    /// Return body of current working settings.json - this could act as backup
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("config")]
+    public ActionResult ExportConfig()
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to get the settings instead.");
+    }
+
+    /// <summary>
+    /// Import config file that was sent to in API body - this act as import from backup
+    /// </summary>
+    /// <returns>APIStatus</returns>
+    [Authorize("init")]
+    [HttpPost("config")]
+    [Obsolete]
+    public ActionResult ImportConfig([FromBody] JToken body = null)
+    {
+        return BadRequest("The model that this method takes is deprecated and will break the settings file. Use APIv3");
+    }
+
+    /// <summary>
+    /// Return given setting
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpGet("setting")]
+    public ActionResult<object> GetSetting([FromBody] JToken body = null)
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to get the settings instead.");
+    }
+
+    /// <summary>
+    /// Set given setting
+    /// </summary>
+    /// <returns></returns>
+    [Authorize("init")]
+    [HttpPatch("setting")]
+    public ActionResult SetSetting([FromBody] JToken body = null)
+    {
+        return APIStatus.BadRequest("Disabled. Use APIv3 to set the settings instead.");
+    }
+
+    #endregion
+}
