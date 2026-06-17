c8a01aab6f9c1e0250030b1507f7edb36ec7ed90
da3dsoul <da3dsoul@gmail.com>
2026-06-17 00:02:40 -0400

    Updates for server side searching

--- Shoko.Server/Repositories/Cached/AniDB/AniDB_Anime_TitleRepository.cs
+++ Shoko.Server/Repositories/Cached/AniDB/AniDB_Anime_TitleRepository.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using NutzCode.InMemoryIndex;
+using Shoko.Abstractions.Metadata.Enums;
 using Shoko.Server.Databases;
 using Shoko.Server.Models.AniDB;
 
@@ -11,12 +14,18 @@ public class AniDB_Anime_TitleRepository(DatabaseFactory databaseFactory) : Base
 {
     private PocoIndex<int, AniDB_Anime_Title, int>? _animeIDs;
 
+    private Dictionary<int, IReadOnlyList<string>> _normalizedSearchTitles = new();
+
     protected override int SelectKey(AniDB_Anime_Title entity)
         => entity.AniDB_Anime_TitleID;
 
     public override void PopulateIndexes()
     {
         _animeIDs = Cache.CreateIndex(a => a.AnimeID);
+        _normalizedSearchTitles = Cache.Values
+            .Where(t => t.TitleType is TitleType.Main or TitleType.Official)
+            .GroupBy(t => t.AnimeID)
+            .ToDictionary(g => g.Key, g => (IReadOnlyList<string>)g.Select(t => NormalizeForSearch(t.Title)).ToList());
     }
 
     public override void RegenerateDb()
@@ -30,6 +39,37 @@ public class AniDB_Anime_TitleRepository(DatabaseFactory databaseFactory) : Base
             Save(title);
         }
     }
+
+    protected override void UpdateCacheUnsafe(AniDB_Anime_Title title)
+    {
+        base.UpdateCacheUnsafe(title);
+        RebuildNormalizedForAnime(title.AnimeID);
+    }
+
+    protected override void DeleteFromCacheUnsafe(AniDB_Anime_Title title)
+    {
+        base.DeleteFromCacheUnsafe(title);
+        RebuildNormalizedForAnime(title.AnimeID);
+    }
+
+    private void RebuildNormalizedForAnime(int animeID)
+    {
+        var normalized = (_animeIDs?.GetMultiple(animeID) ?? [])
+            .Where(t => t.TitleType is TitleType.Main or TitleType.Official)
+            .Select(t => NormalizeForSearch(t.Title))
+            .ToList();
+        if (normalized.Count == 0)
+            _normalizedSearchTitles.Remove(animeID);
+        else
+            _normalizedSearchTitles[animeID] = normalized;
+    }
+
+    public static string NormalizeForSearch(string value)
+        => value.Normalize(NormalizationForm.FormKC).ToLowerInvariant();
+
+    public bool AnimeMatchesSearch(int animeID, string normalizedQuery)
+        => ReadLock(() => _normalizedSearchTitles.TryGetValue(animeID, out var titles)
+            && titles.Any(t => t.Contains(normalizedQuery, StringComparison.Ordinal)));
 
     public List<AniDB_Anime_Title> GetByAnimeID(int animeID)
         => ReadLock(() => _animeIDs!.GetMultiple(animeID));
