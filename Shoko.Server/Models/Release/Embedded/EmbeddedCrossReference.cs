65708289f3057a51c993b5d9878acf707bfed7bf
da3dsoul <da3dsoul@gmail.com>
2026-06-25 09:25:38 -0400

    Null handling for EmbeddedCrossReference

--- Shoko.Server/Models/Release/Embedded/EmbeddedCrossReference.cs
+++ Shoko.Server/Models/Release/Embedded/EmbeddedCrossReference.cs
@@ -1,27 +1,32 @@
+#nullable enable
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Shoko.Abstractions.Metadata.Enums;
 using Shoko.Abstractions.Video.Release;
 
-#nullable enable
 namespace Shoko.Server.Models.Release;
 
 public class EmbeddedCrossReference : IReleaseVideoCrossReference
 {
     public Dictionary<string, string> ProviderIDs { get; set; } = new();
 
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public int PercentageStart { get; set; }
 
-    public int PercentageEnd { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public int PercentageEnd { get; set; } = 100;
 
     /// <summary>
     /// Episode type (Episode, Special, Credits, etc.). Cached from AniDB at
     /// save time; not a provider-supplied ID.
     /// </summary>
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public EpisodeType EpisodeType { get; set; } = EpisodeType.Episode;
 
     /// <summary>
     /// Episode number within its type. Cached from AniDB at save time.
     /// </summary>
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public int EpisodeNumber { get; set; }
 
     public EmbeddedCrossReference() { }
